a88e1a1c849bf1c257d998c53b32deaba1e4e7c1
Michael Jackson <michael@jackson.us>
2020-07-21 12:05:45 -0700

    Better console output when build fails

--- scripts/build.js
+++ scripts/build.js
@@ -4,9 +4,19 @@ import { promisify } from "util";
 const x = promisify(exec);
 
 async function run() {
-  await x("tsc -b");
-
   await x("mkdir -p build/@remix-run");
+
+  try {
+    await x("tsc -b");
+  } catch (error) {
+    console.error(error.stdout);
+    process.exit(1);
+  }
+
+  await x("cp -r .tsc-output/compiler build/@remix-run/compiler");
+  await x(
+    "cp packages/compiler/package.json build/@remix-run/compiler/package.json"
+  );
 
   await x("cp -r .tsc-output/core build/@remix-run/core");
   await x("cp packages/core/package.json build/@remix-run/core/package.json");
