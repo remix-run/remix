57039bdf6ad98f6c78edef8a85815ddc0d0c8037
Michael Jackson <michael@jackson.us>
2020-07-23 09:32:05 -0700

    Convert all build output to CommonJS

--- scripts/build.js
+++ scripts/build.js
@@ -4,8 +4,6 @@ import { promisify } from "util";
 const x = promisify(exec);
 
 async function run() {
-  await x("mkdir -p build/@remix-run");
-
   try {
     await x("tsc -b");
   } catch (error) {
@@ -13,20 +11,12 @@ async function run() {
     process.exit(1);
   }
 
-  await x("cp -r .tsc-output/compiler build/@remix-run/compiler");
-  await x(
-    "cp packages/compiler/package.json build/@remix-run/compiler/package.json"
-  );
-
-  await x("cp -r .tsc-output/core build/@remix-run/core");
   await x("cp packages/core/package.json build/@remix-run/core/package.json");
 
-  await x("cp -r .tsc-output/express build/@remix-run/express");
   await x(
     "cp packages/express/package.json build/@remix-run/express/package.json"
   );
 
-  await x("cp -r .tsc-output/react build/@remix-run/react");
   await x("cp packages/react/package.json build/@remix-run/react/package.json");
 
   return 0;
