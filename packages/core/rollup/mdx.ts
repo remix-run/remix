c64f25df64649ab941c2054e5fa998ef372cf1ef
Ryan Florence <rpflorence@gmail.com>
2020-10-02 18:03:20 -0600

    added meta/header support to markdown

--- packages/core/rollup/mdx.ts
+++ packages/core/rollup/mdx.ts
@@ -1,3 +1,4 @@
+import parseFrontMatter from "front-matter";
 import mdx from "@mdx-js/mdx";
 import type { Plugin } from "rollup";
 
@@ -8,6 +9,11 @@ import { mdx } from '@mdx-js/react'
 
 let regex = /\.mdx?$/;
 
+interface RemixFrontMatter {
+  meta?: { [name: string]: string };
+  headers?: { [header: string]: string };
+}
+
 export default function mdxTransform(): Plugin {
   return {
     name: "mdx",
@@ -16,11 +22,31 @@ export default function mdxTransform(): Plugin {
         return null;
       }
 
-      let js = await mdx(content, {});
-      let code = `${imports}\n${js}`;
-      // TODO: https://rollupjs.org/guide/en/#transformers says to return null
-      // if we don't "move the code" and it'll preserve mappings itself. I dunno
-      // what "move the code" means for sure, but I don't think I moved it.
+      let {
+        body,
+        attributes
+      }: {
+        body: string;
+        attributes: RemixFrontMatter;
+      } = parseFrontMatter(content);
+
+      let meta;
+      if (attributes?.meta) {
+        meta = `export function meta() { return ${JSON.stringify(
+          attributes.meta
+        )}}`;
+      }
+
+      let headers;
+      if (attributes?.headers) {
+        headers = `export function headers() { return ${JSON.stringify(
+          attributes.headers
+        )}}`;
+      }
+
+      let source = await mdx(body, {});
+      let code = [imports, headers, meta, source].filter(Boolean).join("\n");
+
       return { code, map: null };
     }
   };
