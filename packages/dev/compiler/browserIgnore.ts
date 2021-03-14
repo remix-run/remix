aeeeea35abc245336b0c43a8476754c255db3c86
Michael Jackson <michael@jackson.us>
2021-03-14 00:38:51 -0800

    Remove scary comment

--- packages/dev/compiler/browserIgnore.ts
+++ packages/dev/compiler/browserIgnore.ts
@@ -2,25 +2,17 @@
 // modules, which greatly speeds up the browser tree-shaking
 import builtins from "builtin-modules";
 
-export let ignorePackages = builtins.concat([
+const remixServerPackages = [
+  "@remix-run/architect",
+  "@remix-run/express",
+  "@remix-run/vercel"
+];
+
+const thirdPartyPackages = [
   "@databases/mysql",
   "@databases/pg",
   "@databases/sqlite",
   "@prisma/client",
-  // We SHOULD be able to treeshake this out of the build just like everything
-  // else, but Rollup complains about syntax errors in
-  // @remix-run/dev/compiler.js if we don't deliberately exclude it from the
-  // browser builds. This is a bug we do not understand yet! :/ We have only
-  // ever seen this problem when the build imports the built version of itself.
-  //
-  // Fortunately, we don't actually want any code from @remix-run/dev in the
-  // browser builds, so we can exclude it for now.
-  //
-  // TODO: For some adventurous soul, try to figure out why Rollup can't parse
-  // its own output...
-  "@remix-run/architect",
-  "@remix-run/express",
-  "@remix-run/vercel",
   "apollo-server",
   "better-sqlite3",
   "bookshelf",
@@ -39,4 +31,10 @@ export let ignorePackages = builtins.concat([
   "sqlite",
   "sqlite3",
   "tedious"
-]);
+];
+
+export let ignorePackages = [
+  ...builtins,
+  ...remixServerPackages,
+  ...thirdPartyPackages
+];
