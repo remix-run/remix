a8d71cd3aa6fbe7d0baf4bac6eaa1c4b58dccd5e
Ryan Florence <rpflorence@gmail.com>
2021-05-13 07:35:54 -0600

    remove @ts-expect-error from template
    @types/react-dom fixed it in 17.0.4

--- packages/remix-init/templates/_shared_ts/app/entry.client.tsx
+++ packages/remix-init/templates/_shared_ts/app/entry.client.tsx
@@ -1,9 +1,4 @@
 import ReactDOM from "react-dom";
 import { RemixBrowser } from "remix";
 
-// @types/react-dom says the 2nd argument to ReactDOM.hydrate() must be a
-// `Element | DocumentFragment | null` but React 16 allows you to pass the
-// `document` object as well. This is a bug in @types/react-dom that we can
-// safely ignore for now.
-// @ts-expect-error
 ReactDOM.hydrate(<RemixBrowser />, document);
