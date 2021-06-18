d33d45501fc3ea9085cfef06fb8f1d8826faac25
Ryan Florence <rpflorence@gmail.com>
2021-06-18 15:16:18 -0600

    little bit of PE

--- fixtures/gists-app/app/routes/pending-forms.tsx
+++ fixtures/gists-app/app/routes/pending-forms.tsx
@@ -1,4 +1,12 @@
-import { json, useLoaderData, useActionData, Form } from "remix";
+import styles from "../styles/pending-forms.css";
+import { useEffect, useRef, useState } from "react";
+import {
+  json,
+  useLoaderData,
+  useActionData,
+  Form,
+  usePendingFormSubmit
+} from "remix";
 
 interface Task {
   id: string;
@@ -27,6 +35,10 @@ let tasks: Task[] = [
     delay: 1000
   }
 ];
+
+export function links() {
+  return [{ rel: "stylesheet", href: styles }];
+}
 
 export async function loader() {
   return tasks;
@@ -68,24 +80,48 @@ export default function Tasks() {
 }
 
 function TaskItem({ task }: { task: Task }) {
+  let ref = useRef<HTMLFormElement>(null);
+  let pending = usePendingFormSubmit();
+  let thisIsPending = pending?.data.get("id") === task.id;
   let actionData = useActionData();
   let error = actionData?.id === task.id && actionData.error;
 
   return (
-    <Form forceRefresh method="post">
+    <Form method="post" ref={ref}>
       <input type="hidden" name="id" value={task.id} />
       <input type="hidden" name="complete" value={String(!task.complete)} />
       <button
+        disabled={!!pending}
         type="submit"
-        style={{
-          width: "10rem",
-          fontSize: "80%",
-          background: error ? "red" : task.complete ? "green" : "blue"
-        }}
+        data-status={
+          error ? "error" : task.complete ? "complete" : "incomplete"
+        }
       >
         {task.complete ? "Mark Incomplete" : "Mark Complete"}
+        {thisIsPending && <ProgressBar total={task.delay} />}
       </button>{" "}
       {task.name} {error && <span style={{ color: "red" }}>{error}</span>}
     </Form>
   );
+}
+
+function ProgressBar({ total }: { total: number }) {
+  let [ts, setTimeStamp] = useState(0);
+  let [start, setStart] = useState<null | number>(null);
+
+  useEffect(() => {
+    let id = requestAnimationFrame(now => {
+      setTimeStamp(now);
+      if (!start) setStart(now);
+    });
+    return () => cancelAnimationFrame(id);
+  }, [ts, start]);
+
+  let progress = 0;
+  if (start) {
+    let elapsed = ts - start;
+    progress = (elapsed / total) * 100;
+  }
+
+  return <progress value={progress} max="100" />;
 }
