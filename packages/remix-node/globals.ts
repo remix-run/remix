f94752eb6307b6c4b35fa6fbeccacce8af10781f
Michael Jackson <michael@jackson.us>
2021-08-17 17:57:37 -0700

    Add base64 encoding primitives to node globals

--- packages/remix-node/globals.ts
+++ packages/remix-node/globals.ts
@@ -1,10 +1,13 @@
 import crypto from "crypto";
 
+import { atob, btoa } from "./base64";
 import { Headers, Request, Response, fetch } from "./fetch";
 
 declare global {
   namespace NodeJS {
     interface Global {
+      atob: typeof atob;
+      btoa: typeof btoa;
       Headers: typeof Headers;
       Request: typeof Request;
       Response: typeof Response;
@@ -15,12 +18,15 @@ declare global {
 }
 
 export function installGlobals() {
+  global.atob = atob;
+  global.btoa = btoa;
+
   (global as NodeJS.Global).Headers = Headers;
   (global as NodeJS.Global).Request = Request;
   (global as NodeJS.Global).Response = Response;
   (global as NodeJS.Global).fetch = fetch;
 
-  // TODO: Do we need to install something here to get crypto.webcrypto types?
+  // TODO: Missing types
   // @ts-expect-error
-  (global as NodeJS.Global).crypto = crypto.webcrypto as Crypto;
+  global.crypto = crypto.webcrypto;
 }
