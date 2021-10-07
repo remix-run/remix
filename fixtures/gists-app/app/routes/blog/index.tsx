6b7c3be2dd75fe47a66d26619a8e5d9dd1273dd1
Jacob Ebey <jacob.ebey@live.com>
2021-10-07 13:20:17 -0700

    removed console.logs

--- fixtures/gists-app/app/routes/blog/index.tsx
+++ fixtures/gists-app/app/routes/blog/index.tsx
@@ -23,7 +23,6 @@ function postFromModule(mod: any): Post {
 }
 
 export let loader: LoaderFunction = () => {
-  console.log({ HERE: "HERE!!!!" });
   let data: PostsData = {
     posts: [
       postFromModule(helloPost),
@@ -51,7 +50,6 @@ export let headers: HeadersFunction = ({ loaderHeaders }) => {
 
 export default function BlogPosts() {
   let locationPending = usePendingLocation();
-  console.log({ useRouteData: useRouteData<PostsData>() });
   let { posts } = useRouteData<PostsData>();
 
   return (
