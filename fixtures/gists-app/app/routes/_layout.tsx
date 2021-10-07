2d9e4dcc09b3dd1a865195a65ee0efb7598c6b09
Jacob Ebey <jacob.ebey@live.com>
2021-10-07 13:19:13 -0700

    remove comment

--- fixtures/gists-app/app/routes/_layout.tsx
+++ fixtures/gists-app/app/routes/_layout.tsx
@@ -2,18 +2,6 @@ import { Routes, Route, Outlet } from "react-router-dom";
 
 export default function LayoutTest() {
   return (
-    // <Routes>
-    //   <Route
-    //     element={
-    //       <div>
-    //         Layout <Outlet />
-    //       </div>
-    //     }
-    //   >
-    //     <Route path="test" element={<div>Child</div>} />
-    //   </Route>
-    //   <Route index element={<div>Index</div>} />
-    // </Routes>
     <div>
       <h1>Layout Test</h1>
       <Outlet />
