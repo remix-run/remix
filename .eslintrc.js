fb7575d0a8c675b5d92c696ca2f014cdf3f1de45
Logan McAnsh <logan@mcan.sh>
2021-10-07 11:33:49 -0400

    chore: disable no-restricted-globals for cloudflare workers files
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,23 @@
 module.exports = {
   extends: ["react-app", "plugin:import/typescript"],
   parser: "@typescript-eslint/parser",
-  parserOptions: {
-    sourceType: "module",
-    ecmaVersion: 2018
-  },
   plugins: ["@typescript-eslint", "import"],
   settings: {
     "import/resolver": {
       typescript: {}
     }
   },
+  overrides: [
+    {
+      files: [
+        "packages/remix-init/templates/cloudflare-workers/**/*.js",
+        "packages/remix-cloudflare-workers/**/*.ts"
+      ],
+      rules: {
+        "no-restricted-globals": "off"
+      }
+    }
+  ],
   rules: {
     "@typescript-eslint/consistent-type-imports": "error",
 
