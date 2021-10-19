7a58406ef77fb90fa8d98d3d0e1fce958ac95351
Jacob Ebey <jacob.ebey@live.com>
2021-10-18 17:38:17 -0700

    reverted install_remix script

--- fixtures/gists-app/scripts/install_remix.js
+++ fixtures/gists-app/scripts/install_remix.js
@@ -10,12 +10,6 @@ async function run() {
   // Install all remix packages
   await fs.ensureDir(installDir);
   await fs.copy(buildDir, installDir);
-
-  let cli = path.resolve(installDir, "@remix-run/dev/cli.js");
-  let symlink = path.resolve(installDir, ".bin/remix");
-  await fs.chmod(cli, "755");
-  await fs.createSymlink(cli, symlink);
-  await fs.chmod(symlink, "755");
 }
 
 run().then(
