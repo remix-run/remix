e4c7d5b9529349d5b96496a303f10fcc32c47c14
Chance Strickland <hi@chance.dev>
2021-11-18 10:49:45 -0800

    chore(create-remix): Update starter template

--- packages/create-remix/templates/_shared_ts/app/routes/index.tsx
+++ packages/create-remix/templates/_shared_ts/app/routes/index.tsx
@@ -1,9 +1,40 @@
+import { useLoaderData, json } from "remix";
 import type { MetaFunction, LinksFunction, LoaderFunction } from "remix";
-import { useLoaderData } from "remix";
-import { Link } from "react-router-dom";
 
 import stylesUrl from "../styles/index.css";
 
+import { getRemixResources } from "~/data.server";
+import type { RemixResource } from "~/data.server";
+
+interface LoaderData {
+  resources: Array<RemixResource>;
+}
+
+/**
+ * Loader!? What the heck is a loader?
+ *
+ * In Remix, data from your server comes from a `loader` function defined inside
+ * of a route module. Any route can export a loader function and it will only be
+ * called on the server.
+ *
+ * @see {@link [Loading Data in Remix](https://docs.remix.run/v0.21/tutorial/3-loading-data)}
+ * @see {@link [Loader API](https://docs.remix.run/v0.21/api/app/#loader)}
+ */
+export let loader: LoaderFunction = async ({ request, params, context }) => {
+  let loaderData: LoaderData = {
+    resources: await getRemixResources()
+  };
+  return json(loaderData);
+};
+
+/**
+ * In addition to actions, loaders, and links, route modules can export a `meta`
+ * function. This function returns an object that is used to render HTML
+ * `<meta>` tags in the route. The `title` key renders the route's `<title>`
+ * element.
+ *
+ * @see {@link [Meta API](https://docs.remix.run/v0.21/api/app/#meta)}
+ */
 export let meta: MetaFunction = () => {
   return {
     title: "Remix Starter",
@@ -15,24 +46,32 @@ export let links: LinksFunction = () => {
   return [{ rel: "stylesheet", href: stylesUrl }];
 };
 
-export let loader: LoaderFunction = async () => {
-  return { message: "this is awesome 😎" };
-};
-
 export default function Index() {
-  let data = useLoaderData();
+  let data = useLoaderData<LoaderData>();
 
   return (
-    <div style={{ textAlign: "center", padding: 20 }}>
-      <h2>Welcome to Remix!</h2>
-      <p>
-        <a href="https://docs.remix.run">Check out the docs</a> to get started.
-      </p>
-      <p>Message from the loader: {data.message}</p>
-      <p>
-        <Link to="not-found">Link to 404 not found page.</Link> Clicking this
-        link will land you in your root CatchBoundary component.
-      </p>
+    <div className="homepage">
+      <div className="homepage__intro">
+        <h2>Welcome to Remix!</h2>
+        <p>We're stoked that you're here. 🥳</p>
+        <p>
+          Feel free to take a look around the code to see how we do things
+          differently. When you're ready to dive deeper, we've got plenty of
+          resources to get you up-and-running quickly.
+        </p>
+      </div>
+      <div className="homepage__resources">
+        <h2>Resources</h2>
+        <ul>
+          {data.resources.map(resource => {
+            return (
+              <li key={resource.id} className="homepage__resource">
+                <a href={resource.url}>{resource.name}</a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </div>
   );
 }
