5d164f772e0ad97e9621addb41ce237f835f7899
Sergio Xalambrí <hello@sergiodxa.com>
2021-11-22 15:36:04 -0500

    Add responseBack response helper

--- packages/remix-server-runtime/responses.ts
+++ packages/remix-server-runtime/responses.ts
@@ -41,3 +41,24 @@ export function redirect(
     headers
   });
 }
+
+/**
+ * Create a new Response with a redirect set to the URL the user was before.
+ * It uses the Referer header to detect the previous URL. It asks for a fallback
+ * URL in case the Referer couldn't be found, this fallback should be a URL you
+ * may be ok the user to land to after an action even if it's not the same.
+ * @example
+ * let action: ActionFunction = async ({ request }) => {
+ *   await doSomething(request);
+ *   // If the user was on `/search?query=something` we redirect to that URL
+ *   // but if we couldn't we redirect to `/search`, which is an good enough
+ *   // fallback
+ *   return redirectBack(request, { fallback: "/search" });
+ * }
+ */
+export function redirectBack(
+  request: Request,
+  { fallback, ...init }: ResponseInit & { fallback: string }
+): Response {
+  return redirect(request.headers.get("Referer") ?? fallback, init);
+}
