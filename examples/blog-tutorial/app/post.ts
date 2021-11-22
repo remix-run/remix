21101f38c13374facc5906a859faeb7c5fc7b7ca
Ryan Florence <rpflorence@gmail.com>
2021-11-22 12:54:36 -0700

    chore(examples): added blog tutorial

---
+++ examples/blog-tutorial/app/post.ts
@@ -0,0 +1,61 @@
+import path from "path";
+import fs from "fs/promises";
+import parseFrontMatter from "front-matter";
+import invariant from "tiny-invariant";
+import { processMarkdown } from "@ryanflorence/md";
+
+export type Post = {
+  slug: string;
+  title: string;
+};
+
+export type PostMarkdownAttributes = {
+  title: string;
+};
+
+let postsPath = path.join(__dirname, "../posts");
+
+function isValidPostAttributes(
+  attributes: any
+): attributes is PostMarkdownAttributes {
+  return attributes?.title;
+}
+
+type NewPost = {
+  title: string;
+  slug: string;
+  markdown: string;
+};
+
+export async function createPost(post: NewPost) {
+  let md = `---\ntitle: ${post.title}\n---\n\n${post.markdown}`;
+  await fs.writeFile(path.join(postsPath, post.slug + ".md"), md);
+  return getPost(post.slug);
+}
+
+export async function getPost(slug: string) {
+  let filepath = path.join(postsPath, slug + ".md");
+  let file = await fs.readFile(filepath);
+  let { attributes, body } = parseFrontMatter(file.toString());
+  invariant(
+    isValidPostAttributes(attributes),
+    `Post ${filepath} is missing attributes`
+  );
+  let html = await processMarkdown(body);
+  return { slug, html, title: attributes.title };
+}
+
+export async function getPosts() {
+  let dir = await fs.readdir(postsPath);
+  return Promise.all(
+    dir.map(async (filename) => {
+      let file = await fs.readFile(path.join(postsPath, filename));
+      let { attributes } = parseFrontMatter(file.toString());
+      invariant(isValidPostAttributes(attributes));
+      return {
+        slug: filename.replace(/\.md$/, ""),
+        title: attributes.title,
+      };
+    })
+  );
+}
