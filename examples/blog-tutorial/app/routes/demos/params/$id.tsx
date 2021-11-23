9bf6b8f85d1aa69b83318411c44cc099adf87443
mechCoder <mechcoder@pop-os.localdomain>
2021-11-23 20:37:03 +0530

    fix error message falling back to default in CatchBoundary of basic and blog eg

--- examples/blog-tutorial/app/routes/demos/params/$id.tsx
+++ examples/blog-tutorial/app/routes/demos/params/$id.tsx
@@ -61,10 +61,12 @@ export function CatchBoundary() {
           Maybe ask the webmaster ({caught.data.webmasterEmail}) for access.
         </p>
       );
+      break;
     case 404:
       message = (
         <p>Looks like you tried to visit a page that does not exist.</p>
       );
+      break;
     default:
       message = (
         <p>
@@ -105,6 +107,6 @@ export function ErrorBoundary({ error }: { error: Error }) {
 
 export let meta: MetaFunction = ({ data }) => {
   return {
-    title: data ? `Param: ${data.param}` : "Oops...",
+    title: data ? `Param: ${data.param}` : "Oops..."
   };
 };
