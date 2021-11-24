bcb26b6565ca12483c3fa1ac6c67802d7c304816
Michaël De Boey <info@michaeldeboey.be>
2021-11-24 14:43:03 +0100

    feat: use `assetsBuildDirectory` instead of deprecated `browserBuildDirectory` config option

--- packages/create-remix/templates/arc/remix.config.js
+++ packages/create-remix/templates/arc/remix.config.js
@@ -3,7 +3,7 @@
  */
 module.exports = {
   appDirectory: "app",
-  browserBuildDirectory: "public/build",
+  assetsBuildDirectory: "public/build",
   publicPath: "/_static/build/",
   serverBuildDirectory: "server/build",
   devServerPort: 8002
