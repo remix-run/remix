4db74a6b25834f9e7f8d5531b8d2c2632959fd4f
Leo Lamprecht <leo@vercel.com>
2021-11-24 15:21:58 +0100

    Improved Vercel template

---
+++ packages/create-remix/templates/vercel/remix.config.js
@@ -0,0 +1,11 @@
+/**
+ * @type {import('@remix-run/dev/config').AppConfig}
+ */
+module.exports = {
+  appDirectory: "app",
+  browserBuildDirectory: "public/build",
+  publicPath: "/build/",
+  serverBuildDirectory: "api/build",
+  devServerPort: process.env.PORT || 3000
+};
+  
