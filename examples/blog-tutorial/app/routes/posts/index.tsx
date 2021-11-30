ca20283508a0931dc1c63fc660b81b16cf117d80
Michaël De Boey <info@michaeldeboey.be>
2021-11-30 19:02:43 +0100

    chore: fix ESLint errors & warnings (#611)

--- examples/blog-tutorial/app/routes/posts/index.tsx
+++ examples/blog-tutorial/app/routes/posts/index.tsx
@@ -1,4 +1,5 @@
 import { Link, useLoaderData } from "remix";
+
 import { getPosts } from "~/post";
 import type { Post } from "~/post";
 
@@ -12,7 +13,7 @@ export default function Posts() {
     <div>
       <h1>Posts</h1>
       <ul>
-        {posts.map((post) => (
+        {posts.map(post => (
           <li key={post.slug}>
             <Link to={post.slug}>{post.title}</Link>
           </li>
