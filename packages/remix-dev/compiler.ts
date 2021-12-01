5c8f458bad46ef13478bb130b7d9081c8e752e22
ziga <zigomir@gmail.com>
2021-12-01 21:26:48 +0100

    Use inline sourcemaps for server build

--- packages/remix-dev/compiler.ts
+++ packages/remix-dev/compiler.ts
@@ -352,7 +352,7 @@ async function createServerBuild(
     bundle: true,
     logLevel: "silent",
     incremental: options.incremental,
-    sourcemap: true,
+    sourcemap: "inline",
     // The server build needs to know how to generate asset URLs for imports
     // of CSS and other files.
     assetNames: "_assets/[name]-[hash]",
