9884bd9e859c8960c362c05803aa2ea572742acf
Chance Strickland <hi@chance.dev>
2021-12-17 15:39:55 -0800

    chore: formatting

--- packages/create-remix/templates/_shared_ts/app/routes/index.tsx
+++ packages/create-remix/templates/_shared_ts/app/routes/index.tsx
@@ -4,12 +4,20 @@ export default function Index() {
       <h1>Welcome to Remix</h1>
       <ul>
         <li>
-          <a target="_blank" href="https://remix.run/tutorials/blog" rel="noreferrer">
+          <a
+            target="_blank"
+            href="https://remix.run/tutorials/blog"
+            rel="noreferrer"
+          >
             15m Quickstart Blog Tutorial
           </a>
         </li>
         <li>
-          <a target="_blank" href="https://remix.run/tutorials/jokes" rel="noreferrer">
+          <a
+            target="_blank"
+            href="https://remix.run/tutorials/jokes"
+            rel="noreferrer"
+          >
             Deep Dive Jokes App Tutorial
           </a>
         </li>
