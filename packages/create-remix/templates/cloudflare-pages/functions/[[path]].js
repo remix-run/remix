049c26921e24f516e66b7a78a9d61cfeefd4f098
Greg Brimble <developer@gregbrimble.com>
2021-12-17 01:38:27 +0000

    Rename handleFetch to handleRequest

--- packages/create-remix/templates/cloudflare-pages/functions/[[path]].js
+++ packages/create-remix/templates/cloudflare-pages/functions/[[path]].js
@@ -3,10 +3,10 @@ import { createPagesFunctionHandler } from "@remix-run/cloudflare-pages";
 // @ts-ignore
 import * as build from "../build";
 
-const handleFetch = createPagesFunctionHandler({
+const handleRequest = createPagesFunctionHandler({
   build
 });
 
 export function onRequest(context) {
-  return handleFetch(context);
+  return handleRequest(context);
 }
