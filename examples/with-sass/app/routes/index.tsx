7fe5114fc794a117d8e2200b1224a0967ab53631
Logan McAnsh <logan@mcan.sh>
2021-12-21 14:18:46 -0500

    chore(examples): add sass example
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

---
+++ examples/with-sass/app/routes/index.tsx
@@ -0,0 +1,36 @@
+export default function Index() {
+  return (
+    <div>
+      <h1>Welcome to Remix</h1>
+      <ul>
+        <li>
+          <a
+            target="_blank"
+            href="https://remix.run/tutorials/blog"
+            rel="noreferrer noopener"
+          >
+            15m Quickstart Blog Tutorial
+          </a>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            href="https://remix.run/tutorials/jokes"
+            rel="noreferrer noopener"
+          >
+            Deep Dive Jokes App Tutorial
+          </a>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            href="https://remix.run/docs"
+            rel="noreferrer noopener"
+          >
+            Remix Docs
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+}
