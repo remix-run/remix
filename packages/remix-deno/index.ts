b9cc66cb7d8469c37eb53c7ddc58cb403fbb9593
Jacob Ebey <jacob.ebey@live.com>
2022-01-06 18:02:19 -0800

    fix: manual re-export

--- packages/remix-deno/index.ts
+++ packages/remix-deno/index.ts
@@ -1,4 +1,49 @@
-export * from "@remix-run/server-runtime";
+export type {
+  ServerBuild,
+  ServerEntryModule,
+  HandleDataRequestFunction,
+  HandleDocumentRequestFunction,
+  CookieParseOptions,
+  CookieSerializeOptions,
+  CookieSignatureOptions,
+  CookieOptions,
+  Cookie,
+  AppLoadContext,
+  AppData,
+  EntryContext,
+  LinkDescriptor,
+  HtmlLinkDescriptor,
+  PageLinkDescriptor,
+  ServerPlatform,
+  ActionFunction,
+  DataFunctionArgs,
+  ErrorBoundaryComponent,
+  HeadersFunction,
+  HtmlMetaDescriptor,
+  LinksFunction,
+  LoaderFunction,
+  MetaDescriptor,
+  MetaFunction,
+  RouteComponent,
+  RouteHandle,
+  RequestHandler,
+  SessionData,
+  Session,
+  SessionStorage,
+  SessionIdStorageStrategy
+} from "@remix-run/server-runtime";
+export {
+  createCookie,
+  isCookie,
+  json,
+  redirect,
+  createRequestHandler,
+  createSession,
+  isSession,
+  createSessionStorage,
+  createCookieSessionStorage,
+  createMemorySessionStorage
+} from "@remix-run/server-runtime";
 
 import { installGlobals } from "./globals";
 
