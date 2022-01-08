725923fa7fcbc32fdefd1d5ff707c5f58a2379fa
Anish Prashun <66175058+Anishpras@users.noreply.github.com>
2022-01-08 19:51:23 +0530

    docs(example): framer-motion (#1403)
    Co-authored-by: Kent C. Dodds <me+github@kentcdodds.com>

---
+++ examples/framer-motion/app/routes/index.tsx
@@ -0,0 +1,13 @@
+import { motion} from "framer-motion";
+export default function Index() {
+
+  return (
+    <div style={{display: 'flex', width: '100%', height: '100vh', justifyContent: 'center'}}>
+      <motion.div
+        animate={{ rotate: 360 }}
+        transition={{ duration: 2 }}>
+          <h1>Welcome to remix!</h1>
+      </motion.div>
+  </div>
+  );}
+
