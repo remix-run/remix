f218dd8bca668ec5be9145cb33bbc62f5841d688
Kent C. Dodds <me+github@kentcdodds.com>
2022-01-08 07:31:35 -0700

    chore: format

--- examples/framer-motion/app/routes/index.tsx
+++ examples/framer-motion/app/routes/index.tsx
@@ -1,13 +1,17 @@
-import { motion} from "framer-motion";
+import { motion } from "framer-motion";
 export default function Index() {
-
   return (
-    <div style={{display: 'flex', width: '100%', height: '100vh', justifyContent: 'center'}}>
-      <motion.div
-        animate={{ rotate: 360 }}
-        transition={{ duration: 2 }}>
-          <h1>Welcome to remix!</h1>
+    <div
+      style={{
+        display: "flex",
+        width: "100%",
+        height: "100vh",
+        justifyContent: "center"
+      }}
+    >
+      <motion.div animate={{ rotate: 360 }} transition={{ duration: 2 }}>
+        <h1>Welcome to remix!</h1>
       </motion.div>
-  </div>
-  );}
-
+    </div>
+  );
+}
