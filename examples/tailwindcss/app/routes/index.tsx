c07af5a808cf9574b143ae25c9f751671b8942a7
juwiragiye <uwiragiyejanvi@gmail.com>
2022-01-07 22:33:31 -0500

    remove tailwind from prisma

--- examples/tailwindcss/app/routes/index.tsx
+++
@@ -1,3 +0,0 @@
-export default function Index() {
-    return <h1 className="text-6xl text-red-700 font-bold">Hello World!</h1>;
-}
--- examples/tailwindcss/tailwind.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  content: ["./app/**/*.{ts,tsx,js,jsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
