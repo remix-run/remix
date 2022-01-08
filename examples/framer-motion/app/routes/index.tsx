8a2f8e178a33c037fb1c9d87717c84650ee58adc
Anish Prashun <66175058+Anishpras@users.noreply.github.com>
2022-01-08 12:40:56 +0530

    changes done

--- examples/framer-motion/app/routes/index.tsx
+++ examples/framer-motion/app/routes/index.tsx
@@ -3,7 +3,7 @@ export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
       <motion.h1 animate={{ scale: 0.5 }} transition={{ duration: 2 }}>
-        Welcome to Remix + Framer Motion
+        Welcome to Remix
       </motion.h1>
       <ul>
         <li>
