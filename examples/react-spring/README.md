ead86cb417b72922c2e4c28568a8f1f08cd86b83
Girish21 <girishkumarv21@gmail.com>
2022-01-08 14:26:16 +0530

    feat: react-spring

---
+++ examples/react-spring/README.md
@@ -0,0 +1,22 @@
+# React Spring
+
+A simple example demonstrating the integration of `react-spring` with Remix.
+
+## Preview
+
+Open this example on [CodeSandbox](https://codesandbox.com):
+
+[![Open in CodeSandbox](https://codesandbox.io/static/img/play-codesandbox.svg)](https://codesandbox.io/s/github/remix-run/remix/tree/main/examples/react-spring)
+
+## Example
+
+This example shows how to animate on mount and animate on different events using `react-spring`.
+
+Relevant files:
+
+[app/routes/index.tsx](./app/routes/index.tsx)
+
+## Related Links
+
+- [remix.run](https://remix.run/)
+- [react-spring](https://react-spring.io/)
---
+++ examples/react-spring/app/root.tsx
@@ -0,0 +1,32 @@
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration
+} from "remix";
+import type { LinksFunction } from "remix";
+
+import styles from "~/styles.css";
+
+export const links: LinksFunction = () => [{ rel: "stylesheet", href: styles }];
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        {process.env.NODE_ENV === "development" && <LiveReload />}
+      </body>
+    </html>
+  );
+}
