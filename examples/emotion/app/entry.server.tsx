9532a96b176d24f0023881ba6d44704c2d528ed0
Helder Burato Berto <helder.burato@gmail.com>
2022-01-13 18:07:46 +0000

    Create Emotion server instance

--- examples/emotion/app/entry.server.tsx
+++ examples/emotion/app/entry.server.tsx
@@ -1,6 +1,16 @@
+import ReactDOMServer from "react-dom/server";
+import { CacheProvider } from "@emotion/react";
 import { renderToString } from "react-dom/server";
 import { RemixServer } from "remix";
+import createEmotionServer from "@emotion/server/create-instance";
+import createCache from "@emotion/cache";
+import StylesContext from "./StylesContext";
 import type { EntryContext } from "remix";
+
+const key = "remix-emotion";
+const cache = createCache({ key });
+const { extractCriticalToChunks, constructStyleTagsFromChunks } =
+  createEmotionServer(cache);
 
 export default function handleRequest(
   request: Request,
@@ -8,8 +18,21 @@ export default function handleRequest(
   responseHeaders: Headers,
   remixContext: EntryContext
 ) {
-  const markup = renderToString(
-    <RemixServer context={remixContext} url={request.url} />
+  const html = renderToString(
+    <CacheProvider value={cache}>
+      <StylesContext.Provider value={null}>
+        <RemixServer context={remixContext} url={request.url} />
+      </StylesContext.Provider>
+    </CacheProvider>
+  );
+
+  const chunks = extractCriticalToChunks(html);
+  const styles = constructStyleTagsFromChunks(chunks);
+
+  const markup = ReactDOMServer.renderToString(
+    <StylesContext.Provider value={styles}>
+      <RemixServer context={remixContext} url={request.url} />
+    </StylesContext.Provider>
   );
 
   responseHeaders.set("Content-Type", "text/html");
