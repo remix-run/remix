e33511195d7c8767bbd51cad36a8ad669832593f
rphlmr <dev@rphlmr.com>
2022-01-20 09:20:29 +0100

    clear local supabase auth session after signed_in

--- examples/remix-auth-supabase/oauth-github/app/routes/oauth.tsx
+++ examples/remix-auth-supabase/oauth-github/app/routes/oauth.tsx
@@ -1,31 +1,30 @@
-import { useEffect } from 'react'
-import type { ActionFunction } from 'remix'
-import { useSubmit } from 'remix'
-import { authenticator } from '~/auth.server'
-import { supabaseClient } from '~/supabase'
+import { useEffect } from "react";
+import type { ActionFunction } from "remix";
+import { useSubmit } from "remix";
+import { authenticator } from "~/auth.server";
+import { supabaseClient } from "~/supabase";
 
-export const action: ActionFunction = async({ request }) => {
-  await authenticator.authenticate('sb-oauth', request, {
-    successRedirect: '/private',
-    failureRedirect: '/login',
-  })
-}
+export const action: ActionFunction = async ({ request }) => {
+  await authenticator.authenticate("sb-oauth", request, {
+    successRedirect: "/private",
+    failureRedirect: "/login"
+  });
+};
 
 export default function OAuth() {
-  const submit = useSubmit()
+  const submit = useSubmit();
 
   useEffect(() => {
     supabaseClient.auth.onAuthStateChange((event, session) => {
-      if (event === 'SIGNED_IN') {
-        const formData = new FormData()
-        formData.append('session', JSON.stringify(session))
+      if (event === "SIGNED_IN") {
+        const formData = new FormData();
+        formData.append("session", JSON.stringify(session));
 
-        submit(formData, { method: 'post' })
+        submit(formData, { method: "post" });
+        supabaseClient.auth.signOut();
       }
-    },
+    });
+  }, [submit]);
 
-    )
-  }, [submit])
-
-  return null
+  return null;
 }
