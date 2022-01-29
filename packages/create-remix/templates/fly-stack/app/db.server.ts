3a38adc9e6c855fbc6258ca276937cf8119883ec
Logan McAnsh <logan@mcan.sh>
2022-01-28 21:51:58 -0500

    add note about prisma global
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- packages/create-remix/templates/fly-stack/app/db.server.ts
+++ packages/create-remix/templates/fly-stack/app/db.server.ts
@@ -6,6 +6,10 @@ declare global {
   var __db__: PrismaClient;
 }
 
+// this is needed because in development we don't want to restart
+// the server with every change, but we want to make sure we don't
+// create a new connection to the DB with every change either.
+// in production we'll have a single connection to the DB.
 if (process.env.NODE_ENV === "production") {
   prisma = new PrismaClient();
 } else {
