c18721262c6de7047c42905aaa11a1ad9ebb899f
Sebastian Davids <sdavids@users.noreply.github.com>
2022-01-30 04:56:56 +0100

    feat: Remove x-powered-by header (#1710) (#1712)
    Signed-off-by: Sebastian Davids <sdavids@gmx.de>

--- packages/create-remix/templates/express/server.js
+++ packages/create-remix/templates/express/server.js
@@ -8,6 +8,9 @@ import * as build from "@remix-run/dev/server-build";
 const app = express();
 app.use(compression());
 
+// http://expressjs.com/en/advanced/best-practice-security.html#at-a-minimum-disable-x-powered-by-header
+app.disable("x-powered-by");
+
 // You may want to be more aggressive with this caching
 app.use(express.static("public", { maxAge: "1h" }));
 
