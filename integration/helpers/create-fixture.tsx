aa03c19b57a8a393a829b253a608801022497731
Jacob Ebey <jacob.ebey@live.com>
2022-02-01 21:36:19 -0800

    removed dup fuc

--- integration/helpers/create-fixture.tsx
+++ integration/helpers/create-fixture.tsx
@@ -304,10 +304,6 @@ export function selectHtml(source: string, selector: string) {
 export function prettyHtml(source: string): string {
   return prettier.format(source, { parser: "html" });
 }
-
-export function js(strings: TemplateStringsArray, ...args: any[]) {
-  return strings.reduce((acc, str, i) => acc + str + (args[i] || ""), "");
-}
   
 // Taken from https://github.com/puppeteer/puppeteer/issues/5328#issuecomment-986175620
 // Seems to work?
