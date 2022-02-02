e38c23a493a38065d4e0badd098c74faeffa4556
Logan McAnsh <logan@mcan.sh>
2022-02-02 14:45:59 -0500

    fix(remix-architect): support binary data responses
    this uses the same detection that architect itself uses
    
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- packages/remix-architect/server.ts
+++ packages/remix-architect/server.ts
@@ -17,6 +17,8 @@ import type {
 } from "@remix-run/server-runtime";
 import { createRequestHandler as createRemixRequestHandler } from "@remix-run/server-runtime";
 import type { Response as NodeResponse } from "@remix-run/node";
+// @ts-expect-error - should we add this directly to our adapter?
+import binaryTypes from "@architect/functions/src/http/helpers/binary-types";
 
 /**
  * A function that returns the value to use as `context` in route `loader` and
@@ -58,31 +60,49 @@ export function createRequestHandler({
       loadContext
     )) as unknown as NodeResponse;
 
-    let cookies: string[] = [];
+    return sendRemixResponse(response, abortController);
+  };
+}
 
-    // Arc/AWS API Gateway will send back set-cookies outside of response headers.
-    for (let [key, values] of Object.entries(response.headers.raw())) {
-      if (key.toLowerCase() === "set-cookie") {
-        for (let value of values) {
-          cookies.push(value);
-        }
+export async function sendRemixResponse(
+  response: NodeResponse,
+  abortController: AbortController
+) {
+  let cookies: string[] = [];
+
+  // Arc/AWS API Gateway will send back set-cookies outside of response headers.
+  for (let [key, values] of Object.entries(response.headers.raw())) {
+    if (key.toLowerCase() === "set-cookie") {
+      for (let value of values) {
+        cookies.push(value);
       }
     }
+  }
 
-    if (cookies.length) {
-      response.headers.delete("set-cookie");
-    }
+  if (cookies.length) {
+    response.headers.delete("set-cookie");
+  }
 
-    if (abortController.signal.aborted) {
-      response.headers.set("Connection", "close");
-    }
+  if (abortController.signal.aborted) {
+    response.headers.set("Connection", "close");
+  }
 
-    return {
-      statusCode: response.status,
-      headers: Object.fromEntries(response.headers),
-      cookies,
-      body: await response.text()
-    };
+  let isBinary: boolean = false;
+  let contentType = response.headers.get("content-type");
+  if (contentType) {
+    isBinary = binaryTypes.includes(contentType);
+  }
+
+  let body = isBinary
+    ? (await bufferStream(response.body)).toString()
+    : await response.text();
+
+  return {
+    statusCode: response.status,
+    headers: Object.fromEntries(response.headers),
+    cookies,
+    body,
+    isBase64Encoded: isBinary
   };
 }
 
@@ -122,5 +142,16 @@ export function createRemixRequest(
         : event.body,
     abortController,
     signal: abortController?.signal
+  });
+}
+
+function bufferStream(stream: NodeJS.ReadableStream): Promise<Buffer> {
+  return new Promise((accept, reject) => {
+    let chunks: Array<any> = [];
+
+    stream
+      .on("error", reject)
+      .on("data", chunk => chunks.push(chunk))
+      .on("end", () => accept(Buffer.concat(chunks)));
   });
 }
