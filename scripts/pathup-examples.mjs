d56c1a21dc8d0a1020928b44f37ac100f0f79a21
Ryan Florence <rpflorence@gmail.com>
2022-02-01 23:00:37 -0700

    chore: rename all examples to their folder name
    jest complains about their names, also just seems good

---
+++ scripts/pathup-examples.mjs
@@ -0,0 +1,17 @@
+import fs from "fs/promises";
+import path from "path";
+
+let dirPath = path.join(process.cwd(), "examples");
+let dir = await fs.readdir(dirPath);
+
+dir.forEach(async name => {
+  let fullPath = path.join(dirPath, name);
+  let stat = await fs.stat(fullPath);
+  if (stat.isDirectory()) {
+    let pkgPath = path.join(fullPath, "package.json");
+    let pkg = await fs.readFile(pkgPath);
+    let obj = JSON.parse(pkg);
+    obj.name = name;
+    await fs.writeFile(pkgPath, JSON.stringify(obj, null, 2) + "\n");
+  }
+});
