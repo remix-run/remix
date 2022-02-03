2722a5cb98514814f3dc647d96423a344e1da03b
Stratulat Alexandru <alex@sandulat.com>
2022-02-03 21:02:09 +0200

    docs(examples): Add routes-gen example (#1473)

---
+++ examples/routes-gen/app/routes/products/index.tsx
@@ -0,0 +1,46 @@
+import { Link } from "react-router-dom";
+import { LoaderFunction, useLoaderData } from "remix";
+import { route } from "routes-gen";
+
+type Post = {
+  id: string;
+  title: string;
+};
+
+const posts: Post[] = [
+  {
+    id: "1",
+    title: "Cool Product",
+  },
+  {
+    id: "2",
+    title: "Best Product",
+  },
+];
+
+export const loader: LoaderFunction = () => {
+  return posts;
+};
+
+export default function Products() {
+  const data = useLoaderData<Post[]>();
+
+  return (
+    <main>
+      <h2>Products</h2>
+      <ul>
+        {data.map((product) => (
+          <li key={product.id}>
+            <Link
+              to={route("/products/:productId", {
+                productId: product.id,
+              })}
+            >
+              {product.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
+}
