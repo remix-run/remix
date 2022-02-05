9bb17b4f718e1f0aa7ff92e0a4014d971bf9a871
chancestrickland <chance.strickland@gmail.com>
2022-02-05 09:53:54 -0300

    break script into 3 phases

--- scripts/release.js
+++ scripts/release.js
@@ -8,6 +8,8 @@ const {
   prompt
 } = require("./utils");
 
+const releaseTypes = ["patch", "minor", "major"];
+
 run(process.argv.slice(2)).then(
   () => {
     process.exit(0);
@@ -18,55 +20,143 @@ run(process.argv.slice(2)).then(
   }
 );
 
+/**
+ * @param {string[]} args
+ */
 async function run(args) {
-  let givenVersion = args[0];
-  let prereleaseId = args[1];
-
   ensureCleanWorkingDirectory();
 
-  // 1. Get the next version number
-  let currentVersion = await getPackageVersion("remix");
-  let nextVersion = semver.valid(givenVersion);
-  if (nextVersion == null) {
-    nextVersion = getNextVersion(currentVersion, givenVersion, prereleaseId);
+  /** @type {string | undefined} */
+  let phase;
+  /** @type {string | undefined} */
+  let givenVersion;
+  /** @type {string | undefined} */
+  let nextVersion;
+
+  // Validate args and get the next version number
+  if (
+    args.length === 1 &&
+    (releaseTypes.includes(args[0]) || semver.valid(args[0]))
+  ) {
+    phase = "start";
+    givenVersion = args[0];
+  } else {
+    phase = args[0];
+    givenVersion = args[1];
   }
-  let versionTag = "v" + nextVersion;
-  let releaseBranch = `release/${versionTag}`;
 
-  let initialBranch = getCurrentBranch();
+  let allTags = getAllTags();
+  let currentBranch = getCurrentBranch();
+  let gitArgs = { tags: allTags, initialBranch: currentBranch };
 
-  // 2. Confirm the next version number
+  switch (phase) {
+    case "start": {
+      nextVersion = await initStart(givenVersion, gitArgs);
+      break;
+    }
+    case "bump": {
+      nextVersion = await initBump(gitArgs);
+      break;
+    }
+    case "finish": {
+      nextVersion = await initFinish(gitArgs);
+      break;
+    }
+    default:
+      throw Error(`Invalid argument. Usage:
+
+  $ yarn release [start | bump | finish] [patch | minor | major]`);
+  }
+
+  if (versionExists(allTags, nextVersion)) {
+    throw Error(`Version ${nextVersion} has already been released.`);
+  }
+
   let answer = await prompt(
     `Are you sure you want to release version ${chalk.bold(nextVersion)}? [Yn] `
   );
   if (answer === false) return 0;
 
-  // 3. Ensure that non-experimental releases are created from the dev branch
-  if (!isExperimentalRelease(nextVersion)) {
-    if (initialBranch !== "dev") {
-      throw Error("Releases should be created from the dev branch.");
+  switch (phase) {
+    case "start": {
+      await execStart(nextVersion);
+      break;
     }
-
-    // 3a. Pull from the origin/dev and rebase commits as needed
-    try {
-      let resp = execSync(`git pull --rebase origin dev`).toString();
-      if (hasMergeConflicts(resp)) {
-        let answer = await prompt(
-          `Merge conflicts detected. Resolve all conflicts and commit the changes before resuming the process.
-        ${chalk.bold("Press Y to continue or N to cancel the release.")}`
-        );
-        if (answer === false) return 0;
-      } else if (mergeFailed(resp)) {
-        console.error(chalk.red("Merge failed.\n"));
-        throw Error(resp);
-      }
-    } catch (e) {
-      console.error(chalk.red("Error rebasing to origin/dev"));
-      throw e;
+    case "bump": {
+      await execBump(nextVersion, gitArgs);
+      break;
+    }
+    case "finish": {
+      await execFinish(nextVersion, gitArgs);
+      break;
     }
   }
 
-  // 4. Checkout new release branch
+  let versionTag = getVersionTag(nextVersion);
+
+  console.log(
+    chalk.green(
+      `Remix version ${nextVersion} is now ready to release. To trigger the release process, create a new release in GitHub from the ${versionTag} tag:
+  - Navigate to https://github.com/remix-run/remix/releases/new
+  - Select ${chalk.bold(versionTag)} from the "Choose a tag" menu
+  - Draft the release notes
+  - If this is a pre-release, be sure to select the ${chalk.bold(
+    "This is a pre-release"
+  )} checkbox
+  - Click ${chalk.bold("Publish release")}
+  Once the CI is complete, the new release will be published to npm. 🥳`
+    )
+  );
+}
+
+/**
+ * @param {string} givenVersion
+ * @param {{ tags: string[]; initialBranch: string }} git
+ * @returns {Promise<string>}
+ */
+async function initStart(givenVersion, git) {
+  ensureDevBranch(git.initialBranch);
+  let nextVersion = semver.valid(givenVersion);
+  if (nextVersion == null) {
+    nextVersion = getNextVersion(
+      await getPackageVersion("remix"),
+      givenVersion
+    );
+  }
+  return nextVersion;
+}
+
+/**
+ * @param {{ tags: string[]; initialBranch: string }} git
+ * @returns {Promise<string>}
+ */
+async function initBump(git) {
+  ensureReleaseBranch(git.initialBranch);
+  let versionFromBranch = getVersionFromReleaseBranch(git.initialBranch);
+  let currentVersion = git.tags
+    .filter(tag => tag.startsWith("v" + versionFromBranch))
+    .sort((a, b) => (a > b ? -1 : a < b ? 1 : 0))[0];
+  let nextVersion = semver.inc(currentVersion, "prerelease");
+  return nextVersion;
+}
+
+/**
+ * @param {{ tags: string[]; initialBranch: string }} git
+ * @returns {Promise<string>}
+ */
+async function initFinish(git) {
+  ensureReleaseBranch(git.initialBranch);
+  let nextVersion = getVersionFromReleaseBranch(git.initialBranch);
+  return nextVersion;
+}
+
+/**
+ * @param {string} nextVersion
+ */
+async function execStart(nextVersion) {
+  let releaseBranch = getReleaseBranch(nextVersion);
+
+  await gitPull("dev");
   try {
     checkoutNewBranch(releaseBranch);
   } catch (e) {
@@ -77,147 +167,110 @@ async function run(args) {
     );
   }
 
-  // 5. Merge updates from the main branch for stable releases
-  if (!isPreRelease(nextVersion)) {
-    execSync(`git checkout main`);
-    try {
-      let resp = execSync(`git pull --rebase origin main`).toString();
-      if (hasMergeConflicts(resp)) {
-        let answer = await prompt(
-          `Merge conflicts detected. Resolve all conflicts and commit the changes before resuming the process.
-    ${chalk.bold("Press Y to continue or N to cancel the release.")}`
-        );
-        if (answer === false) return 0;
-      } else if (mergeFailed(resp)) {
-        console.error(chalk.red("Merge failed.\n"));
-        throw Error(resp);
-      }
-    } catch (e) {
-      console.error(chalk.red("Rebasing error when pulling from origin/main"));
-      throw e;
-    }
+  await gitMerge("main", releaseBranch, { pullFirst: true });
+  incrementVersion(nextVersion);
+  execSync(`git push origin ${releaseBranch} --follow-tags`);
+}
 
-    // 5a. Return to the release branch and merge changes from main
-    execSync(`git checkout ${releaseBranch}`);
-    let resp = execSync(`git merge main`).toString();
+/**
+ * @param {string} nextVersion
+ * @param {{ tags: string[]; initialBranch: string }} git
+ */
+async function execBump(nextVersion, git) {
+  ensureReleaseBranch(git.initialBranch);
+  await gitMerge("main", git.initialBranch, { pullFirst: true });
+  incrementVersion(nextVersion);
+  execSync(`git push origin ${git.initialBranch} --follow-tags`);
+}
+
+/**
+ * @param {string} nextVersion
+ * @param {{ tags: string[]; initialBranch: string }} git
+ */
+async function execFinish(nextVersion, git) {
+  ensureReleaseBranch(git.initialBranch);
+  await gitMerge(git.initialBranch, "main");
+  incrementVersion(nextVersion);
+  await gitMerge(git.initialBranch, "dev");
+}
+
+/**
+ * @param {string} version
+ */
+function incrementVersion(version) {
+  return execSync(`yarn run version ${version}`);
+}
+
+/**
+ * @param {string} from
+ * @param {string} to
+ * @param {{ pullFirst?: boolean }} [opts]
+ */
+async function gitMerge(from, to, opts = {}) {
+  execSync(`git checkout ${from}`);
+  if (opts.pullFirst) {
+    await gitPull(from);
+  }
+  execSync(`git checkout ${to}`);
+  let resp = execSync(`git merge ${from}`).toString();
+  if (hasMergeConflicts(resp)) {
+    let answer = await prompt(
+      `Merge conflicts detected. Resolve all conflicts and commit the changes before resuming the process.
+          ${chalk.bold("Press Y to continue or N to cancel the release.")}`
+    );
+    if (answer === false) return 0;
+  } else if (mergeFailed(resp)) {
+    console.error(chalk.red("Merge failed.\n"));
+    throw Error(resp);
+  }
+}
+
+/**
+ * @param {string} branch
+ * @returns
+ */
+async function gitPull(branch) {
+  try {
+    let resp = execSync(`git pull --rebase origin ${branch}`).toString();
     if (hasMergeConflicts(resp)) {
       let answer = await prompt(
         `Merge conflicts detected. Resolve all conflicts and commit the changes before resuming the process.
-        ${chalk.bold("Press Y to continue or N to cancel the release.")}`
+
+    ${chalk.bold("Press Y to continue or N to cancel the release.")}`
       );
       if (answer === false) return 0;
     } else if (mergeFailed(resp)) {
       console.error(chalk.red("Merge failed.\n"));
       throw Error(resp);
     }
+  } catch (e) {
+    console.error(chalk.red(`Error rebasing to origin/${branch}`));
+    throw e;
   }
-
-  // 6. Run the version script to update all package versions and create tag
-  execSync(`yarn run version ${nextVersion} ${prereleaseId ?? ""}`);
-
-  // 7. Push the release branch and tags to GitHub. All releases will be
-  //    triggered from there.
-  execSync(`git push origin ${releaseBranch} --follow-tags`);
-
-  console.log(
-    chalk.green(
-      `Remix version ${nextVersion} is now ready to release. To trigger the release process, create a new release in GitHub from the ${versionTag} tag:
-
-  - Navigate to https://github.com/remix-run/remix/releases/new
-  - Select ${chalk.bold(versionTag)} from the "Choose a tag" menu
-  - Draft the release notes
-  - If this is a pre-release, be sure to select the ${chalk.bold(
-    "This is a pre-release"
-  )} checkbox
-  - Click ${chalk.bold("Publish release")}
-
-Once the CI is complete, the new release will be published to npm. 🥳`
-    )
-  );
-
-  // 8. Optionally, merge new release branch back into dev. This should usually
-  //    be done but may be skipped in some cases (hotfixes, etc.)
-  let mergeToDev = await prompt(
-    `Do you want to merge ${releaseBranch} into ${chalk.bold("dev")}? [Yn]`
-  );
-  if (mergeToDev) {
-    execSync(`git checkout dev`);
-    execSync(`git merge ${releaseBranch}`);
-
-    // For simplicity, do not push back to origin/dev automatically. If the
-    // remote dev branch has changed since we started the release process we
-    // should handle this manually.
-    console.log(
-      `Successfully merged ${releaseBranch} into dev. Be sure to push changes to origin/dev`
-    );
-  }
-
-  // 9. Optionally, merge new release branch back into main. Only offered for
-  //    stable releases, as main should reflect the latest stable version.
-  if (!isPreRelease(nextVersion)) {
-    let mergeToMain = await prompt(
-      `Do you want to merge ${releaseBranch} into ${chalk.bold("main")}? [Yn]`
-    );
-    if (mergeToMain) {
-      execSync(`git checkout main`);
-      execSync(`git merge ${releaseBranch}`);
-
-      // For simplicity, do not push back to origin/main automatically. If the
-      // remote main branch has changed since we started the release process we
-      // should handle this manually.
-      console.log(
-        `Successfully merged ${releaseBranch} into main. Be sure to push changes to origin/main`
-      );
-    }
-  }
-
-  // 10. Return to the initial branch before exit
-  execSync(`git checkout ${initialBranch}`);
 }
 
+/**
+ * @param {string} currentVersion
+ * @param {string} givenVersion
+ * @param {string | undefined} [prereleaseId]
+ */
 function getNextVersion(currentVersion, givenVersion, prereleaseId = "pre") {
   if (givenVersion == null) {
-    throw Error("Missing next version. Usage: node release.js [nextVersion]");
+    throw Error(
+      "Missing next version. Usage: node scripts/release.js start [nextVersion]"
+    );
   }
-
-  let nextVersion;
-  if (givenVersion === "experimental") {
-    let hash = execSync(`git rev-parse --short HEAD`).toString().trim();
-    nextVersion = `0.0.0-experimental-${hash}`;
-  } else {
-    nextVersion = semver.inc(currentVersion, givenVersion, prereleaseId);
-  }
-
+  // @ts-ignore
+  let nextVersion = semver.inc(currentVersion, givenVersion, prereleaseId);
   if (nextVersion == null) {
     throw Error(`Invalid version specifier: ${givenVersion}`);
   }
-
   return nextVersion;
 }
 
 function getCurrentBranch() {
   let output = execSync("git rev-parse --abbrev-ref HEAD").toString().trim();
   return output;
-}
-
-/**
- * @param {string} version
- * @returns {boolean}
- */
-function isExperimentalRelease(version) {
-  return version.includes("experimental");
-}
-
-/**
- * @param {string} version
- * @returns {boolean}
- */
-function isPreRelease(version) {
-  return (
-    isExperimentalRelease(version) ||
-    version.includes("alpha") ||
-    version.includes("beta")
-  );
 }
 
 /**
@@ -247,3 +300,55 @@ function checkoutNewBranch(branch) {
     throw Error(`Branch ${chalk.bold(output)} already exists.`);
   }
 }
+
+function getAllTags() {
+  return execSync("git tag --list").toString().trim().split("\n");
+}
+
+/**
+ * @param {string} branch
+ * @returns {"dev"}
+ */
+function ensureDevBranch(branch) {
+  if (branch !== "dev") {
+    throw Error("Releases should be created from the dev branch.");
+  }
+  return branch;
+}
+
+/**
+ * @param {string} branch
+ */
+function ensureReleaseBranch(branch) {
+  let version = getVersionFromReleaseBranch(branch);
+  if (version == null || !semver.valid(version)) {
+    throw Error(
+      "You must be on a valid release branch when continuing the release process."
+    );
+  }
+}
+
+/**
+ * @param {string} branch
+ * @returns {string | undefined}
+ */
+const getVersionFromReleaseBranch = branch => branch.split("/")[1]?.slice(1);
+
+/**
+ * @param {string} version
+ */
+const getVersionTag = version => (version.startsWith("v") ? "" : "v") + version;
+
+/**
+ * @param {string} version
+ */
+const getReleaseBranch = version =>
+  `release/${getVersionTag(
+    version.includes("-") ? version.slice(0, version.indexOf("-")) : version
+  )}`;
+
+/**
+ * @param {string[]} tags
+ * @param {string} version
+ */
+const versionExists = (tags, version) => tags.includes(getVersionTag(version));
