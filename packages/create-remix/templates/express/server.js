40216cdf83593b53e1f8d98b936e4662238787b6
Logan McAnsh <logan@mcan.sh>
2022-02-10 00:42:11 -0500

    fix(create-remix/express): update caching to be dynamic based on path (#1861)
    otherwise it was always 1 hour canary.discord.com/channels/770287896669978684/777863745321304104/941061633000763444
    
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- packages/create-remix/templates/express/server.js
+++ packages/create-remix/templates/express/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import compression from "compression";
 import morgan from "morgan";
@@ -5,17 +6,30 @@ import { createRequestHandler } from "@remix-run/express";
 
 import * as build from "@remix-run/dev/server-build";
 
+const PUBLIC_DIR = path.join(process.cwd(), "public");
+const BROWSER_BUILD_DIR = "/build/";
+
 const app = express();
 app.use(compression());
 
 // http://expressjs.com/en/advanced/best-practice-security.html#at-a-minimum-disable-x-powered-by-header
 app.disable("x-powered-by");
 
-// You may want to be more aggressive with this caching
-app.use(express.static("public", { maxAge: "1h" }));
-
-// Remix fingerprints its assets so we can cache forever
-app.use(express.static("public/build", { immutable: true, maxAge: "1y" }));
+app.use(
+  express.static("public", {
+    setHeaders(res, pathname) {
+      const relativePath = pathname.replace(PUBLIC_DIR, "");
+      res.setHeader(
+        "Cache-Control",
+        relativePath.startsWith(BROWSER_BUILD_DIR)
+          ? // Remix fingerprints its assets so we can cache forever
+            "public, max-age=31536000, immutable"
+          : // You may want to be more aggressive with this caching
+            "public, max-age=3600"
+      );
+    }
+  })
+);
 
 app.use(morgan("tiny"));
 app.all("*", createRequestHandler({ build, mode: process.env.NODE_ENV }));
