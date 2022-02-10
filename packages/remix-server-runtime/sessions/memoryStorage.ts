f4462fa9ddafbd762d5b7692ec5c006f82b87e90
Chris Shepherd <chris@chrisshepherd.me>
2022-02-10 22:08:16 +0000

    fix(server-runtime): keep memory storage data across cache clears

--- packages/remix-server-runtime/sessions/memoryStorage.ts
+++ packages/remix-server-runtime/sessions/memoryStorage.ts
@@ -5,6 +5,21 @@ import type {
   CreateSessionStorageFunction,
 } from "../sessions";
 
+let memoryStorageData: {
+  map: Map<string, { data: SessionData; expires?: Date }>;
+  uniqueId: number;
+};
+
+declare global {
+  // eslint-disable-next-line prefer-let/prefer-let
+  var __memoryStorageData:
+    | {
+        map: Map<string, { data: SessionData; expires?: Date }>;
+        uniqueId: number;
+      }
+    | undefined;
+}
+
 interface MemorySessionStorageOptions {
   /**
    * The Cookie used to store the session id on the client, or options used
@@ -31,35 +46,47 @@ export const createMemorySessionStorageFactory =
     createSessionStorage: CreateSessionStorageFunction
   ): CreateMemorySessionStorageFunction =>
   ({ cookie } = {}) => {
-    let uniqueId = 0;
-    let map = new Map<string, { data: SessionData; expires?: Date }>();
+    if (process.env.NODE_ENV === "production") {
+      memoryStorageData = {
+        map: new Map<string, { data: SessionData; expires?: Date }>(),
+        uniqueId: 0,
+      };
+    } else {
+      if (!global.__memoryStorageData) {
+        global.__memoryStorageData = {
+          map: new Map<string, { data: SessionData; expires?: Date }>(),
+          uniqueId: 0,
+        };
+      }
+      memoryStorageData = global.__memoryStorageData;
+    }
 
     return createSessionStorage({
       cookie,
       async createData(data, expires) {
-        let id = (++uniqueId).toString();
-        map.set(id, { data, expires });
+        let id = (++memoryStorageData.uniqueId).toString();
+        memoryStorageData.map.set(id, { data, expires });
         return id;
       },
       async readData(id) {
-        if (map.has(id)) {
-          let { data, expires } = map.get(id)!;
+        if (memoryStorageData.map.has(id)) {
+          let { data, expires } = memoryStorageData.map.get(id)!;
 
           if (!expires || expires > new Date()) {
             return data;
           }
 
           // Remove expired session data.
-          if (expires) map.delete(id);
+          if (expires) memoryStorageData.map.delete(id);
         }
 
         return null;
       },
       async updateData(id, data, expires) {
-        map.set(id, { data, expires });
+        memoryStorageData.map.set(id, { data, expires });
       },
       async deleteData(id) {
-        map.delete(id);
+        memoryStorageData.map.delete(id);
       },
     });
   };
