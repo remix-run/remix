3894224d7dfd8d78b0823d0a054986a184bb8850
Abdullah Mzaien <xa300600@hotmail.com>
2022-02-14 10:48:12 +0300

    Update examples/next-ui/app/root.tsx
    remove unneeded code
    
    Co-authored-by: Michaël De Boey <info@michaeldeboey.be>

--- examples/next-ui/app/root.tsx
+++ examples/next-ui/app/root.tsx
@@ -30,7 +30,7 @@ function Document({
         {children}
         <ScrollRestoration />
         <Scripts />
-        {process.env.NODE_ENV === "development" && <LiveReload />}
+        <LiveReload />
       </body>
     </html>
   );
