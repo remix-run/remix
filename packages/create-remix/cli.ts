72fefe80c8e47aa5952ab6bcf79e7cbbe6465186
Jacob Ebey <jacob.ebey@live.com>
2022-02-16 03:12:09 +0700

    chore: add experimental label for deno

--- packages/create-remix/cli.ts
+++ packages/create-remix/cli.ts
@@ -124,7 +124,7 @@ async function run() {
         { name: "Vercel", value: "vercel" },
         { name: "Cloudflare Pages", value: "cloudflare-pages" },
         { name: "Cloudflare Workers", value: "cloudflare-workers" },
-        { name: "Deno", value: "deno" }
+        { name: "Deno (experimental)", value: "deno" }
       ]
     },
     {
