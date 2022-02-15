1ebfa284a10c4685e4b051135fbad9f871daee8d
Helder Burato Berto <helder.burato@gmail.com>
2022-02-15 09:30:34 +0000

    Trigger catch/error boundary

--- examples/emotion/app/routes/jokes/$jokeId.tsx
+++ examples/emotion/app/routes/jokes/$jokeId.tsx
@@ -1,8 +1,22 @@
+import type { LoaderFunction } from "remix";
+import { useCatch } from "remix";
+
 import styled from "@emotion/styled";
 
 const Wrapper = styled("div")`
   background-color: #e6e6e6;
 `;
+
+const ErrorWrapper = styled("div")`
+  background-color: #ff0000;
+  padding: 1em;
+`;
+
+export const loader: LoaderFunction = async () => {
+  throw new Response("What a joke! Not found.", {
+    status: 404
+  });
+};
 
 export default function JokeRoute() {
   return (
@@ -13,5 +27,19 @@ export default function JokeRoute() {
         at it.
       </p>
     </Wrapper>
+  );
+}
+
+export function CatchBoundary() {
+  const caught = useCatch();
+
+  return (
+    <ErrorWrapper>
+      <h1>Caught</h1>
+      <p>Status: {caught.status}</p>
+      <pre>
+        <code>{JSON.stringify(caught.data, null, 2)}</code>
+      </pre>
+    </ErrorWrapper>
   );
 }
