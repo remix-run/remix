398a18a5b8160c2862f066bdb0db9ff2af86af1c
Kent C. Dodds <me+github@kentcdodds.com>
2022-02-17 17:03:20 -0700

    docs(examples): update jokes app

--- examples/jokes/app/routes/jokes/index.tsx
+++ examples/jokes/app/routes/jokes/index.tsx
@@ -1,17 +1,28 @@
+import type { LoaderFunction } from "remix";
 import { useLoaderData, Link, useCatch } from "remix";
 import type { Joke } from "@prisma/client";
 
 import { db } from "~/utils/db.server";
+import { getUserId } from "~/utils/session.server";
 
 type LoaderData = { randomJoke: Joke };
 
-export const loader = async () => {
+export const loader: LoaderFunction = async ({ request }) => {
+  const userId = await getUserId(request);
   const count = await db.joke.count();
   const randomRowNumber = Math.floor(Math.random() * count);
-  const [randomJoke] = await db.joke.findMany({
-    take: 1,
-    skip: randomRowNumber,
-  });
+
+  // in the official deployed version of the app, we don't want to deploy
+  // a site with unmoderated content, so we only show users their own jokes
+  const [randomJoke] = userId
+    ? await db.joke.findMany({
+        take: 1,
+        skip: randomRowNumber,
+        where: {
+          jokesterId: userId,
+        },
+      })
+    : [];
   if (!randomJoke) {
     throw new Response("No jokes to be found!", { status: 404 });
   }
@@ -37,7 +48,15 @@ export function CatchBoundary() {
   if (caught.status === 404) {
     return (
       <div className="error-container">
-        <p>There are no jokes to display.</p>
+        <p>
+          There are no jokes to display.
+          <br />
+          <small>
+            Note: this is the deployed version of the jokes app example and
+            because we don't want to show you unmoderated content, we only
+            display jokes you create in this version.
+          </small>
+        </p>
         <Link to="new">Add your own</Link>
       </div>
     );
