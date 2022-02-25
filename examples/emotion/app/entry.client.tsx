3317a1f5c9df135e2cec852853f17dbc52ca7f13
Helder Burato Berto <helder.burato@gmail.com>
2022-02-25 20:30:09 +0000

    docs(examples): Add Emotion example  (#1485)
    Co-authored-by: Michaël De Boey <info@michaeldeboey.be>

---
+++ examples/emotion/app/entry.client.tsx
@@ -0,0 +1,31 @@
+import { hydrate } from "react-dom";
+import { RemixBrowser } from "remix";
+import React, { useState } from "react";
+import { CacheProvider } from "@emotion/react";
+import createEmotionCache from "./styles/createEmotionCache";
+import ClientStyleContext from "./styles/client.context";
+
+interface ClientCacheProviderProps {
+  children: React.ReactNode;
+}
+
+function ClientCacheProvider({ children }: ClientCacheProviderProps) {
+  const [cache, setCache] = useState(createEmotionCache());
+
+  function reset() {
+    setCache(createEmotionCache());
+  }
+
+  return (
+    <ClientStyleContext.Provider value={{ reset }}>
+      <CacheProvider value={cache}>{children}</CacheProvider>
+    </ClientStyleContext.Provider>
+  );
+}
+
+hydrate(
+  <ClientCacheProvider>
+    <RemixBrowser />
+  </ClientCacheProvider>,
+  document
+);
