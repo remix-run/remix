2ca18898cbc3aa475c7f8d3740ff60268420f191
Michaël De Boey <info@michaeldeboey.be>
2022-02-28 14:55:07 +0100

    chore(create-remix): update shared templates

--- packages/create-remix/templates/_shared_js/remix.config.js
+++ packages/create-remix/templates/_shared_js/remix.config.js
@@ -3,4 +3,8 @@
  */
 module.exports = {
   ignoredRouteFiles: [".*"],
+  // appDirectory: "app",
+  // assetsBuildDirectory: "public/build",
+  // serverBuildPath: "build/index.js",
+  // publicPath: "/build/",
 };
