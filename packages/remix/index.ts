5a7b281d0e93ad5eb0cb61687f3f95fd8223ab8f
Andrew Jefferson <8148776+eastlondoner@users.noreply.github.com>
2022-03-03 17:02:46 +0100

    fix(remix): export a type so that remix/index.d.ts is not empty
    Closes: #2031
    
    Co-authored-by: Andrew Jefferson <andy@mandadata.com>

--- packages/remix/index.ts
+++ packages/remix/index.ts
@@ -1,1 +1,8 @@
-throw new Error("Did you forget to run `remix setup` for your platform?");
+// This class exists to prevent https://github.com/remix-run/remix/issues/2031 from occuring
+export class RemixNotSetupError extends Error {
+    constructor() {
+        super("Did you forget to run `remix setup` for your platform?")
+    }
+}
+
+throw new RemixNotSetupError();
