0a520c5551b0547c8320db27c177937e186d961d
Michaël De Boey <info@michaeldeboey.be>
2022-03-03 17:00:48 +0100

    docs(examples): fix ESLint warnings (#2186)

--- examples/graphql-api/app/routes/api/characters.tsx
+++ examples/graphql-api/app/routes/api/characters.tsx
@@ -1,7 +1,7 @@
 import type { ApolloError } from "apollo-server-errors";
 import type { LoaderFunction } from "remix";
 
-import { fetchFromGraphQL } from "~/utils/index";
+import { fetchFromGraphQL, gql } from "~/utils";
 import type { Characters } from "~/generated/types";
 
 export type LoaderData = {
@@ -13,7 +13,7 @@ export const loader: LoaderFunction = async (args): Promise<LoaderData> => {
   const { params } = args;
   const { page = 1 } = params;
 
-  const getCharactersQuery = `
+  const getCharactersQuery = gql`
     fragment CharacterFields on Character {
       gender
       id
