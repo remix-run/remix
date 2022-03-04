895a05cfc9efa68a7b00939bf3dec67425ca86aa
Jonas Auer <jonas.auer.94@gmail.com>
2022-03-04 19:30:48 +0100

    docs(examples): Add Yarn PnP example

---
+++ examples/yarn-pnp/app/routes/index.tsx
@@ -0,0 +1,18 @@
+export default function Index() {
+  return (
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+      <h1>Welcome to Remix</h1>
+      <p>
+        This example is using{" "}
+        <a
+          target="_blank"
+          href="https://next.yarnpkg.com/features/pnp"
+          rel="noreferrer"
+        >
+          Yarn PnP
+        </a>
+        !
+      </p>
+    </div>
+  );
+}
