7449ce30df177e1123a8cc6118baf1158871fad6
Chance Strickland <hi@chance.dev>
2022-03-04 18:12:04 -0800

    types

--- packages/remix-server-runtime/entry.ts
+++ packages/remix-server-runtime/entry.ts
@@ -39,7 +39,10 @@ interface CssModulesResults {
 
 interface CssModuleFileContents {
   css: string;
+  sourceMap: string | null;
   json: CssModuleClassMap;
+  moduleExports: CssModuleExports;
+  dependencies: CssModuleDependency[];
 }
 
 type CssModuleFileMap = Record<string, CssModuleFileContents>;
@@ -64,4 +67,80 @@ export function createEntryRouteModules(
     memo[routeId] = manifest[routeId].module;
     return memo;
   }, {} as RouteModules<EntryRouteModule>);
+}
+
+/**
+ * Many of these types were copied from @parcel/css and modified slightly for
+ * our needs. Some of this data will end up in the assets manifest but we don't
+ * want more than we need.
+ */
+
+type CssModuleExports = {
+  [name: string]: CssModuleExport;
+};
+
+interface CssModuleExport {
+  name: string;
+  composes: CssModuleReference[];
+}
+
+type CssModuleReference =
+  | LocalCssModuleReference
+  | GlobalCssModuleReference
+  | DependencyCssModuleReference;
+
+interface LocalCssModuleReference {
+  type: "local";
+  name: string;
+}
+
+interface GlobalCssModuleReference {
+  type: "global";
+  name: string;
+}
+
+interface DependencyCssModuleReference {
+  type: "dependency";
+  name: string;
+  /** The dependency specifier for the referenced file. */
+  specifier: string;
+}
+
+type CssModuleDependency = CssModuleImportDependency | CssModuleUrlDependency;
+
+interface CssModuleImportDependency {
+  type: "import";
+  /** The url of the `@import` dependency. */
+  url: string;
+  /** The media query for the `@import` rule. */
+  media: string | null;
+  /** The `supports()` query for the `@import` rule. */
+  supports: string | null;
+  /** The source location where the `@import` rule was found. */
+  loc: CssModuleSourceLocation;
+}
+
+interface CssModuleUrlDependency {
+  type: "url";
+  /** The url of the dependency. */
+  url: string;
+  /** The source location where the `url()` was found. */
+  loc: CssModuleSourceLocation;
+  /** The placeholder that the url was replaced with. */
+  placeholder: string;
+}
+
+interface CssModuleSourceLocation {
+  /** The file path in which the dependency exists. */
+  filePath: string;
+  /** The start location of the dependency. */
+  start: {
+    line: number;
+    column: number;
+  };
+  /** The end location (inclusive) of the dependency. */
+  end: {
+    line: number;
+    column: number;
+  };
 }
