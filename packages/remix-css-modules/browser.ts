e0085c52ea444a59d5e8e5fc66ea52b2541b0ed9
Chance Strickland <hi@chance.dev>
2022-03-04 16:57:36 -0800

    fix browser import

--- packages/remix-css-modules/browser.ts
+++ packages/remix-css-modules/browser.ts
@@ -1,3 +1,3 @@
-import type { AssetsManifest } from "@remix-run/dev";
+import type { AssetsManifest } from "@remix-run/dev/assets-manifest";
 let assetsManifest: AssetsManifest = (window as any).__remixManifest;
 export default assetsManifest.cssModules?.globalStylesheetFileUrl;
