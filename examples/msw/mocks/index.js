2f564d3e104344b5d08d4a2586c4db6fb8d80d0d
Girish21 <girishkumarv21@gmail.com>
2022-03-08 10:40:34 +0530

    chore: split handlers

--- examples/msw/mocks/index.js
+++ examples/msw/mocks/index.js
@@ -1,15 +1,6 @@
-const { rest } = require("msw");
 const { setupServer } = require("msw/node");
-
-const handlers = [
-  rest.get("https://my-mock-api.com", (_, res, ctx) => {
-    return res(ctx.status(200), ctx.json({ message: "from msw" }));
-  }),
-];
+const handlers = require("./handlers");
 
 const server = setupServer(...handlers);
 server.listen({ onUnhandledRequest: "warn" });
 console.info("MSW initialised");
-
-process.once("SIGINT", () => server.close());
-process.once("SIGTERM", () => server.close());
