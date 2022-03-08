528e1de8cf99b0d5af4fc1beb1b3b37fdce25895
Chance Strickland <hi@chance.dev>
2022-03-08 11:36:00 -0800

    Update packages/remix-eslint-config/index.js
    Co-authored-by: Michaël De Boey <info@michaeldeboey.be>

--- packages/remix-eslint-config/index.js
+++ packages/remix-eslint-config/index.js
@@ -75,7 +75,7 @@ const config = {
       },
     },
     {
-      files: ["**/routes/**/*.js?(x)", "**/routes/**/*.ts?(x)"],
+      files: ["**/routes/**/*.jsx", "**/routes/**/*.tsx"],
       rules: {
         // Routes may use default exports without a name. At the route level
         // identifying components for debugging purposes is less of an issue, as
