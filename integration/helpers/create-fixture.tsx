2e09d371272ef5fc69c38c50cb0a2478ac2a4e45
Jacob Ebey <jacob.ebey@live.com>
2022-03-09 12:10:22 +0700

    test: added fixture test from Logan
    There doesn't appear to be anything that needs to be done here.

--- integration/helpers/create-fixture.tsx
+++ integration/helpers/create-fixture.tsx
@@ -216,12 +216,22 @@ export async function createAppFixture(fixture: Fixture) {
        */
       clickSubmitButton: async (
         action: string,
-        options: { wait: boolean } = { wait: true }
+        options: { wait: boolean; method?: string } = { wait: true }
       ) => {
-        let selector = `button[formaction="${action}"]`;
+        let selector: string;
+        if (options.method) {
+          selector = `button[formAction="${action}"][formMethod="${options.method}"]`;
+        } else {
+          selector = `button[formAction="${action}"]`;
+        }
+
         let el = await page.$(selector);
         if (!el) {
-          selector = `form[action="${action}"] button[type="submit"]`;
+          if (options.method) {
+            selector = `form[action="${action}"] button[type="submit"][formMethod="${options.method}"]`;
+          } else {
+            selector = `form[action="${action}"] button[type="submit"]`;
+          }
           el = await page.$(selector);
           if (!el) {
             throw new Error(`Can't find button for: ${action}`);
@@ -362,6 +372,7 @@ function build(projectDir: string) {
   });
   cp.spawnSync("node", ["node_modules/@remix-run/dev/cli.js", "build"], {
     cwd: projectDir,
+    stdio: "inherit",
   });
 }
 
