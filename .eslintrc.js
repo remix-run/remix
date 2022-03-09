d3f3b8de67823f927a0a89f63e8ca3c79f55786c
Chance Strickland <hi@chance.dev>
2022-03-08 16:18:44 -0800

    disallow node globals in remix-server-runtime

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,30 +5,4 @@ module.exports = {
     "plugin:markdown/recommended",
   ],
   plugins: ["markdown"],
-  overrides: [
-    {
-      files: [
-        "packages/create-remix/templates/cloudflare-workers/**/*.js",
-        "packages/remix-cloudflare-workers/**/*.ts",
-      ],
-      rules: {
-        "no-restricted-globals": "off",
-      },
-    },
-    {
-      files: [
-        "packages/remix-server-runtime/**/*.js?(x)",
-        "packages/remix-server-runtime/**/*.ts?(x)",
-      ],
-      rules: {
-        "node/no-unsupported-features/node-builtins": [
-          "error",
-          {
-            version: ">=14.0.0",
-            ignores: [],
-          },
-        ],
-      },
-    },
-  ],
 };
---
+++ packages/remix-server-runtime/.eslintrc.js
@@ -0,0 +1,14 @@
+let restrictedGlobalsError = `Node globals are not allowed in this package.`;
+
+module.exports = {
+  rules: {
+    "no-restricted-globals": [
+      "error",
+      { name: "__dirname", message: restrictedGlobalsError },
+      { name: "__filename", message: restrictedGlobalsError },
+      { name: "Buffer", message: restrictedGlobalsError },
+      { name: "process", message: restrictedGlobalsError },
+    ],
+    "import/no-nodejs-modules": "error",
+  },
+};
