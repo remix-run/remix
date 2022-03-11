ab659e81a6a990addaa8b1ac3cf1945563409178
Matt Kane <m@mk.gg>
2022-03-11 19:35:11 +0000

    fix(netlify): use Netlify internal functions dir (#2291)

--- packages/create-remix/templates/netlify/remix.config.js
+++ packages/create-remix/templates/netlify/remix.config.js
@@ -7,6 +7,6 @@ module.exports = {
   ignoredRouteFiles: [".*"],
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
-  // serverBuildPath: "netlify/functions/server/index.js",
+  // serverBuildPath: ".netlify/functions-internal/server.js",
   // publicPath: "/build/",
 };
