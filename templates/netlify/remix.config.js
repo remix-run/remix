e7f768abf2b335393f3ffa5ec28e0fc474484da6
Michaël De Boey <info@michaeldeboey.be>
2022-03-15 00:18:39 +0100

    fix(templates): bring in line with each other

--- templates/netlify/remix.config.js
+++ templates/netlify/remix.config.js
@@ -9,5 +9,4 @@ module.exports = {
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: ".netlify/functions-internal/server.js",
   // publicPath: "/build/",
-  // devServerPort: 8002
 };
