3a605a362e35b29345d159664dcb3c9de08d6d6d
Michaël De Boey <info@michaeldeboey.be>
2022-03-15 17:14:59 +0100

    feat(remix-react): add support for `charset` in `meta` function (#2325)

--- templates/express-ts/app/root.tsx
+++ templates/express-ts/app/root.tsx
@@ -8,16 +8,16 @@ import {
 } from "remix";
 import type { MetaFunction } from "remix";
 
-export const meta: MetaFunction = () => {
-  return { title: "New Remix App" };
-};
+export const meta: MetaFunction = () => ({
+  charset: "utf-8",
+  title: "New Remix App",
+  viewport: "width=device-width,initial-scale=1",
+});
 
 export default function App() {
   return (
     <html lang="en">
       <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
         <Meta />
         <Links />
       </head>
