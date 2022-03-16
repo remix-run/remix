d8080bc8cd5c60d986c1d55ba95fe3cd27558a1e
Logan McAnsh <logan@mcan.sh>
2022-03-15 20:36:26 -0400

    chore: sort package.json for examples and templates, pass base as arg
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- scripts/patchup-example-names.mjs
+++ scripts/patchup-example-names.mjs
@@ -1,13 +1,13 @@
 import fs from "fs/promises";
 import path from "path";
+import sort from "sort-package-json";
 
 let examplePath = path.join(process.cwd(), "examples");
 let templatePath = path.join(process.cwd(), "templates");
 let examples = await fs.readdir(examplePath);
 let templates = await fs.readdir(templatePath);
 
-async function update(dir, name) {
-  let base = path.basename(dir).slice(0, -1);
+async function update(dir, name, base) {
   let fullPath = path.join(dir, name);
   let stat = await fs.stat(fullPath);
   if (stat.isDirectory()) {
@@ -15,14 +15,15 @@ async function update(dir, name) {
     let pkg = await fs.readFile(pkgPath);
     let obj = JSON.parse(pkg);
     obj.name = `remix-${base}-${name}`;
-    await fs.writeFile(pkgPath, JSON.stringify(obj, null, 2) + "\n");
+    let sorted = sort(obj);
+    await fs.writeFile(pkgPath, JSON.stringify(sorted, null, 2) + "\n");
   }
 }
 
 examples.forEach(async (name) => {
-  await update(examplePath, name);
+  await update(examplePath, name, "example");
 });
 
 templates.forEach(async (name) => {
-  await update(templatePath, name);
+  await update(templatePath, name, "template");
 });
