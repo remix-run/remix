3fd52a5b9d3ab0fc6d6f5483cada8e80cd38d41e
Logan McAnsh <logan@mcan.sh>
2022-03-17 15:04:31 -0400

    chore: update example and template package.json names to resolve 'jest-haste-map' warning (#2333)

--- scripts/patchup-example-names.mjs
+++ scripts/patchup-example-names.mjs
@@ -1,17 +1,27 @@
 import fs from "fs/promises";
 import path from "path";
 
-let dirPath = path.join(process.cwd(), "examples");
-let dir = await fs.readdir(dirPath);
+let examplePath = path.join(process.cwd(), "examples");
+let templatePath = path.join(process.cwd(), "templates");
+let examples = await fs.readdir(examplePath);
+let templates = await fs.readdir(templatePath);
 
-dir.forEach(async (name) => {
-  let fullPath = path.join(dirPath, name);
+async function update(dir, name, base) {
+  let fullPath = path.join(dir, name);
   let stat = await fs.stat(fullPath);
   if (stat.isDirectory()) {
     let pkgPath = path.join(fullPath, "package.json");
     let pkg = await fs.readFile(pkgPath);
     let obj = JSON.parse(pkg);
-    obj.name = `remix-example-${name}`;
+    obj.name = `remix-${base}-${name}`;
     await fs.writeFile(pkgPath, JSON.stringify(obj, null, 2) + "\n");
   }
+}
+
+examples.forEach(async (name) => {
+  await update(examplePath, name, "example");
+});
+
+templates.forEach(async (name) => {
+  await update(templatePath, name, "template");
 });
