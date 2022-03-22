a6ef353ab96cc97b66267c803b05e86f800986ad
Michael Jackson <michael@jackson.us>
2022-03-21 21:05:04 -0700

    Consolidate importKey logic in createKey helper

--- packages/remix-cloudflare/crypto.ts
+++ packages/remix-cloudflare/crypto.ts
@@ -3,14 +3,7 @@ import type { SignFunction, UnsignFunction } from "@remix-run/server-runtime";
 const encoder = new TextEncoder();
 
 export const sign: SignFunction = async (value, secret) => {
-  let key = await crypto.subtle.importKey(
-    "raw",
-    encoder.encode(secret),
-    { name: "HMAC", hash: "SHA-256" },
-    false,
-    ["sign"]
-  );
-
+  let key = await createKey(secret, ["sign"]);
   let data = encoder.encode(value);
   let signature = await crypto.subtle.sign("HMAC", key, data);
   let hash = btoa(String.fromCharCode(...new Uint8Array(signature))).replace(
@@ -22,23 +15,31 @@ export const sign: SignFunction = async (value, secret) => {
 };
 
 export const unsign: UnsignFunction = async (cookie, secret) => {
-  let key = await crypto.subtle.importKey(
-    "raw",
-    encoder.encode(secret),
-    { name: "HMAC", hash: "SHA-256" },
-    false,
-    ["verify"]
-  );
-
   let value = cookie.slice(0, cookie.lastIndexOf("."));
   let hash = cookie.slice(cookie.lastIndexOf(".") + 1);
 
+  let key = await createKey(secret, ["verify"]);
   let data = encoder.encode(value);
   let signature = byteStringToUint8Array(atob(hash));
   let valid = await crypto.subtle.verify("HMAC", key, signature, data);
 
   return valid ? value : false;
 };
+
+async function createKey(
+  secret: string,
+  usages: CryptoKey["usages"]
+): Promise<CryptoKey> {
+  let key = await crypto.subtle.importKey(
+    "raw",
+    encoder.encode(secret),
+    { name: "HMAC", hash: "SHA-256" },
+    false,
+    usages
+  );
+
+  return key;
+}
 
 function byteStringToUint8Array(byteString: string): Uint8Array {
   let array = new Uint8Array(byteString.length);
