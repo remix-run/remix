28ef6426861e6f6b670b6bd654272726b3c5c936
Kent C. Dodds <me+github@kentcdodds.com>
2022-03-25 15:34:04 -0600

    docs(blog-tutorial): switch to Indie Stack for the blog tutorial (#2502)

--- examples/blog-tutorial/.eslintrc.js
+++ examples/blog-tutorial/.eslintrc.js
@@ -1,3 +1,4 @@
 module.exports = {
+  root: true,
   extends: ["@remix-run/eslint-config", "@remix-run/eslint-config/node"],
 };
---
+++ examples/blog-tutorial/cypress/support/delete-user.ts
@@ -0,0 +1,22 @@
+// Use this to delete a user by their email
+// Simply call this with:
+// node --require esbuild-register ./cypress/support/delete-user.ts username@example.com
+// and that user will get deleted
+
+import { installGlobals } from "@remix-run/node/globals";
+import { prisma } from "~/db.server";
+
+installGlobals();
+
+async function deleteUser(email: string) {
+  if (!email) {
+    throw new Error("email required for login");
+  }
+  if (!email.endsWith("@example.com")) {
+    throw new Error("All test emails must end in @example.com");
+  }
+
+  await prisma.user.delete({ where: { email } });
+}
+
+deleteUser(process.argv[2]);
