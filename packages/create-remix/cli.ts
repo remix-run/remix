34ccdee87ce3e13148bbc7dcb9f6a745c753672c
Chance Strickland <hi@chance.dev>
2022-03-27 17:28:47 -0700

    simplify error handling

--- packages/create-remix/cli.ts
+++ packages/create-remix/cli.ts
@@ -1,11 +1,11 @@
 import { cli } from "@remix-run/dev";
 
-cli.run(["create", ...process.argv.slice(2)]).then(
-  () => {
+cli
+  .run(["create", ...process.argv.slice(2)])
+  .then(() => {
     process.exit(0);
-  },
-  (error: Error) => {
+  })
+  .catch((error) => {
     console.error(error);
     process.exit(1);
-  }
-);
+  });
