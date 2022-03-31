063c204e26f5be063ac08153b7e0e97a5aef94cb
Kent C. Dodds <me+github@kentcdodds.com>
2022-03-31 14:38:25 -0600

    Update templates/express/server.js

--- templates/express/server.js
+++ templates/express/server.js
@@ -46,6 +46,15 @@ app.listen(port, () => {
   console.log(`Express server listening on port ${port}`);
 });
 
-function purgeRequireCache(path) {
-  delete require.cache[require.resolve(path)];
+function purgeRequireCache() {
+  // purge require cache on requests for "server side HMR" this won't let
+  // you have in-memory objects between requests in development,
+  // alternatively you can set up nodemon/pm2-dev to restart the server on
+  // file changes, but then you'll have to reconnect to databases/etc on each
+  // change. We prefer the DX of this, so we've included it for you by default
+  for (const key in require.cache) {
+    if (key.startsWith(BUILD_DIR)) {
+      delete require.cache[key];
+    }
+  }
 }
