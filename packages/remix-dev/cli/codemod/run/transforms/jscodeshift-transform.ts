eac71f4397a1f3cee16b0aaf9ca574eff73fb0a7
Michaël De Boey <info@michaeldeboey.be>
2022-03-31 20:02:06 +0200

    feat(remix-dev): add support for `extraOptions` in `jscodeshift` transforms

--- packages/remix-dev/cli/codemod/run/transforms/jscodeshift-transform.ts
+++ packages/remix-dev/cli/codemod/run/transforms/jscodeshift-transform.ts
@@ -2,16 +2,29 @@ import { sync as execaSync } from "execa";
 
 const jscodeshiftExecutable = require.resolve(".bin/jscodeshift");
 
-type JSCodeshiftTransformArgs = {
+type ExtraOptions = Record<string, unknown>;
+const normalizeExtraOptions = (extraOptions: ExtraOptions = {}) =>
+  Object.entries(extraOptions)
+    .filter(([_key, value]) => Boolean(value))
+    .map(
+      ([key, value]) =>
+        `--${key}${typeof value === "boolean" ? "" : `=${value}`}`
+    );
+
+type JSCodeshiftTransformArgs<ExtraOptionsType extends ExtraOptions = {}> = {
+  extraOptions?: ExtraOptionsType;
   files: string[];
   flags: { dry?: boolean; print?: boolean; runInBand?: boolean };
   transformPath: string;
 };
-export const JSCodeshiftTransform = ({
+export const JSCodeshiftTransform = <
+  ExtraOptionsType extends ExtraOptions = {}
+>({
+  extraOptions,
   files,
   flags: { dry, print, runInBand },
   transformPath,
-}: JSCodeshiftTransformArgs) => {
+}: JSCodeshiftTransformArgs<ExtraOptionsType>) => {
   let args = [
     dry ? "--dry" : "",
     print ? "--print" : "",
@@ -24,6 +37,7 @@ export const JSCodeshiftTransform = ({
     "--parser=tsx",
     ...["--transform", transformPath],
     ...files,
+    ...normalizeExtraOptions(extraOptions),
   ];
 
   console.log(`Executing command: jscodeshift ${args.join(" ")}`);
