c5e7ccaf6961dde9ff0281a70bf594833e3f5f43
Jacob Ebey <jacob.ebey@live.com>
2022-04-01 11:34:28 -0700

    more tests

--- integration/form-data-test.ts
+++ integration/form-data-test.ts
@@ -22,11 +22,28 @@ beforeAll(async () => {
   });
 });
 
-it("should respond and not crash server", async () => {
-  // You can test any request your app might get using `fixture`.
+it("invalid content-type does not crash server", async () => {
   let response = await fixture.requestDocument("/", {
     method: "post",
     headers: { "content-type": "application/json" },
   });
   expect(await response.text()).toMatch("no pizza");
+});
+
+it("invalid urlencoded body does not crash server", async () => {
+  let response = await fixture.requestDocument("/", {
+    method: "post",
+    headers: { "content-type": "application/x-www-form-urlencoded" },
+    body: "$rofl this is totally invalid$",
+  });
+  expect(await response.text()).toMatch("pizza");
+});
+
+it("invalid multipart body does not crash server", async () => {
+  let response = await fixture.requestDocument("/", {
+    method: "post",
+    headers: { "content-type": "multipart/form-data" },
+    body: "$rofl this is totally invalid$",
+  });
+  expect(await response.text()).toMatch("pizza");
 });
