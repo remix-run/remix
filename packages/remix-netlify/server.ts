931cb2bda881213c35e63a500e44457349a744e1
Logan McAnsh <logan@mcan.sh>
2022-04-05 15:46:39 -0400

    fix(remix-netlify): fix uploads similar to #2495

--- packages/remix-netlify/server.ts
+++ packages/remix-netlify/server.ts
@@ -83,10 +83,17 @@ export function createRemixRequest(
     signal: abortController?.signal,
   };
 
+  let isFormData = event.headers["content-type"]?.includes(
+    "multipart/form-data"
+  );
+
   if (event.httpMethod !== "GET" && event.httpMethod !== "HEAD" && event.body) {
-    init.body = event.isBase64Encoded
-      ? Buffer.from(event.body, "base64").toString()
-      : event.body;
+    init.body =
+      event.body && event.isBase64Encoded
+        ? isFormData
+          ? Buffer.from(event.body, "base64")
+          : Buffer.from(event.body, "base64").toString()
+        : event.body;
   }
 
   return new NodeRequest(url.href, init);
