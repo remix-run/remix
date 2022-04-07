4ecd21ad3564a936010b0e5e3d2e722c1a004259
Logan McAnsh <logan@mcan.sh>
2022-04-07 16:40:20 -0400

    chore: remove screenshots and videos
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- integration/playwright.config.ts
+++ integration/playwright.config.ts
@@ -39,8 +39,6 @@ const config: PlaywrightTestConfig = {
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
-    screenshot: "only-on-failure",
-    video: "on-first-retry",
   },
 
   /* Configure projects for major browsers */
