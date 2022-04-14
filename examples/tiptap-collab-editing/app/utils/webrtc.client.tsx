ba447b78e10d667e346beaca2284afbe3096011d
Marvin Wu <marvinwu@gmail.com>
2022-04-14 15:48:39 +0800

    tip tab realtime edit example

---
+++ examples/tiptap-collab-editing/app/utils/webrtc.client.tsx
@@ -0,0 +1,6 @@
+import Collaboration from "@tiptap/extension-collaboration";
+import * as Y from "yjs";
+const ydoc = new Y.Doc();
+import { WebrtcProvider } from "y-webrtc";
+
+export { Collaboration, WebrtcProvider, ydoc };
