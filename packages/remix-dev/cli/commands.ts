ee7f80b58970e91fe91ef625318ac3c48c2c154c
David Vargas <dvargas92495@gmail.com>
2022-04-17 01:00:04 -0400

    fix import lint and debug test

--- packages/remix-dev/cli/commands.ts
+++ packages/remix-dev/cli/commands.ts
@@ -10,6 +10,7 @@ import type { Server } from "http";
 import type * as Express from "express";
 import type { createApp as createAppType } from "@remix-run/serve";
 import getPort, { makeRange } from "get-port";
+import * as esbuild from "esbuild";
 
 import { BuildMode, isBuildMode } from "../build";
 import * as colors from "../colors";
@@ -22,7 +23,6 @@ import { log } from "../logging";
 import { createApp } from "./create";
 import { getPreferredPackageManager } from "./getPreferredPackageManager";
 import { setupRemix, isSetupPlatform, SetupPlatform } from "./setup";
-import * as esbuild from "esbuild";
 
 export * as migrate from "./migrate";
 
