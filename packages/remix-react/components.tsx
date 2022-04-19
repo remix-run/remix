65a54ee59cfcf2d7790ff0328eabf0b155b20a1a
Jacob Ebey <jacob.ebey@live.com>
2022-04-19 12:54:39 -0700

    iife to get it out of global scope

--- packages/remix-react/components.tsx
+++ packages/remix-react/components.tsx
@@ -1416,27 +1416,29 @@ export const LiveReload =
             suppressHydrationWarning
             dangerouslySetInnerHTML={{
               __html: js`
-                let protocol = location.protocol === "https:" ? "wss:" : "ws:";
-                let host = location.hostname;
-                let socketPath = protocol + "//" + host + ":" + ${String(
-                  port
-                )} + "/socket";
-      
-                let ws = new WebSocket(socketPath);
-                ws.onmessage = (message) => {
-                  let event = JSON.parse(message.data);
-                  if (event.type === "LOG") {
-                    console.log(event.message);
-                  }
-                  if (event.type === "RELOAD") {
-                    console.log("💿 Reloading window ...");
-                    window.location.reload();
-                  }
-                };
-                ws.onerror = (error) => {
-                  console.log("Remix dev asset server web socket error:");
-                  console.error(error);
-                };
+                (() => {
+                  let protocol = location.protocol === "https:" ? "wss:" : "ws:";
+                  let host = location.hostname;
+                  let socketPath = protocol + "//" + host + ":" + ${String(
+                    port
+                  )} + "/socket";
+        
+                  let ws = new WebSocket(socketPath);
+                  ws.onmessage = (message) => {
+                    let event = JSON.parse(message.data);
+                    if (event.type === "LOG") {
+                      console.log(event.message);
+                    }
+                    if (event.type === "RELOAD") {
+                      console.log("💿 Reloading window ...");
+                      window.location.reload();
+                    }
+                  };
+                  ws.onerror = (error) => {
+                    console.log("Remix dev asset server web socket error:");
+                    console.error(error);
+                  };
+                })();
               `,
             }}
           />
