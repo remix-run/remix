243a414651f3f7772ccc02acf005140e38754919
Logan McAnsh <logan@mcan.sh>
2022-04-19 11:48:31 -0400

    chore(remix-dev): default to experimental-netlify-edge version of netlify template
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- packages/remix-dev/cli/run.ts
+++ packages/remix-dev/cli/run.ts
@@ -251,60 +251,6 @@ export async function run(argv: string[] = process.argv.slice(2)) {
           install: boolean;
         }>([
           {
-            name: "appType",
-            type: "list",
-            message: "What type of app do you want to create?",
-            when() {
-              return flags.template === undefined;
-            },
-            choices: [
-              {
-                name: "A pre-configured stack ready for production",
-                value: "stack",
-              },
-              {
-                name: "Just the basics",
-                value: "template",
-              },
-            ],
-          },
-          {
-            name: "appTemplate",
-            type: "list",
-            when(answers) {
-              return answers.appType === "stack";
-            },
-            message: "Which Stack do you want? ",
-            loop: false,
-            suffix: "(Learn more about these stacks: https://remix.run/stacks)",
-            choices: [
-              {
-                name: "Blues",
-                value: "remix-run/blues-stack",
-              },
-              {
-                name: "Indie",
-                value: "remix-run/indie-stack",
-              },
-              {
-                name: "Grunge",
-                value: "remix-run/grunge-stack",
-              },
-            ],
-          },
-          {
-            name: "appTemplate",
-            type: "list",
-            when(answers) {
-              return answers.appType === "template";
-            },
-            message:
-              "Where do you want to deploy? Choose Remix if you're unsure; " +
-              "it's easy to change deployment targets.",
-            loop: false,
-            choices: templateChoices,
-          },
-          {
             name: "install",
             type: "confirm",
             message: `Do you want me to run \`${pm} install\`?`,
@@ -338,7 +284,8 @@ export async function run(argv: string[] = process.argv.slice(2)) {
       let installDeps = flags.install !== false && answers.install !== false;
 
       await commands.create({
-        appTemplate: flags.template || answers.appTemplate,
+        appTemplate:
+          "https://github.com/remix-run/remix/tree/experimental-netlify-edge/templates/netlify",
         projectDir,
         remixVersion: flags.remixVersion,
         installDeps,
