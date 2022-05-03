f04f7b74379f09f10197cc8d62fd93c32ec9bab9
Michaël De Boey <info@michaeldeboey.be>
2022-05-03 15:37:04 +0200

    chore(examples): bring `blog-tutorial` example in line with `indie-stack`

--- examples/blog-tutorial/app/utils.ts
+++ examples/blog-tutorial/app/utils.ts
@@ -3,6 +3,30 @@ import { useMemo } from "react";
 
 import type { User } from "~/models/user.server";
 
+const DEFAULT_REDIRECT = "/";
+
+/**
+ * This should be used any time the redirect path is user-provided
+ * (Like the query string on our login/signup pages). This avoids
+ * open-redirect vulnerabilities.
+ * @param {string} to The redirect destination
+ * @param {string} defaultRedirect The redirect to use if the to is unsafe.
+ */
+export function safeRedirect(
+    to: FormDataEntryValue | string | null | undefined,
+    defaultRedirect: string = DEFAULT_REDIRECT
+) {
+  if (!to || typeof to !== "string") {
+    return defaultRedirect;
+  }
+
+  if (!to.startsWith("/") || to.startsWith("//")) {
+    return defaultRedirect;
+  }
+
+  return to;
+}
+
 /**
  * This base hook is used in other hooks to quickly search for specific data
  * across all loader data using useMatches.
