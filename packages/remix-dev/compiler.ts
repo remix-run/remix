cc41d27b4318dff988658a45b087d76fb9e93273
Pedro Cattori <pcattori@gmail.com>
2022-05-05 23:39:58 -0400

    refactor(compiler): do not bundle url imports
    keep url imports as-is in build output so that each runtime can handle url imports in their own way.
    for example, deno will download dependencies from url imports at runtime. the browser will download
    dependendencies from url imports on-demand. node will simply throw an error when encountering a url
    import.
    
    since we will recommend to use npm/node_modules for dependency
    management (even for deno), the only url imports we expect are for deno
    imports on the server from https://deno.land or from Deno-friendly CDNs
    like https://esm.sh .

--- packages/remix-dev/compiler.ts
+++ packages/remix-dev/compiler.ts
@@ -23,6 +23,7 @@ import { serverBareModulesPlugin } from "./compiler/plugins/serverBareModulesPlu
 import { serverEntryModulePlugin } from "./compiler/plugins/serverEntryModulePlugin";
 import { serverRouteModulesPlugin } from "./compiler/plugins/serverRouteModulesPlugin";
 import { writeFileSafe } from "./compiler/utils/fs";
+import { urlImportsPlugin } from "./compiler/plugins/urlImportsPlugin";
 
 // When we build Remix, this shim file is copied directly into the output
 // directory in the same place relative to this file. It is eventually injected
@@ -347,22 +348,12 @@ async function createBrowserBuild(
   }
 
   let plugins = [
+    urlImportsPlugin(),
     mdxPlugin(config),
     browserRouteModulesPlugin(config, /\?browser$/),
     emptyModulesPlugin(config, /\.server(\.[jt]sx?)?$/),
     NodeModulesPolyfillPlugin(),
   ];
-
-  if (config.serverBuildTarget === "deno") {
-    // @ts-expect-error
-    let { cache } = await import("esbuild-plugin-cache");
-    plugins.unshift(
-      cache({
-        importmap: {},
-        directory: path.join(config.cacheDirectory, "http-import-cache"),
-      })
-    );
-  }
 
   return esbuild.build({
     entryPoints,
@@ -419,6 +410,7 @@ function createServerBuild(
     config.serverBuildTarget ?? ""
   );
   let plugins: esbuild.Plugin[] = [
+    urlImportsPlugin(),
     mdxPlugin(config),
     emptyModulesPlugin(config, /\.client(\.[jt]sx?)?$/),
     serverRouteModulesPlugin(config),
