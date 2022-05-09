988ef5076e9f9e2f8d031b42603d47db3de96b92
Logan McAnsh <logan@mcan.sh>
2022-05-09 14:35:29 -0400

    ci: use tags instead of releases

--- scripts/release/constants.ts
+++ scripts/release/constants.ts
@@ -1,3 +1,9 @@
+if (!process.env.DEFAULT_BRANCH) {
+  throw new Error("DEFAULT_BRANCH is required");
+}
+if (!process.env.DEV_BRANCH) {
+  throw new Error("DEV_BRANCH is required");
+}
 if (!process.env.GITHUB_TOKEN) {
   throw new Error("GITHUB_TOKEN is required");
 }
@@ -12,8 +18,10 @@ if (!process.env.VERSION.startsWith("refs/tags/")) {
 }
 
 export const [OWNER, REPO] = process.env.GITHUB_REPOSITORY.split("/");
-export const LATEST_RELEASE = process.env.VERSION.replace("refs/tags/", "");
+export const REF = process.env.VERSION.replace("refs/tags/", "");
 export const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
 export const GITHUB_REPOSITORY = process.env.GITHUB_REPOSITORY;
-
+export const DEFAULT_BRANCH = process.env.DEFAULT_BRANCH;
+export const DEV_BRANCH = process.env.DEV_BRANCH;
 export const PR_FILES_STARTS_WITH = ["packages/"];
+diff --git a/scripts/release/constants.mjs b/scripts/release/constants.mjs
