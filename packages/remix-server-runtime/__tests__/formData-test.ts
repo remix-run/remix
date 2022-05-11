37576e4e26f1be6c2b0198c2a3796da9ee994778
Jacob Ebey <jacob.ebey@live.com>
2022-05-10 17:51:42 -0700

    added another test for upload handlers

--- packages/remix-server-runtime/__tests__/formData-test.ts
+++ packages/remix-server-runtime/__tests__/formData-test.ts
@@ -10,8 +10,8 @@ describe("parseMultipartFormData", () => {
   it("can use a custom upload handler", async () => {
     let formData = new NodeFormData();
     formData.set("a", "value");
-    formData.set("blob", new Blob(["blob"]), "blob.txt");
-    formData.set("file", new File(["file"], "file.txt"));
+    formData.set("blob", new Blob(["blob".repeat(1000)]), "blob.txt");
+    formData.set("file", new File(["file".repeat(1000)], "file.txt"));
 
     let req = new NodeRequest("https://test.com", {
       method: "post",
@@ -35,10 +35,31 @@ describe("parseMultipartFormData", () => {
 
     expect(parsedFormData.get("a")).toBe("value");
     let blob = parsedFormData.get("blob") as Blob;
-    expect(await blob.text()).toBe("blob");
+    expect(await blob.text()).toBe("blob".repeat(1000));
     let file = parsedFormData.get("file") as File;
     expect(file.name).toBe("file.txt");
-    expect(await file.text()).toBe("file");
+    expect(await file.text()).toBe("file".repeat(1000));
+  });
+
+  it("can return undefined", async () => {
+    let formData = new NodeFormData();
+    formData.set("a", "value");
+    formData.set("blob", new Blob(["blob".repeat(1000)]), "blob.txt");
+    formData.set("file", new File(["file".repeat(1000)], "file.txt"));
+
+    let req = new NodeRequest("https://test.com", {
+      method: "post",
+      body: formData,
+    });
+
+    let parsedFormData = await parseMultipartFormData(
+      req,
+      async () => undefined
+    );
+
+    expect(parsedFormData.get("a")).toBe(null);
+    expect(parsedFormData.get("blob")).toBe(null);
+    expect(parsedFormData.get("file")).toBe(null);
   });
 
   it("can throw errors in upload handlers", async () => {
@@ -56,14 +77,16 @@ describe("parseMultipartFormData", () => {
       body: formData,
     });
 
+    let error: Error;
     try {
       await parseMultipartFormData(req, async () => {
         throw new CustomError();
       });
       throw new Error("should have thrown");
     } catch (err) {
-      expect(err.message).toBe("test error");
-      expect(err).toBeInstanceOf(CustomError);
+      error = err;
     }
+    expect(error).toBeInstanceOf(CustomError);
+    expect(error.message).toBe("test error");
   });
 });
