0a893e1cc586e8897ae21aa1154906f64d1efdb0
Jacob Ebey <jacob.ebey@live.com>
2022-05-13 22:53:03 -0700

    update playground

--- scripts/playground/template/app/entry.client.tsx
+++ scripts/playground/template/app/entry.client.tsx
@@ -1,4 +1,4 @@
-import { RemixBrowser } from "@remix-run/react";
 import { hydrateRoot } from "react-dom/client";
+import { RemixBrowser } from "@remix-run/react";
 
 hydrateRoot(document, <RemixBrowser />);
--- scripts/playground/template/app/entry.server.tsx
+++ scripts/playground/template/app/entry.server.tsx
@@ -1,6 +1,11 @@
-import type { EntryContext } from "@remix-run/node";
+import { PassThrough } from "stream";
+import { renderToPipeableStream } from "react-dom/server";
 import { RemixServer } from "@remix-run/react";
-import { renderToString } from "react-dom/server";
+import type { EntryContext } from "@remix-run/node";
+import { Response } from "@remix-run/node";
+import isbot from "isbot";
+
+const ABORT_DELAY = 5000;
 
 export default function handleRequest(
   request: Request,
@@ -8,14 +13,38 @@ export default function handleRequest(
   responseHeaders: Headers,
   remixContext: EntryContext
 ) {
-  let markup = renderToString(
-    <RemixServer context={remixContext} url={request.url} />
-  );
+  let callbackName = isbot(request.headers.get("user-agent"))
+    ? "onAllReady"
+    : "onShellReady";
 
-  responseHeaders.set("Content-Type", "text/html");
+  return new Promise((resolve, reject) => {
+    let didError = false;
 
-  return new Response("<!DOCTYPE html>" + markup, {
-    status: responseStatusCode,
-    headers: responseHeaders,
+    let { pipe, abort } = renderToPipeableStream(
+      <RemixServer context={remixContext} url={request.url} />,
+      {
+        [callbackName]() {
+          let body = new PassThrough();
+
+          responseHeaders.set("Content-Type", "text/html");
+
+          resolve(
+            new Response(body, {
+              status: didError ? 500 : responseStatusCode,
+              headers: responseHeaders,
+            })
+          );
+          pipe(body);
+        },
+        onShellError(err) {
+          reject(err);
+        },
+        onError(error) {
+          didError = true;
+          console.error(error);
+        },
+      }
+    );
+    setTimeout(abort, ABORT_DELAY);
   });
 }
