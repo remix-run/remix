2630896fdc0a024f71d5981d623cb67cc1ada40b
Jacob Ebey <jacob.ebey@live.com>
2022-05-17 13:28:12 -0700

    removed derrived properties from init

--- packages/remix-server-runtime/server.ts
+++ packages/remix-server-runtime/server.ts
@@ -295,16 +295,9 @@ async function handleDocumentRequest({
 
   let loaderRequest = new Request(request.url, {
     body: null,
-    cache: request.cache,
-    credentials: request.credentials,
     headers: request.headers,
-    integrity: request.integrity,
-    keepalive: request.keepalive,
     method: request.method,
-    mode: request.mode,
     redirect: request.redirect,
-    referrer: request.referrer,
-    referrerPolicy: request.referrerPolicy,
     signal: request.signal,
   });
 
