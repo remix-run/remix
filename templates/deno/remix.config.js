e37e1f907fe77ae594e6e703b1dc808b922329dc
Pedro Cattori <pcattori@gmail.com>
2022-05-19 11:13:58 -0400

    Revert "chore: update Deno import maps on release (#3225)" (#3254)
    This reverts commit c28e179dc3d5a80ac5d6bc74f3ec0ed8154e7aee.

--- templates/deno/remix.config.js
+++ templates/deno/remix.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   serverBuildTarget: "deno",
   server: "./server.ts",
+
   /*
   If live reload causes page to re-render without changes (live reload is too fast),
   increase the dev server broadcast delay.
@@ -8,9 +9,4 @@ module.exports = {
   If live reload seems slow, try to decrease the dev server broadcast delay.
   */
   devServerBroadcastDelay: 300,
-  ignoredRouteFiles: ["**/.*"],
-  // appDirectory: "app",
-  // assetsBuildDirectory: "public/build",
-  // serverBuildPath: "build/index.js",
-  // publicPath: "/build/",
 };
