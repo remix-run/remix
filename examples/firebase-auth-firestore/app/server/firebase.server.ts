ee73bd5560eea48d3f0a18e7a31d9113166b42f5
Alasdair McLeay <penx@users.noreply.github.com>
2022-06-01 12:51:55 +0100

    fix(examples/firebase-auth-firestore): Fix imports and throw meaningful errors when missing .env (#3353)

--- examples/firebase-auth-firestore/app/server/firebase.server.ts
+++ examples/firebase-auth-firestore/app/server/firebase.server.ts
@@ -1,15 +1,40 @@
-import admin from "firebase-admin/app";
-import client from "firebase/app";
+import {
+  getApps as getServerApps,
+  initializeApp as initializeServerApp,
+  cert as serverCert,
+} from "firebase-admin/app";
+import {
+  getApps as getClientApps,
+  initializeApp as initializeClientApp,
+} from "firebase/app";
 import { getAuth as getServerAuth } from "firebase-admin/auth";
 import { getAuth as getClientAuth } from "firebase/auth";
 
-if (client.getApps().length === 0) {
-  client.initializeApp(JSON.parse(process.env.CLIENT_CONFIG as string));
+if (getClientApps().length === 0) {
+  if (!process.env.CLIENT_CONFIG) {
+    throw new Error("Missing CLIENT_CONFIG environment variable");
+  }
+  let config;
+  try {
+    config = JSON.parse(process.env.CLIENT_CONFIG);
+  } catch {
+    throw Error("Invalid CLIENT_CONFIG environment variable");
+  }
+  initializeClientApp(config);
 }
 
-if (admin.getApps().length === 0) {
-  admin.initializeApp({
-    credential: admin.cert(JSON.parse(process.env.SERVICE_ACCOUNT as string)),
+if (getServerApps().length === 0) {
+  if (!process.env.SERVICE_ACCOUNT) {
+    throw new Error("Missing SERVICE_ACCOUNT environment variable");
+  }
+  let config;
+  try {
+    config = JSON.parse(process.env.SERVICE_ACCOUNT);
+  } catch {
+    throw Error("Invalid SERVICE_ACCOUNT environment variable");
+  }
+  initializeServerApp({
+    credential: serverCert(config),
   });
 }
 
