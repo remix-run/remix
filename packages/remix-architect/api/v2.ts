0e18fad97bfd3a8dafa38b7b2ef5167464064e93
Wing <damianleung@gmail.com>
2022-06-09 20:00:08 +0200

    Update packages/remix-architect/api/v2.ts
    Co-authored-by: Michaël De Boey <info@michaeldeboey.be>

--- packages/remix-architect/api/v2.ts
+++ packages/remix-architect/api/v2.ts
@@ -32,7 +32,6 @@ export function createRemixRequest(event: APIGatewayProxyEventV2): NodeRequest {
           ? Buffer.from(event.body, "base64")
           : Buffer.from(event.body, "base64").toString()
         : event.body,
-    signal: abortController?.signal,
   });
 }
 
