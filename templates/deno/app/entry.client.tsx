bc21c8d69bf62a5b9cbfd55bddb19c7167ac84c8
迷渡 <justjavac@gmail.com>
2022-06-10 21:53:13 +0800

    feat(templates/deno): add triple-slash reference for client code

--- templates/deno/app/entry.client.tsx
+++ templates/deno/app/entry.client.tsx
@@ -1,3 +1,6 @@
+/// <reference no-default-lib="true" />
+/// <reference lib="dom" />
+
 import React from "react";
 import ReactDOM from "react-dom";
 import { RemixBrowser } from "@remix-run/react";
