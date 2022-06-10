d7b02127336504b6f36e699ba5b4a07421622fc3
Matt Brophy <matt@brophy.org>
2022-06-10 10:38:27 -0400

    simplify filter approach in root config

--- rollup.config.js
+++ rollup.config.js
@@ -2,17 +2,14 @@ const fs = require("fs");
 const path = require("path");
 
 module.exports = function rollup(options) {
-  return fs
-    .readdirSync("packages")
-    .flatMap((dir) => {
-      let configPath = path.join("packages", dir, "rollup.config.js");
-      try {
-        fs.readFileSync(configPath);
-      } catch (e) {
-        return null;
-      }
-      let packageBuild = require(`.${path.sep}${configPath}`);
-      return packageBuild(options);
-    })
-    .filter((p) => p);
+  return fs.readdirSync("packages").flatMap((dir) => {
+    let configPath = path.join("packages", dir, "rollup.config.js");
+    try {
+      fs.readFileSync(configPath);
+    } catch (e) {
+      return [];
+    }
+    let packageBuild = require(`.${path.sep}${configPath}`);
+    return packageBuild(options);
+  });
 };
