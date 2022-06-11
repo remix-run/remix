d399b3f48d07940e1b6b8daaf53cc64e20a000bc
迷渡 <justjavac@gmail.com>
2022-06-12 01:18:16 +0800

    use `deno.json`

--- templates/deno/app/entry.client.tsx
+++ templates/deno/app/entry.client.tsx
@@ -1,6 +1,3 @@
-/// <reference no-default-lib="true" />
-/// <reference lib="dom" />
-
 import { RemixBrowser } from "@remix-run/react";
 import * as React from "react";
 import { hydrate } from "react-dom";
