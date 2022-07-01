8b2ae5f297ee8ddf5f26f2e64d0b8f865f75d2c1
Alejandro Gonzalez Sole <guatedude@gmail.com>
2022-07-01 19:52:15 -0400

    chore: adds theme-ui example project

---
+++ examples/theme-ui/app/styles/context.tsx
@@ -0,0 +1,15 @@
+import React, { createContext } from 'react';
+
+export interface ServerStyleContextData {
+  key: string;
+  ids: Array<string>;
+  css: string;
+}
+
+export const ServerStyleContext = createContext<ServerStyleContextData[] | null>(null);
+
+export interface ClientStyleContextData {
+  reset: () => void;
+}
+
+export const ClientStyleContext = createContext<ClientStyleContextData | null>(null);
