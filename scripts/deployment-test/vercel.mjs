4e46bc499773a5b4fbcb3d4ffdd221b8496136b1
Logan McAnsh <logan@mcan.sh>
2022-07-05 13:03:19 -0400

    ci: debug failing deployment tests (#3553)

--- scripts/deployment-test/vercel.mjs
+++ scripts/deployment-test/vercel.mjs
@@ -3,6 +3,7 @@ import { sync as spawnSync } from "cross-spawn";
 import fse from "fs-extra";
 import fetch from "node-fetch";
 import { createApp } from "@remix-run/dev";
+import PackageJson from "@npmcli/package-json";
 
 import {
   addCypress,
@@ -45,10 +46,14 @@ async function createVercelProject() {
   let response = await vercelClient(`/v9/projects`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ name: APP_NAME }),
+    body: JSON.stringify({
+      name: APP_NAME,
+      // we need to manually specify, otherwise it will use "other" for deployments
+      framework: "remix",
+    }),
   });
 
-  if (response.status !== 200) {
+  if (!response.ok) {
     throw new Error(`Error creating project: ${response.status}`);
   }
 
@@ -59,7 +64,7 @@ async function createVercelProject() {
 async function getVercelDeploymentUrl(projectId) {
   let response = await vercelClient(`/v8/projects/${projectId}`);
 
-  if (response.status !== 200) {
+  if (!response.ok) {
     throw new Error(`Error fetching project: ${response.status}`);
   }
 
@@ -68,31 +73,38 @@ async function getVercelDeploymentUrl(projectId) {
   return project.targets?.production?.url;
 }
 
-async function destroyApp() {
-  console.log(`Destroying app ${APP_NAME}`);
-  let response = await vercelClient(`/v9/projects/${APP_NAME}`, {
-    method: "DELETE",
-  });
-  if (response.status !== 200) {
-    console.error(`Error destroying project ${APP_NAME}: ${response.status}`);
-  } else {
-    console.log(`Destroyed app ${APP_NAME}`);
-  }
-}
+let spawnOpts = getSpawnOpts(PROJECT_DIR, {
+  // these would usually be here by default, but I'd rather be explicit, so there is no spreading internally
+  VERCEL_TOKEN: process.env.VERCEL_TOKEN,
+  VERCEL_ORG_ID: process.env.VERCEL_ORG_ID,
+});
 
 async function createAndDeployApp() {
   await createNewApp();
 
   // validate dependencies are available
-  await validatePackageVersions(PROJECT_DIR);
+  let [valid, errors] = await validatePackageVersions(PROJECT_DIR);
+
+  if (!valid) {
+    console.error(errors);
+    process.exit(1);
+  }
+
+  let pkgJson = await PackageJson.load(PROJECT_DIR);
+  pkgJson.update({
+    devDependencies: {
+      ...pkgJson.content.devDependencies,
+      vercel: "latest",
+    },
+  });
 
   await Promise.all([
     fse.copy(CYPRESS_SOURCE_DIR, path.join(PROJECT_DIR, "cypress")),
     fse.copy(CYPRESS_CONFIG, path.join(PROJECT_DIR, "cypress.json")),
     addCypress(PROJECT_DIR, CYPRESS_DEV_URL),
+    pkgJson.save(),
   ]);
 
-  let spawnOpts = getSpawnOpts(PROJECT_DIR);
   spawnSync("npm", ["install"], spawnOpts);
   spawnSync("npm", ["run", "build"], spawnOpts);
 
@@ -102,19 +114,16 @@ async function createAndDeployApp() {
   let project = await createVercelProject();
   console.log("Project created");
 
+  spawnOpts.env.VERCEL_PROJECT_ID = project.id;
+
   // deploy to vercel
-  let vercelDeployCommand = spawnSync(
+  let deployCommand = spawnSync(
     "npx",
-    ["vercel", "deploy", "--prod", "--token", process.env.VERCEL_TOKEN],
-    {
-      ...spawnOpts,
-      env: {
-        ...process.env,
-        VERCEL_PROJECT_ID: project.id,
-      },
-    }
+    ["vercel", "deploy", "--prod"],
+    spawnOpts
   );
-  if (vercelDeployCommand.status !== 0) {
+  if (deployCommand.status !== 0) {
+    console.error(deployCommand.error);
     throw new Error("Vercel deploy failed");
   }
 
@@ -132,10 +141,30 @@ async function createAndDeployApp() {
   runCypress(PROJECT_DIR, false, fullUrl);
 }
 
-createAndDeployApp()
-  .then(() => process.exit(0))
-  .catch((error) => {
+async function destroyApp() {
+  console.log(`Destroying app ${APP_NAME}`);
+  let response = await vercelClient(`/v9/projects/${APP_NAME}`, {
+    method: "DELETE",
+  });
+  if (!response.ok) {
+    console.error(`Error destroying project ${APP_NAME}: ${response.status}`);
+  } else {
+    console.log(`Destroyed app ${APP_NAME}`);
+  }
+}
+
+async function main() {
+  let exitCode;
+  try {
+    await createAndDeployApp();
+    exitCode = 0;
+  } catch (error) {
     console.error(error);
-    process.exit(1);
-  })
-  .finally(destroyApp);
+    exitCode = 1;
+  } finally {
+    await destroyApp();
+    process.exit(exitCode);
+  }
+}
+
+main();
