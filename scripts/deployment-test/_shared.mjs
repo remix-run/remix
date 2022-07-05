f6bf69e039db0985c3339dfa0ff2b78c592d699e
Logan McAnsh <logan@mcan.sh>
2022-07-05 12:30:03 -0400

    chore: add suffix for extra uniqueness
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- scripts/deployment-test/_shared.mjs
+++ scripts/deployment-test/_shared.mjs
@@ -1,6 +1,7 @@
-import path from "path";
-import { fileURLToPath } from "url";
-import { execSync } from "child_process";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import { execSync } from "node:child_process";
+import crypto from "node:crypto";
 import { sync as spawnSync } from "cross-spawn";
 import PackageJson from "@npmcli/package-json";
 import jsonfile from "jsonfile";
@@ -20,7 +21,8 @@ export let CYPRESS_CONFIG = path.join(__dirname, "cypress.json");
 
 export function getAppName(target) {
   let sha = execSync("git rev-parse HEAD").toString().trim().slice(0, 7);
-  return `remix-deployment-test-${target}-${sha}`;
+  let suffix = crypto.randomBytes(4).toString("hex");
+  return `remix-deployment-test-${target}-${sha}-${suffix}`;
 }
 
 export async function addCypress(directory, url) {
