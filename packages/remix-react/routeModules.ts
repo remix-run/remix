c3ec2791eb379a404777d6e76738328df8f1cdc5
Jacob Ebey <jacob.ebey@live.com>
2022-07-07 16:12:12 -0700

    feat: add magic comment to allow webpack bundling (#3676)

--- packages/remix-react/routeModules.ts
+++ packages/remix-react/routeModules.ts
@@ -125,7 +125,7 @@ export async function loadRouteModule(
   }
 
   try {
-    let routeModule = await import(route.module);
+    let routeModule = await import(/* webpackIgnore: true */ route.module);
     routeModulesCache[route.id] = routeModule;
     return routeModule;
   } catch (error) {
