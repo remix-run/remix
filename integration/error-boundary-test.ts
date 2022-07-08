23ec1e66081e33886f09d26aa93df46dd28dc856
Logan McAnsh <logan@mcan.sh>
2022-07-08 18:38:41 -0400

    chore: indent or i'll go mad
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- integration/error-boundary-test.ts
+++ integration/error-boundary-test.ts
@@ -664,14 +664,14 @@ test.describe("ErrorBoundary", () => {
           `,
 
           [`app/routes${LINKS_ERROR}.jsx`]: js`
-          export function links() {
-            throw new Error("Kaboom!")
-          }
+            export function links() {
+              throw new Error("Kaboom!")
+            }
 
-          export default function () {
-            return <div/>
-          }
-        `,
+            export default function () {
+              return <div/>
+            }
+          `,
 
           [`app/routes${META_ERROR}.jsx`]: js`
             export function meta() {
