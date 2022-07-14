222ba1cb868319ac4f2e972535e216173b735986
Jacob Ebey <jacob.ebey@live.com>
2022-07-14 15:17:46 -0700

    added deferred to versioning script

--- scripts/utils.js
+++ scripts/utils.js
@@ -18,7 +18,7 @@ let remixPackages = {
     "vercel",
   ],
   runtimes: ["cloudflare", "deno", "node"],
-  core: ["dev", "server-runtime", "react", "eslint-config"],
+  core: ["dev", "deferred", "server-runtime", "react", "eslint-config"],
   get all() {
     return [...this.adapters, ...this.runtimes, ...this.core, "serve"];
   },
