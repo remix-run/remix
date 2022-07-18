ecd8556c67d9ce7f0a97119f0b65e5e68b0824c8
samstanding <sastandi@microsoft.com>
2022-07-18 16:40:32 -0400

    potential fix

--- packages/remix-react/links.ts
+++ packages/remix-react/links.ts
@@ -354,7 +354,6 @@ export function getNewMatchesForLinks(
   mode: "data" | "assets"
 ): RouteMatch<ClientRoute>[] {
   let path = parsePathPatch(page);
-
   let isNew = (match: RouteMatch<ClientRoute>, index: number) => {
     if (!currentMatches[index]) return true;
     return match.route.id !== currentMatches[index].route.id;
@@ -370,7 +369,6 @@ export function getNewMatchesForLinks(
         currentMatches[index].params["*"] !== match.params["*"])
     );
   };
-
   // NOTE: keep this mostly up-to-date w/ the transition data diff, but this
   // version doesn't care about submissions
   let newMatches =
@@ -385,8 +383,7 @@ export function getNewMatchesForLinks(
           if (isNew(match, index) || matchPathChanged(match, index)) {
             return true;
           }
-
-          if (match.route.shouldReload) {
+          if (match.route.shouldReload && typeof window !== "undefined") {
             return match.route.shouldReload({
               params: match.params,
               prevUrl: new URL(
