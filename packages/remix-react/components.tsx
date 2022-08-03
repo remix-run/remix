14975eeeeb58b37202b4ebc86183f48ea34259b3
Jacob Ebey <jacob.ebey@live.com>
2022-08-03 12:31:35 -0700

    updated to use runtime cast instead of type cast

--- packages/remix-react/components.tsx
+++ packages/remix-react/components.tsx
@@ -719,7 +719,7 @@ export function Meta() {
         }
 
         if (name === "title") {
-          return <title key="title">{value as string}</title>;
+          return <title key="title">{String(value)}</title>;
         }
 
         // Open Graph tags use the `property` attribute, while other meta tags
