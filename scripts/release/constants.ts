0815ed0838c78002f52620f37268a5af24d9d237
Logan McAnsh <logan@mcan.sh>
2022-08-16 10:48:28 -0400

    ci: use graphql api for tags
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- scripts/release/constants.ts
+++ scripts/release/constants.ts
@@ -18,10 +18,11 @@ if (!process.env.VERSION) {
 if (!/^refs\/tags\//.test(process.env.VERSION)) {
   throw new Error("VERSION must start with refs/tags/");
 }
+if (!process.env.PACKAGE_VERSION_TO_FOLLOW) {
+  throw new Error("PACKAGE_VERSION_TO_FOLLOW is required");
+}
 
 export const [OWNER, REPO] = process.env.GITHUB_REPOSITORY.split("/");
-// this one is optional, nightlies only create a single tag,
-// but stable releases create one for each package
 export const PACKAGE_VERSION_TO_FOLLOW = process.env.PACKAGE_VERSION_TO_FOLLOW;
 export const VERSION = cleanupTagName(cleanupRef(process.env.VERSION));
 export const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
