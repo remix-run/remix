ec7e49231bfd25cce226da26eb1da43204d50514
Michel Rasschaert <michel.rasschaert@gmail.com>
2022-09-07 15:09:59 +0200

    fix(remix-dev/cli/create) enable usage behind proxies (#3159)

--- packages/remix-dev/cli/create.ts
+++ packages/remix-dev/cli/create.ts
@@ -7,6 +7,7 @@ import fse from "fs-extra";
 import gunzip from "gunzip-maybe";
 import fetch from "node-fetch";
 import ora from "ora";
+import ProxyAgent from "proxy-agent";
 import * as semver from "semver";
 import sortPackageJSON from "sort-package-json";
 import tar from "tar-fs";
@@ -18,6 +19,7 @@ import { getPreferredPackageManager } from "./getPreferredPackageManager";
 import { convertToJavaScript } from "./migrate/migrations/convert-to-javascript";
 
 const remixDevPackageVersion = packageJson.version;
+const agent = new ProxyAgent();
 
 interface CreateAppArgs {
   appTemplate: string;
@@ -304,7 +306,7 @@ async function downloadAndExtractTarball(
     headers.Accept = "application/vnd.github.v3+json";
     let response = await fetch(
       `https://api.github.com/repos/${info.owner}/${info.name}/releases/tags/${info.tag}`,
-      { headers }
+      { agent, headers }
     );
     if (response.status !== 200) {
       throw Error(
@@ -325,7 +327,7 @@ async function downloadAndExtractTarball(
     resourceUrl = `https://api.github.com/repos/${info.owner}/${info.name}/releases/assets/${assetId}`;
     headers.Accept = "application/octet-stream";
   }
-  let response = await fetch(resourceUrl, { headers });
+  let response = await fetch(resourceUrl, { agent, headers });
 
   if (response.status !== 200) {
     if (token) {
@@ -584,7 +586,7 @@ export async function validateTemplate(
       }
       let response;
       try {
-        response = await fetch(apiUrl, { method, headers });
+        response = await fetch(apiUrl, { agent, method, headers });
       } catch (_) {
         throw Error(
           "🚨 There was a problem verifying the template file. Please ensure " +
@@ -646,7 +648,7 @@ export async function validateTemplate(
             Authorization: `token ${options.githubToken}`,
           };
         }
-        response = await fetch(apiUrl, { method, headers });
+        response = await fetch(apiUrl, { agent, method, headers });
       } catch (_) {
         throw Error(
           "🚨 There was a problem fetching the template. Please ensure you " +
@@ -716,7 +718,7 @@ export async function validateTemplate(
       let templateUrl = `https://github.com/remix-run/remix/tree/main/${typeDir}/${name}`;
       let response;
       try {
-        response = await fetch(templateUrl, { method: "HEAD" });
+        response = await fetch(templateUrl, { agent, method: "HEAD" });
       } catch (_) {
         throw Error(
           "🚨 There was a problem verifying the template. Please ensure you are " +
