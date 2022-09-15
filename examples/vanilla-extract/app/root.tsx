8ae8a6ba83ea685a53b030160352791dd062e814
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2022-09-15 19:28:20 +1000

    docs(examples): add `vanilla-extract` example (#4173)
    * docs(example): add vanilla-extract example
    
    * docs(example): fix typo in Sprinkles config comment

---
+++ examples/vanilla-extract/app/root.tsx
@@ -0,0 +1,43 @@
+import type { MetaFunction } from "@remix-run/node";
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "@remix-run/react";
+
+import vanillaExtractStyles from "~/styles/index.css";
+
+export function links() {
+  return [
+    {
+      rel: "stylesheet",
+      href: vanillaExtractStyles,
+    },
+  ];
+}
+
+export const meta: MetaFunction = () => ({
+  charset: "utf-8",
+  title: "New Remix App",
+  viewport: "width=device-width,initial-scale=1",
+});
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
