6992ff75caead8bab84a53df67a34456665264f2
Chance Strickland <hi@chance.dev>
2022-09-15 12:40:44 -0700

    chore: remove all examples

--- examples/strapi/strapi/src/api/post/services/post.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-/**
- * post service.
- */
-
-const { createCoreService } = require("@strapi/strapi").factories;
-
-module.exports = createCoreService("api::post.post");
--- examples/tailwindcss/app/routes/index.tsx
+++
@@ -1,3 +0,0 @@
-export default function Index() {
-  return <h1 className="text-6xl font-bold text-red-700">Hello World!</h1>;
-}
