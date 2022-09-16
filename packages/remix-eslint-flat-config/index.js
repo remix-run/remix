3b4a75c10f0ab8e8520fb12a1a0de4a916e5c5e4
Mikey Binns <mikey.binns@atomicsmash.co.uk>
2022-09-16 18:58:22 +0100

    fix: remove errant colon

--- packages/remix-eslint-flat-config/index.js
+++ packages/remix-eslint-flat-config/index.js
@@ -3,4 +3,9 @@ import { unstable__testingLibraryConfig } from "./configs/jest-testing-library";
 import { unstable__nodeConfig } from "./configs/node";
 import { unstable__typescriptConfig } from "./configs/typescript";
 
-export { unstable__coreConfig:, unstable__testingLibraryConfig, unstable__nodeConfig, unstable__typescriptConfig };
+export {
+  unstable__coreConfig,
+  unstable__testingLibraryConfig,
+  unstable__nodeConfig,
+  unstable__typescriptConfig,
+};
