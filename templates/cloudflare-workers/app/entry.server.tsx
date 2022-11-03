8cb56c1b78a20549d2c1878f590cf5a4b4bc2f1f
lili.21 <lili.21@bytedance.com>
2022-11-03 15:17:57 +0800

    support streaming ssr

--- templates/cloudflare-workers/app/entry.server.tsx
+++ templates/cloudflare-workers/app/entry.server.tsx
@@ -1,21 +1,42 @@
 import type { EntryContext } from "@remix-run/cloudflare";
 import { RemixServer } from "@remix-run/react";
-import { renderToString } from "react-dom/server";
+import { renderToReadableStream } from "react-dom/server";
 
-export default function handleRequest(
+export default async function handleRequest(
   request: Request,
   responseStatusCode: number,
   responseHeaders: Headers,
   remixContext: EntryContext
 ) {
-  const markup = renderToString(
-    <RemixServer context={remixContext} url={request.url} />
-  );
-
-  responseHeaders.set("Content-Type", "text/html");
-
-  return new Response("<!DOCTYPE html>" + markup, {
-    status: responseStatusCode,
-    headers: responseHeaders,
-  });
+  let controller = new AbortController();
+  let didError = false;
+  try {
+    const stream = await renderToReadableStream(
+      <RemixServer context={remixContext} url={request.url} />,
+      {
+        signal: controller.signal,
+        onError(error: unknown) {
+          didError = true;
+          console.error(error);
+        }
+      }
+    );
+  
+    responseHeaders.set("Content-Type", "text/html");
+  
+    return new Response(stream, {
+      status: didError ? 500 : responseStatusCode,
+      headers: responseHeaders,
+    });
+  } catch (error: unknown) {
+    console.error(error);
+    const message = error instanceof Error ? error.message : "Unknown Error";
+    return new Response(
+      `<!doctype html><p>An error ocurred:</p><pre>${message}</pre>`,
+      {
+        status: 500,
+        headers: { "Content-Type": "text/html" },
+      }
+    );
+  }
 }
