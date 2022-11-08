bce671f73d39332735c643bf89b52ba999496298
Logan McAnsh <logan@mcan.sh>
2022-11-08 13:24:25 -0500

    chore: use experimental, publish all packages to yalc store
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- yalc.sh
+++ yalc.sh
@@ -1,4 +1,19 @@
-packages=('testing' 'server-runtime' 'react')
+packages=(
+'architect'
+'cloudflare'
+'cloudflare-pages'
+'cloudflare-workers'
+'deno'
+'dev'
+'express'
+'netlify'
+'node'
+'react'
+'serve'
+'server-runtime'
+'testing'
+'vercel'
+)
 for i in "${packages[@]}"; do
   yalc publish ./build/node_modules/@remix-run/$i
 done
