3d4843fe24dee34f8a34d7fccef5f68e7a20bdee
Matt Brophy <matt@brophy.org>
2022-11-22 14:58:43 -0500

    Bump remix to react-router-dom@6.4.4 (#4668)
    Co-authored-by: Mehdi Achour <machour@gmail.com>

--- packages/remix-server-runtime/routeModules.ts
+++ packages/remix-server-runtime/routeModules.ts
@@ -1,5 +1,5 @@
 import type { ComponentType } from "react";
-import type { Location, Params } from "react-router-dom";
+import type { Location, Params } from "@remix-run/router";
 
 import type { AppLoadContext, AppData } from "./data";
 import type { LinkDescriptor } from "./links";
