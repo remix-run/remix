0e69713f67985201997d8ee6d291fbdc5424e5c1
Michael Henson <liegeandlief@users.noreply.github.com>
2022-12-01 15:57:07 +0000

    Failing test case for bug

--- integration/bug-report-test.ts
+++ integration/bug-report-test.ts
@@ -1,108 +1,73 @@
-import { test, expect } from "@playwright/test";
+import { test, expect } from '@playwright/test'
+import { PlaywrightFixture } from './helpers/playwright-fixture'
+import type { Fixture, AppFixture } from './helpers/create-fixture'
+import { createAppFixture, createFixture, js } from './helpers/create-fixture'
 
-import { PlaywrightFixture } from "./helpers/playwright-fixture";
-import type { Fixture, AppFixture } from "./helpers/create-fixture";
-import { createAppFixture, createFixture, js } from "./helpers/create-fixture";
-
-let fixture: Fixture;
-let appFixture: AppFixture;
-
-////////////////////////////////////////////////////////////////////////////////
-// 💿 👋 Hola! It's me, Dora the Remix Disc, I'm here to help you write a great
-// bug report pull request.
-//
-// You don't need to fix the bug, this is just to report one.
-//
-// The pull request you are submitting is supposed to fail when created, to let
-// the team see the erroneous behavior, and understand what's going wrong.
-//
-// If you happen to have a fix as well, it will have to be applied in a subsequent
-// commit to this pull request, and your now-succeeding test will have to be moved
-// to the appropriate file.
-//
-// First, make sure to install dependencies and build Remix. From the root of
-// the project, run this:
-//
-//    ```
-//    yarn && yarn build
-//    ```
-//
-// Now try running this test:
-//
-//    ```
-//    yarn bug-report-test
-//    ```
-//
-// You can add `--watch` to the end to have it re-run on file changes:
-//
-//    ```
-//    yarn bug-report-test --watch
-//    ```
-////////////////////////////////////////////////////////////////////////////////
+let fixture: Fixture
+let appFixture: AppFixture
 
 test.beforeAll(async () => {
   fixture = await createFixture({
-    ////////////////////////////////////////////////////////////////////////////
-    // 💿 Next, add files to this object, just like files in a real app,
-    // `createFixture` will make an app and run your tests against it.
-    ////////////////////////////////////////////////////////////////////////////
     files: {
-      "app/routes/index.jsx": js`
-        import { json } from "@remix-run/node";
-        import { useLoaderData, Link } from "@remix-run/react";
+      'app/routes/index.jsx': js`
+        import { Form } from '@remix-run/react'
+        import { redirect, json } from '@remix-run/server-runtime'
+        import { PrefetchPageLinks } from '@remix-run/react'
 
-        export function loader() {
-          return json("pizza");
+        export const loader = async () => {
+          const response = json(null)
+          response.headers.set('set-cookie', 'testcookie=setByLoader')
+          return response
         }
 
+        export const action = async () => {
+          const response = redirect('/result-page')
+          response.headers.set('set-cookie', 'testcookie=setByAction')
+          return response
+        }       
+
         export default function Index() {
-          let data = useLoaderData();
           return (
-            <div>
-              {data}
-              <Link to="/burgers">Other Route</Link>
-            </div>
+            <Form method='post'>
+              <PrefetchPageLinks page='/result-page' />
+              <button type='submit'>Submit</button>
+            </Form>            
           )
         }
       `,
+      
+      'app/routes/result-page.jsx': js`
+        import { json } from '@remix-run/server-runtime'
+        import { useLoaderData } from '@remix-run/react'
 
-      "app/routes/burgers.jsx": js`
-        export default function Index() {
-          return <div>cheeseburger</div>;
+        export const loader = async ({ request }) => {
+          return json(request.headers.get('cookie'))
         }
-      `,
+
+        export default function ResultPage() {
+          const loaderData = useLoaderData()
+
+          return (
+            <p>{JSON.stringify(loaderData)}</p>
+          )
+        }
+      `, 
     },
-  });
+  })
 
-  // This creates an interactive app using puppeteer.
-  appFixture = await createAppFixture(fixture);
-});
+  appFixture = await createAppFixture(fixture)
+})
 
-test.afterAll(() => appFixture.close());
+test.afterAll(() => appFixture.close())
 
-////////////////////////////////////////////////////////////////////////////////
-// 💿 Almost done, now write your failing test case(s) down here Make sure to
-// add a good description for what you expect Remix to do 👇🏽
-////////////////////////////////////////////////////////////////////////////////
+test(`testcookie=setByAction should be displayed because the /result-page loader should have been rerun after the /index action.
 
-test("[description of what you expect it to do]", async ({ page }) => {
-  let app = new PlaywrightFixture(appFixture, page);
-  // You can test any request your app might get using `fixture`.
-  let response = await fixture.requestDocument("/");
-  expect(await response.text()).toMatch("pizza");
+It seems to work in Firefox and Webkit but not in Chromium and Edge.
 
-  // If you need to test interactivity use the `app`
-  await app.goto("/");
-  await app.clickLink("/burgers");
-  expect(await app.getHtml()).toMatch("cheeseburger");
-
-  // If you're not sure what's going on, you can "poke" the app, it'll
-  // automatically open up in your browser for 20 seconds, so be quick!
-  // await app.poke(20);
-
-  // Go check out the other tests to see what else you can do.
-});
-
-////////////////////////////////////////////////////////////////////////////////
-// 💿 Finally, push your changes to your fork of Remix and open a pull request!
-////////////////////////////////////////////////////////////////////////////////
+In Chromium and Edge testcookie=setByLoader is displayed which is the data returned by the /result-page loader when called due to the presence of the PrefetchPageLinks component in /index.
+`, async ({ page }) => {
+  let app = new PlaywrightFixture(appFixture, page)
+  await app.goto('/')
+  await app.clickElement('button')
+  await expect(page.locator('p')).toContainText('testcookie=setByAction')
+})
