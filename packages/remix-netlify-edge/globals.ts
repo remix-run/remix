d9c4634c3b8077542d68b6bfe3a9997df331ec53
Nick Taylor <nick@iamdeveloper.com>
2022-12-02 15:14:59 -0500

    chore: removed comment that is no longer valid

--- packages/remix-netlify-edge/globals.ts
+++ packages/remix-netlify-edge/globals.ts
@@ -1,6 +1,4 @@
 import "@remix-run/deno/globals.ts";
 
-// Deno will exist in Deno land and we only need the minimal types for ProcessEnv
-// The types for this for Deno are defined in globals.d.ts in the Netlify template
 // @ts-ignore
 globalThis.process ||= { env: Deno.env.toObject() };
