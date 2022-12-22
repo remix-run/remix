ce823da8867b1965ef423c40c72ed07a3591d591
Daniel Reichhart <daniel.reichhart@gmx.net>
2022-12-22 10:01:09 +0100

    prettier

--- templates/cloudflare-workers/app/entry.client.tsx
+++ templates/cloudflare-workers/app/entry.client.tsx
@@ -3,20 +3,20 @@ import { startTransition, StrictMode } from "react";
 import { hydrateRoot } from "react-dom/client";
 
 function hydrate() {
-    startTransition(() => {
-        hydrateRoot(
-            document,
-            <StrictMode>
-                <RemixBrowser />
-            </StrictMode>
-        );
-    });
+  startTransition(() => {
+    hydrateRoot(
+      document,
+      <StrictMode>
+        <RemixBrowser />
+      </StrictMode>
+    );
+  });
 }
 
 if (window.requestIdleCallback) {
-    window.requestIdleCallback(hydrate);
+  window.requestIdleCallback(hydrate);
 } else {
-    // Safari doesn't support requestIdleCallback
-    // https://caniuse.com/requestidlecallback
-    window.setTimeout(hydrate, 1);
+  // Safari doesn't support requestIdleCallback
+  // https://caniuse.com/requestidlecallback
+  window.setTimeout(hydrate, 1);
 }
