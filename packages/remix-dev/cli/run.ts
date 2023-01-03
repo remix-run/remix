e74211df3767d53bf1238d5ad0bfcf13fba13833
Logan McAnsh <logan@mcan.sh>
2023-01-03 13:12:06 -0500

    chore: rename to eject
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- packages/remix-dev/cli/run.ts
+++ packages/remix-dev/cli/run.ts
@@ -481,8 +481,7 @@ export async function run(argv: string[] = process.argv.slice(2)) {
       await commands.codemod(input[1], input[2]);
       break;
     }
-    case "g":
-    case "generate": {
+    case "eject": {
       let remixRoot = process.env.REMIX_ROOT || process.cwd();
       await commands.generateEntry(remixRoot, input[1]);
       break;
