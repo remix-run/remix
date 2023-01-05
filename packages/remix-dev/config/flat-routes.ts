1bfc22f859b26bdd91d5aaa134acddaa56ba208a
Logan McAnsh <logan@mcan.sh>
2023-01-05 15:45:04 -0500

    Update packages/remix-dev/config/flat-routes.ts
    Co-authored-by: Pedro Cattori <pcattori@gmail.com>

--- packages/remix-dev/config/flat-routes.ts
+++ packages/remix-dev/config/flat-routes.ts
@@ -129,7 +129,7 @@ export function getRouteSegments(routeId: string) {
     let notSupportedInRR = (segment: string, char: string) => {
       throw new Error(
         `Route segment "${segment}" for "${routeId}" cannot contain "${char}".\n` +
-          `If this is something you need, upvote this proposal on React Router https://github.com/remix-run/react-router/discussions/9822.`
+          `If this is something you need, upvote this proposal for React Router https://github.com/remix-run/react-router/discussions/9822.`
       );
     };
 
