f5fbdaca4f5fdae6a40fdbb466e9b36d0707ef22
Logan McAnsh <logan@mcan.sh>
2023-01-23 11:10:02 -0500

    chore: update cloudflare/deno default entry.server
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- packages/remix-dev/config/defaults/entry.server.cloudflare.tsx
+++ packages/remix-dev/config/defaults/entry.server.cloudflare.tsx
@@ -1,5 +1,6 @@
-import type { EntryContext } from "@remix-run/cloudflare";
+import { type EntryContext } from "@remix-run/cloudflare";
 import { RemixServer } from "@remix-run/react";
+import isbot from "isbot";
 import { renderToReadableStream } from "react-dom/server";
 
 export default async function handleRequest(
@@ -8,23 +9,24 @@ export default async function handleRequest(
   responseHeaders: Headers,
   remixContext: EntryContext
 ) {
-  let didError = false;
-
-  const stream = await renderToReadableStream(
+  const body = await renderToReadableStream(
     <RemixServer context={remixContext} url={request.url} />,
     {
-      signal: request.signal,
-      onError(error: unknown) {
-        didError = true;
+      onError(error) {
+        console.error("renderToReadableStream error");
         console.error(error);
+        responseStatusCode = 500;
       },
     }
   );
 
-  responseHeaders.set("Content-Type", "text/html");
+  if (isbot(request.headers.get("user-agent"))) {
+    await body.allReady;
+  }
 
-  return new Response(stream, {
+  responseHeaders.set("Content-Type", "text/html");
+  return new Response(body, {
     headers: responseHeaders,
-    status: didError ? 500 : responseStatusCode,
+    status: responseStatusCode,
   });
 }
