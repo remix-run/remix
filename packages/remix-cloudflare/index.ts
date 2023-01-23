ceb36ba47de788400c412b5c4b3781aeecfd63ff
Michael Jackson <michael@jackson.us>
2023-01-23 07:28:42 -0800

    feat(remix-cloudflare): rename `createCloudflareKVSessionStorage` to `createWorkersKVSessionStorage` (#2542)
    Co-authored-by: Michaël De Boey <info@michaeldeboey.be>

--- packages/remix-cloudflare/index.ts
+++ packages/remix-cloudflare/index.ts
@@ -1,6 +1,22 @@
 import "./globals";
 
-export { createCloudflareKVSessionStorage } from "./sessions/cloudflareKVSessionStorage";
+import {createWorkersKVSessionStorage} from './sessions/workersKVStorage';
+
+const warn = <T extends Function>(fn: T, message: string): T =>
+  ((...args: unknown[]) => {
+    console.warn(message);
+
+    return fn(...args);
+  }) as unknown as T;
+
+
+/** @deprecated Use `createWorkersKVSessionStorage` instead. */
+export const createCloudflareKVSessionStorage = warn(
+  createWorkersKVSessionStorage,
+  "`createCloudflareKVSessionStorage` is deprecated. Please use `createWorkersKVSessionStorage` instead.",
+);
+
+export { createWorkersKVSessionStorage } from "./sessions/workersKVStorage";
 
 export {
   createCookie,
