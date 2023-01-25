dcb1987935214b8e658361b079fc08a6a800e747
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2023-01-25 14:54:50 +1100

    feat: add built-in PostCSS and Tailwind support (#5229)

--- packages/remix-react/entry.ts
+++ packages/remix-react/entry.ts
@@ -22,6 +22,8 @@ export interface EntryContext extends RemixContextObject {
 export interface FutureConfig {
   unstable_cssModules: boolean;
   unstable_cssSideEffectImports: boolean;
+  unstable_postcss: boolean;
+  unstable_tailwind: boolean;
   unstable_vanillaExtract: boolean;
   v2_errorBoundary: boolean;
   v2_meta: boolean;
