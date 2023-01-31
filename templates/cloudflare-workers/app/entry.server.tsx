94df96f6085ac7f070e349fb3231efb15931f43b
Logan McAnsh <logan@mcan.sh>
2023-01-31 17:48:40 -0500

    chore: update cloudflare workers entry.server to use streaming
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- templates/cloudflare-workers/app/entry.server.tsx
+++ templates/cloudflare-workers/app/entry.server.tsx
@@ -1,21 +1,35 @@
 import type { EntryContext } from "@remix-run/cloudflare";
 import { RemixServer } from "@remix-run/react";
-import { renderToString } from "react-dom/server";
+import { renderToReadableStream } from "react-dom/server";
+import isbot from "isbot";
 
-export default function handleRequest(
+export default async function handleRequest(
   request: Request,
   responseStatusCode: number,
   responseHeaders: Headers,
   remixContext: EntryContext
 ) {
-  const markup = renderToString(
-    <RemixServer context={remixContext} url={request.url} />
+  let didError = false;
+
+  const stream = await renderToReadableStream(
+    <RemixServer context={remixContext} url={request.url} />,
+    {
+      signal: request.signal,
+      onError(error: unknown) {
+        didError = true;
+        console.error(error);
+      },
+    }
   );
+
+  if (isbot(request.headers.get("user-agent"))) {
+    await stream.allReady;
+  }
 
   responseHeaders.set("Content-Type", "text/html");
 
-  return new Response("<!DOCTYPE html>" + markup, {
-    status: responseStatusCode,
+  return new Response(stream, {
     headers: responseHeaders,
+    status: didError ? 500 : responseStatusCode,
   });
 }
