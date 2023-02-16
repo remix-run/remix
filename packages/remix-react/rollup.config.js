026b1c44233091bddcf963fab897ac6577d3e229
Jacob Ebey <jacob.ebey@live.com>
2023-02-15 16:05:20 -0800

    strip `import.meta` from cjs builds

--- packages/remix-react/rollup.config.js
+++ packages/remix-react/rollup.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 const babel = require("@rollup/plugin-babel").default;
 const nodeResolve = require("@rollup/plugin-node-resolve").default;
 const copy = require("rollup-plugin-copy");
+const replace = require("@rollup/plugin-replace");
 
 const {
   copyToPlaygrounds,
@@ -34,6 +35,9 @@ module.exports = function rollup() {
       exports: "auto",
     },
     plugins: [
+      replace({
+        "import.meta": "null",
+      }),
       babel({
         babelHelpers: "bundled",
         exclude: /node_modules/,
