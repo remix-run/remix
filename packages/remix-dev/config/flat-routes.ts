e8447f0383815958ca655574860162c33a806a2d
Logan McAnsh <logan@mcan.sh>
2023-02-17 12:00:07 -0500

    chore: add some comments, mark the next route as the conflict
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- packages/remix-dev/config/flat-routes.ts
+++ packages/remix-dev/config/flat-routes.ts
@@ -360,28 +360,32 @@ function getRouteMap(
     return b.segments.length - a.segments.length;
   });
 
-  // update parentIds for all routes
   for (let i = 0; i < routes.length; i++) {
     let routeInfo = routes[i];
+    // update parentIds for all routes
     routeInfo.parentId = findParentRouteId(routeInfo, nameMap);
 
+    // remove routes that conflict with other routes
     let nextRouteInfo = routes[i + 1];
     if (!nextRouteInfo) continue;
 
     let segments = routeInfo.segments;
     let nextSegments = nextRouteInfo.segments;
+    // if segment count is different, there can't be a conflict
     if (segments.length !== nextSegments.length) continue;
 
     for (let k = 0; k < segments.length; k++) {
       let segment = segments[k];
       let nextSegment = nextSegments[k];
 
+      // if segments are different, but they're both dynamic, there's a conflict
       if (
         segment !== nextSegment &&
         segment.startsWith(":") &&
         nextSegment.startsWith(":")
       ) {
         let currentConflicts = conflicts.get(routeInfo.path || "/");
+        // collect conflicts for later reporting, marking the next route as the conflicting route
         if (!currentConflicts) {
           conflicts.set(routeInfo.path || "/", [routeInfo, nextRouteInfo]);
         } else {
@@ -389,7 +393,8 @@ function getRouteMap(
           conflicts.set(routeInfo.path || "/", currentConflicts);
         }
 
-        routeMap.delete(routeInfo.id);
+        // remove conflicting route
+        routeMap.delete(nextRouteInfo.id);
 
         continue;
       }
