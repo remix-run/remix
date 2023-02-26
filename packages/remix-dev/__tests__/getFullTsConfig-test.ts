16898406a55fac9fb699e1177b376db7d9e509f2
Ali Zeaiter <ali.zeaiter@volvocars.com>
2023-02-27 00:33:50 +0100

    Test non-relative paths and error

--- packages/remix-dev/__tests__/getFullTsConfig-test.ts
+++ packages/remix-dev/__tests__/getFullTsConfig-test.ts
@@ -20,9 +20,11 @@ describe("getFullTsConfig", () => {
     let { fullConfig } = await getFullTsConfig(
       path.resolve(__dirname, "./fixtures/tsconfig/tsconfig.json")
     );
-    expect(fullConfig.options).toMatchInlineSnapshot(`
+
+    // exclude properties that have device specific paths
+    let { baseUrl, pathsBasePath, ...options } = fullConfig.options;
+    expect(options).toMatchInlineSnapshot(`
       Object {
-        "baseUrl": "C:/Users/AliZ/work/remix/packages/remix-dev/__tests__/fixtures/tsconfig",
         "configFilePath": undefined,
         "esModuleInterop": true,
         "forceConsistentCasingInFileNames": true,
@@ -37,10 +39,51 @@ describe("getFullTsConfig", () => {
             "./app/*",
           ],
         },
-        "pathsBasePath": "C:/Users/AliZ/work/remix/packages/remix-dev/__tests__/fixtures/tsconfig",
         "strict": true,
         "target": 6,
       }
     `);
+  });
+
+  test('reads correct tsconfig options when "extends" is provided for a non-relative path', async () => {
+    let { fullConfig } = await getFullTsConfig(
+      path.resolve(__dirname, "./fixtures/tsconfig/tsconfig.no-relative.json")
+    );
+
+    // exclude properties that have device specific paths
+    let { baseUrl, pathsBasePath, outDir, rootDir, ...options } =
+      fullConfig.options;
+
+    expect(options).toMatchInlineSnapshot(`
+      Object {
+        "configFilePath": undefined,
+        "esModuleInterop": true,
+        "forceConsistentCasingInFileNames": true,
+        "jsx": 4,
+        "lib": Array [
+          "lib.dom.d.ts",
+          "lib.dom.iterable.d.ts",
+          "lib.es2019.d.ts",
+        ],
+        "moduleResolution": 2,
+        "paths": Object {
+          "~/*": Array [
+            "./app/*",
+          ],
+        },
+        "strict": true,
+        "target": 6,
+      }
+    `);
+  });
+
+  test("throws when config is invalid", async () => {
+    await expect(
+      getFullTsConfig(
+        path.resolve(__dirname, "./fixtures/tsconfig/tsconfig.invalid.json")
+      )
+    ).rejects.toThrow(
+      "error TS5024: Compiler option 'strict' requires a value of type boolean."
+    );
   });
 });
