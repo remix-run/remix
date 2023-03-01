119e7d27b44b2fcc3de2d2808dd18dd11fe51d38
Ethan Arrowood <ethan.arrowood@vercel.com>
2023-03-01 14:21:16 -0700

    Add checkout (#4)
    * Add checkout
    
    * add workflow dispatch operation

--- .github/workflow_scripts/sync-repo.js
+++ .github/workflow_scripts/sync-repo.js
@@ -1,3 +1,4 @@
+const { execSync } = require('child_process');
 const fs = require('fs');
 const path = require('path');
 
@@ -5,15 +6,44 @@ module.exports = async ({ github, context }) => {
     const { owner, repo } = context.repo;
 
     try {
+        const packageJSONPath = path.join(
+            __dirname,
+            '..',
+            '..',
+            'packages',
+            'remix-dev',
+            'package.json'
+        );
+
+        let packageJSON = JSON.parse(fs.readFileSync(packageJSONPath, 'utf-8'));
+
+        const existingVersion = packageJSON.version;
+    
         await github.rest.repos.mergeUpstream({
             owner,
             repo,
             branch: 'main',
-        })
+        });
+
+        execSync('git pull origin main');
+
+        packageJSON = JSON.parse(fs.readFileSync(packageJSONPath, 'utf-8'));
+
+        const newVersion = packageJSON.version;
+
+        if (existingVersion !== newVersion) {
+            await github.rest.actions.createWorkflowDispatch({
+                owner,
+                repo,
+                workflow_id: 'publish.yml',
+                ref: 'main'
+            });
+        }
+
     } catch (err) {
         // Conflict detected
         if (err.code === 409) {
-
+            // TODO(@Ethan-Arrowood): Send a slack message/generate a github issue
         }
 
         throw err
