a83383a1cefe6ba56db963fa61bf146d9b38da99
Logan McAnsh <logan@mcan.sh>
2023-03-10 14:31:55 -0500

    Revert "templates: enable `v2_routeConvention` config flag by default (#5693)"
    This reverts commit b74dbae9e9f4afe4f128ea7d02086612dc0ff608.

--- templates/cloudflare-pages/remix.config.js
+++ templates/cloudflare-pages/remix.config.js
@@ -1,9 +1,6 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   devServerBroadcastDelay: 1000,
-  future: {
-    v2_routeConvention: true,
-  },
   ignoredRouteFiles: ["**/.*"],
   server: "./server.js",
   serverBuildPath: "functions/[[path]].js",
--- templates/fly/remix.config.js
+++ templates/fly/remix.config.js
@@ -1,8 +1,5 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
-  future: {
-    v2_routeConvention: true,
-  },
   ignoredRouteFiles: ["**/.*"],
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
