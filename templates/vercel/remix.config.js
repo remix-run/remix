5448f37a5a9d0961281908de2dd894d2a0c1bf38
Logan McAnsh <logan@mcan.sh>
2023-03-10 14:36:29 -0500

    Revert "templates: enable `v2_routeConvention` config flag by default" (#5761)
    (cherry picked from commit 7d96a22f59bf3521a61edfa2aa79d35003ad1aaf)

--- templates/vercel/remix.config.js
+++ templates/vercel/remix.config.js
@@ -1,8 +1,5 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
-  future: {
-    v2_routeConvention: true,
-  },
   ignoredRouteFiles: ["**/.*"],
   // When running locally in development mode, we use the built-in remix
   // server. This does not understand the vercel lambda module format,
