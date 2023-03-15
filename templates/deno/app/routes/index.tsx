6c253728b9d832b7c163c38a620026018f205e50
Chance Strickland <hi@chance.dev>
2023-03-15 13:08:49 -0700

    oops found some more

--- templates/deno/app/routes/index.tsx
+++ templates/deno/app/routes/index.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
-import type { V2_MetaFunction } from "@remix-run/node";
+import type { MetaFunction } from "@remix-run/node";
 
-export const meta: V2_MetaFunction = () => {
+export const meta: MetaFunction = () => {
   return [{ title: "New Remix App" }];
 };
 
