38e091857dd264c9562e23bacdd1057ca1e48fc8
Logan McAnsh <logan@mcan.sh>
2023-03-22 12:06:32 -0400

    fix: add NODE_ENV check to warnOnce
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- packages/remix-react/warnings.ts
+++ packages/remix-react/warnings.ts
@@ -1,7 +1,11 @@
 const alreadyWarned: { [message: string]: boolean } = {};
 
 export function warnOnce(condition: boolean, message: string): void {
-  if (!condition && !alreadyWarned[message]) {
+  if (
+    !condition &&
+    !alreadyWarned[message] &&
+    process.env.NODE_ENV !== "production"
+  ) {
     alreadyWarned[message] = true;
     console.warn(message);
   }
