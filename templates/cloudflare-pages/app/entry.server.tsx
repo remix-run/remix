3510e6f06717a57fbfec6ee3913614f74896d149
Logan McAnsh <logan@mcan.sh>
2023-03-24 12:53:48 -0400

    chore(templates): restore optional entry files (#5892)

--- templates/cloudflare-pages/app/entry.server.tsx
+++ templates/cloudflare-pages/app/entry.server.tsx
@@ -1,3 +1,9 @@
+/**
+ * By default, Remix will handle generating the HTTP Response for you.
+ * You are free to delete this file if you'd like to, but if you ever want it revealed again, you can run `npx remix reveal` ✨
+ * For more information, see https://remix.run/docs/en/main/file-conventions/entry.server
+ */
+
 import type { EntryContext } from "@remix-run/cloudflare";
 import { RemixServer } from "@remix-run/react";
 import isbot from "isbot";
--- templates/deno/app/entry.server.tsx
+++ templates/deno/app/entry.server.tsx
@@ -1,4 +1,10 @@
-import type { EntryContext } from "@remix-run/cloudflare";
+/**
+ * By default, Remix will handle generating the HTTP Response for you.
+ * You are free to delete this file if you'd like to, but if you ever want it revealed again, you can run `npx remix reveal` ✨
+ * For more information, see https://remix.run/docs/en/main/file-conventions/entry.server
+ */
+
+import type { EntryContext } from "@remix-run/deno";
 import { RemixServer } from "@remix-run/react";
 import isbot from "isbot";
 import { renderToReadableStream } from "react-dom/server";
