19046a0f73ffe93fb308912379a957b899bf0dc4
Pedro Cattori <pcattori@gmail.com>
2023-03-30 12:47:15 -0400

    refactor(dev): assets subcompiler

--- packages/remix-dev/compiler/compiler.ts
+++ packages/remix-dev/compiler/compiler.ts
@@ -1,15 +1,8 @@
 import type { RemixConfig } from "../config";
 import { type Manifest } from "../manifest";
-import {
-  create as createManifest,
-  write as writeManifest,
-} from "./utils/manifest";
-import * as BrowserJS from "./browserjs";
 import * as ServerJS from "./serverjs";
 import type { CompileOptions } from "./options";
-import type { Channel } from "../channel";
-import { createChannel } from "../channel";
-import * as CSS from "./css";
+import * as AssetsCompiler from "./assets";
 
 type Compiler = {
   compile: () => Promise<Manifest | undefined>;
@@ -20,37 +13,13 @@ export let create = async (
   config: RemixConfig,
   options: CompileOptions
 ): Promise<Compiler> => {
-  let cssBundleHrefChannel: Channel<string | undefined>;
-  let writeCssBundleHref = (cssBundleHref?: string) =>
-    cssBundleHrefChannel.write(cssBundleHref);
-  let readCssBundleHref = () => cssBundleHrefChannel.read();
-  let css = await CSS.compiler.create(config, options, writeCssBundleHref);
-  let browser = await BrowserJS.compiler.create(
-    config,
-    options,
-    readCssBundleHref
-  );
+  let assets = await AssetsCompiler.create(config, options);
   let server = ServerJS.compiler.create(config, options);
   return {
     compile: async () => {
-      // TODO: only reset cssBundleHrefChannel if css bundling is enabled?
-      // otherwise we could just write `undefined` to the channel immediately
-      cssBundleHrefChannel = createChannel();
       try {
-        let [cssBundleHref, { metafile, hmr }] = await Promise.all([
-          css.compile(),
-          browser.compile(),
-        ]);
-        let manifest = await createManifest({
-          config,
-          metafile: metafile,
-          cssBundleHref,
-          hmr,
-        });
-        await Promise.all([
-          server.compile(manifest),
-          writeManifest(config, manifest),
-        ]);
+        let manifest = await assets.compile();
+        await server.compile(manifest);
 
         return manifest;
       } catch (error: unknown) {
@@ -59,8 +28,7 @@ export let create = async (
       }
     },
     dispose: () => {
-      css.dispose();
-      browser.dispose();
+      assets.dispose();
       server.dispose();
     },
   };
