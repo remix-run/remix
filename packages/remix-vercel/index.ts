7d3a9a59909f6cd1812f346560368f1c91b4ddaa
Aaron <lifeiscontent@users.noreply.github.com>
2023-04-02 14:56:10 -0700

    Add EntryContext to remix-vercel

--- packages/remix-vercel/index.ts
+++ packages/remix-vercel/index.ts
@@ -2,3 +2,4 @@ import "./globals";
 
 export type { GetLoadContextFunction, RequestHandler } from "./server";
 export { createRequestHandler } from "./server";
+export { EntryContext } from "@remix-run/server-runtime";
