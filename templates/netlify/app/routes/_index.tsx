eec848a96f487302b638ee80e3aca79af7fc169d
Timon van Spronsen <timonvanspronsen@outlook.com>
2023-04-05 14:23:24 +0000

    Fix `V2_MetaFunction` type import in templates

--- templates/netlify/app/routes/_index.tsx
+++ templates/netlify/app/routes/_index.tsx
@@ -1,4 +1,4 @@
-import type { V2_MetaFunction } from "@remix-run/node";
+import type { V2_MetaFunction } from "@remix-run/react";
 
 export const meta: V2_MetaFunction = () => {
   return [{ title: "New Remix App" }];
