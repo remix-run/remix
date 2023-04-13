848fb8f768b61f62f4d7bd4c78709f8f87abf135
Michaël De Boey <info@michaeldeboey.be>
2023-04-13 23:39:59 +0200

    templates: use TS `server` file (#5822)

--- templates/express/server.ts
+++ templates/express/server.ts
@@ -1,10 +1,8 @@
-const path = require("path");
-const express = require("express");
-const compression = require("compression");
-const morgan = require("morgan");
-const { createRequestHandler } = require("@remix-run/express");
-
-const BUILD_DIR = path.join(process.cwd(), "build");
+import path from "path";
+import express from "express";
+import compression from "compression";
+import morgan from "morgan";
+import { createRequestHandler } from "@remix-run/express";
 
 const app = express();
 
@@ -25,34 +23,33 @@ app.use(express.static("public", { maxAge: "1h" }));
 
 app.use(morgan("tiny"));
 
+const BUILD_DIR = path.join(process.cwd(), "build");
+const MODE = process.env.NODE_ENV;
 app.all(
   "*",
-  process.env.NODE_ENV === "development"
-    ? (req, res, next) => {
+  MODE === "production"
+    ? createRequestHandler({ build: require(BUILD_DIR), mode: MODE })
+    : (...args) => {
         purgeRequireCache();
 
         return createRequestHandler({
           build: require(BUILD_DIR),
-          mode: process.env.NODE_ENV,
-        })(req, res, next);
+          mode: MODE,
+        })(...args);
       }
-    : createRequestHandler({
-        build: require(BUILD_DIR),
-        mode: process.env.NODE_ENV,
-      })
 );
-const port = process.env.PORT || 3000;
 
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Express server listening on port ${port}`);
+  console.log(`✅ Express server listening on port ${port}`);
 });
 
 function purgeRequireCache() {
   // purge require cache on requests for "server side HMR" this won't let
   // you have in-memory objects between requests in development,
   // alternatively you can set up nodemon/pm2-dev to restart the server on
-  // file changes, but then you'll have to reconnect to databases/etc on each
-  // change. We prefer the DX of this, so we've included it for you by default
+  // file changes, we prefer the DX of this though, so we've included it
+  // for you by default
   for (const key in require.cache) {
     if (key.startsWith(BUILD_DIR)) {
       delete require.cache[key];
--- templates/vercel/remix.config.js
+++ templates/vercel/remix.config.js
@@ -4,7 +4,7 @@ module.exports = {
   // When running locally in development mode, we use the built-in remix
   // server. This does not understand the vercel lambda module format,
   // so we default back to the standard build output.
-  server: process.env.NODE_ENV === "development" ? undefined : "./server.js",
+  server: process.env.NODE_ENV === "development" ? undefined : "./server.ts",
   serverBuildPath: "api/index.js",
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
