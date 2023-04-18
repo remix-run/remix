b730b7808878e480c1e935c50ee2e42c09466545
Leo Singer <leo.p.singer@nasa.gov>
2023-04-17 21:00:53 -0400

    Update packages/remix-eslint-config/index.js
    Co-authored-by: Michaël De Boey <info@michaeldeboey.be>

--- packages/remix-eslint-config/index.js
+++ packages/remix-eslint-config/index.js
@@ -78,7 +78,7 @@ const config = {
       },
     },
     {
-      files: ["**/routes/**/*.js?(x)", "**/routes/**/*.ts?(x)", "**/root.js?(x)", "**/root.ts?(x)"],
+      files: ["**/routes/**/*.js?(x)", "**/routes/**/*.tsx", "app/root.js?(x)", "app/root.tsx"],
       rules: {
         // Routes may use default exports without a name. At the route level
         // identifying components for debugging purposes is less of an issue, as
