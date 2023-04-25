94a251cecf8b7eeab16ea0dcebe1af823a2a3a58
Jacob Ebey <jacob.ebey@live.com>
2023-04-25 13:51:58 -0700

    feat: express template outputs ESM for build feat: express template server entry is now ESM feat: express template uses new dev command

--- templates/express/server.js
+++ templates/express/server.js
@@ -1,10 +1,12 @@
-const path = require("path");
-const express = require("express");
-const compression = require("compression");
-const morgan = require("morgan");
-const { createRequestHandler } = require("@remix-run/express");
+import express from "express";
+import compression from "compression";
+import morgan from "morgan";
+import { createRequestHandler } from "@remix-run/express";
+import { installGlobals } from "@remix-run/node";
 
-const BUILD_DIR = path.join(process.cwd(), "build");
+import * as build from "./build/index.js";
+
+installGlobals();
 
 const app = express();
 
@@ -25,37 +27,18 @@ app.use(express.static("public", { maxAge: "1h" }));
 
 app.use(morgan("tiny"));
 
+const MODE = process.env.NODE_ENV;
 app.all(
   "*",
-  process.env.NODE_ENV === "development"
-    ? (req, res, next) => {
-        purgeRequireCache();
-
-        return createRequestHandler({
-          build: require(BUILD_DIR),
-          mode: process.env.NODE_ENV,
-        })(req, res, next);
-      }
-    : createRequestHandler({
-        build: require(BUILD_DIR),
-        mode: process.env.NODE_ENV,
-      })
+  createRequestHandler({ build, mode: MODE })
 );
+
 const port = process.env.PORT || 3000;
+app.listen(port, async () => {
+  console.log(`✅ Express server listening on port ${port}`);
 
-app.listen(port, () => {
-  console.log(`Express server listening on port ${port}`);
-});
-
-function purgeRequireCache() {
-  // purge require cache on requests for "server side HMR" this won't let
-  // you have in-memory objects between requests in development,
-  // alternatively you can set up nodemon/pm2-dev to restart the server on
-  // file changes, but then you'll have to reconnect to databases/etc on each
-  // change. We prefer the DX of this, so we've included it for you by default
-  for (const key in require.cache) {
-    if (key.startsWith(BUILD_DIR)) {
-      delete require.cache[key];
-    }
+  if (process.env.NODE_ENV === "development") {
+    const { devReady } = await import("@remix-run/node");
+    devReady(build);
   }
-}
+});
