c8de6e3ec427a82cb4bb3e60390f4cb4159c93b5
Matt Brophy <matt@brophy.org>
2023-04-26 13:59:29 -0400

    fix(remix-react): Bump RR for removal of `useSyncExternalStore` (#6121)

--- packages/remix-react/browser.tsx
+++ packages/remix-react/browser.tsx
@@ -1,9 +1,7 @@
 import type { HydrationState, Router } from "@remix-run/router";
 import type { ReactElement } from "react";
 import * as React from "react";
-import type { Location } from "react-router-dom";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { useSyncExternalStore } from "use-sync-external-store/shim";
 
 import { RemixContext } from "./components";
 import type { EntryContext, FutureConfig } from "./entry";
@@ -134,6 +132,7 @@ if (import.meta && import.meta.hot) {
           // Abort if a new update comes in while we're waiting for the
           // router to be idle.
           if (signal.aborted) return;
+          // Ensure RouterProvider setState has flushed before re-rendering
           setTimeout(() => {
             Object.assign(window.__remixManifest, assetsManifest);
             window.$RefreshRuntime$.performReactRefresh();
@@ -180,16 +179,20 @@ export function RemixBrowser(_props: RemixBrowserProps): ReactElement {
     });
   }
 
+  let [location, setLocation] = React.useState(router.state.location);
+
+  React.useLayoutEffect(() => {
+    return router.subscribe((newState) => {
+      if (newState.location !== location) {
+        setLocation(newState.location);
+      }
+    });
+  }, [location]);
+
   // We need to include a wrapper RemixErrorBoundary here in case the root error
   // boundary also throws and we need to bubble up outside of the router entirely.
   // Then we need a stateful location here so the user can back-button navigate
   // out of there
-  let location: Location = useSyncExternalStore(
-    router.subscribe,
-    () => router.state.location,
-    () => router.state.location
-  );
-
   return (
     <RemixContext.Provider
       value={{
