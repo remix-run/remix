ef172f471529770ceb48762ee319f0cd949a92eb
Jacob Ebey <jacob.ebey@live.com>
2023-04-27 09:28:06 -0700

    Revert "feat(remix-cloudflare): support `@cloudflare/workers-types` v4 (#6213)"
    This reverts commit d6b5ec7b339896a7e0f5934efc651a7f239b3bf4.

--- templates/cloudflare-workers/build.d.ts
+++
@@ -1,9 +0,0 @@
-import { type ServerBuild } from "@remix-run/cloudflare";
-
-export declare const entry: ServerBuild["entry"];
-export declare const routes: ServerBuild["routes"];
-export declare const assets: ServerBuild["assets"];
-export declare const publicPath: ServerBuild["publicPath"];
-export declare const assetsBuildDirectory: ServerBuild["assetsBuildDirectory"];
-export declare const future: ServerBuild["future"];
-export declare const dev: ServerBuild["dev"];
