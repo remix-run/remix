1f9d9843edce7ef3834dd8800427527bdaa27923
Jacob Ebey <jacob.ebey@live.com>
2023-04-27 09:38:32 -0700

    revert remix template as well

--- templates/remix/remix.config.js
+++ templates/remix/remix.config.js
@@ -7,7 +7,6 @@ module.exports = {
   // publicPath: "/build/",
   serverModuleFormat: "cjs",
   future: {
-    unstable_dev: true,
     v2_errorBoundary: true,
     v2_meta: true,
     v2_normalizeFormMethod: true,
