dbae9db11e271f0fc3695bcc4a6407c7283027a4
Jacob Ebey <jacob.ebey@live.com>
2023-04-27 20:26:44 -0700

    chore: update express template

--- templates/express/server.js
+++ templates/express/server.js
@@ -1,13 +1,12 @@
-const path = require("path");
-const express = require("express");
-const compression = require("compression");
-const morgan = require("morgan");
-const { createRequestHandler } = require("@remix-run/express");
-const { installGlobals } = require("@remix-run/node");
+import express from "express";
+import compression from "compression";
+import morgan from "morgan";
+import { createRequestHandler } from "@remix-run/express";
+import { broadcastDevReady, installGlobals } from "@remix-run/node";
+
+import * as build from "./build/index.js";
 
 installGlobals();
-
-const BUILD_DIR = path.join(process.cwd(), "build");
 
 const app = express();
 
@@ -30,35 +29,17 @@ app.use(morgan("tiny"));
 
 app.all(
   "*",
-  process.env.NODE_ENV === "development"
-    ? (req, res, next) => {
-        purgeRequireCache();
-
-        return createRequestHandler({
-          build: require(BUILD_DIR),
-          mode: process.env.NODE_ENV,
-        })(req, res, next);
-      }
-    : createRequestHandler({
-        build: require(BUILD_DIR),
-        mode: process.env.NODE_ENV,
-      })
+  createRequestHandler({
+    build,
+    mode: process.env.NODE_ENV,
+  })
 );
-const port = process.env.PORT || 3000;
 
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Express server listening on port ${port}`);
-});
 
-function purgeRequireCache() {
-  // purge require cache on requests for "server side HMR" this won't let
-  // you have in-memory objects between requests in development,
-  // alternatively you can set up nodemon/pm2-dev to restart the server on
-  // file changes, but then you'll have to reconnect to databases/etc on each
-  // change. We prefer the DX of this, so we've included it for you by default
-  for (const key in require.cache) {
-    if (key.startsWith(BUILD_DIR)) {
-      delete require.cache[key];
-    }
+  if (process.env.NODE_ENV === "development") {
+    broadcastDevReady(build);
   }
-}
+});
