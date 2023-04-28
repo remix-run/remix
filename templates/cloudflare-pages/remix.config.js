e13d5aaeba27d9ff95945dcb05995d78c5c920ae
Jacob Ebey <jacob.ebey@live.com>
2023-04-27 20:22:32 -0700

    chore(templates/cloudflare-pages): integrate v2_dev

--- templates/cloudflare-pages/remix.config.js
+++ templates/cloudflare-pages/remix.config.js
@@ -1,5 +1,5 @@
 /** @type {import('@remix-run/dev').AppConfig} */
-module.exports = {
+export default {
   devServerBroadcastDelay: 1000,
   ignoredRouteFiles: ["**/.*"],
   server: "./server.ts",
@@ -14,6 +14,7 @@ module.exports = {
   // assetsBuildDirectory: "public/build",
   // publicPath: "/build/",
   future: {
+    v2_dev: true,
     v2_errorBoundary: true,
     v2_headers: true,
     v2_meta: true,
