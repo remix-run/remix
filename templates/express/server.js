3f0ba3780c748782c31df2d19392909765a28f5d
Pedro Cattori <pcattori@gmail.com>
2023-05-04 12:53:46 -0400

    feat(templates/express): server-side "hmr" via import cache busting

--- templates/express/server.js
+++ templates/express/server.js
@@ -1,3 +1,5 @@
+import * as fs from "node:fs";
+import chokidar from "chokidar";
 import express from "express";
 import compression from "compression";
 import morgan from "morgan";
@@ -5,6 +7,8 @@ import { createRequestHandler } from "@remix-run/express";
 import { broadcastDevReady, installGlobals } from "@remix-run/node";
 
 import * as build from "./build/index.js";
+
+const BUILD_PATH = "./build/index.js";
 
 installGlobals();
 
@@ -29,17 +33,49 @@ app.use(morgan("tiny"));
 
 app.all(
   "*",
-  createRequestHandler({
-    build,
-    mode: process.env.NODE_ENV,
-  })
+  process.env.NODE_ENV === "development"
+    ? createDevRequestHandler()
+    : createRequestHandler({
+        build,
+        mode: process.env.NODE_ENV,
+      })
 );
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+app.listen(port, async () => {
   console.log(`Express server listening on port ${port}`);
 
   if (process.env.NODE_ENV === "development") {
     broadcastDevReady(build);
   }
 });
+
+function createDevRequestHandler() {
+  // initial build
+  /**
+   * @type { import('@remix-run/node').ServerBuild | Promise<import('@remix-run/node').ServerBuild> }
+   */
+  let devBuild = build;
+
+  const watcher = chokidar.watch(BUILD_PATH, { ignoreInitial: true });
+
+  watcher.on("all", async () => {
+    // 1. purge require cache && load updated server build
+    const stat = fs.statSync(BUILD_PATH);
+    devBuild = import(BUILD_PATH + "?t=" + stat.mtimeMs);
+    // 2. tell dev server that this app server is now ready
+    broadcastDevReady(await devBuild);
+  });
+
+  return async (req, res, next) => {
+    try {
+      //
+      return createRequestHandler({
+        build: await devBuild,
+        mode: "development",
+      })(req, res, next);
+    } catch (error) {
+      next(error);
+    }
+  };
+}
