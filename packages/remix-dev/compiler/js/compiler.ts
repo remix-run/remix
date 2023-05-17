6a570cf3ad11b95014bc6cc0851cc39522f54870
Luke Askew <lukeaskew@gmail.com>
2023-05-17 10:15:31 -0600

    refactor(remix-dev): replace `esbuild-plugin-polyfill-node` with `esbuild-plugins-node-modules-polyfill`

--- packages/remix-dev/compiler/js/compiler.ts
+++ packages/remix-dev/compiler/js/compiler.ts
@@ -1,7 +1,7 @@
 import * as path from "path";
 import { builtinModules as nodeBuiltins } from "module";
 import * as esbuild from "esbuild";
-import { polyfillNode as NodeModulesPolyfillPlugin } from "esbuild-plugin-polyfill-node";
+import { nodeModulesPolyfillPlugin } from "esbuild-plugins-node-modules-polyfill";
 
 import type { RemixConfig } from "../../config";
 import { type Manifest } from "../../manifest";
@@ -145,7 +145,7 @@ const createEsbuildConfig = (
       : browserRouteModulesPlugin(ctx, /\?browser$/),
     mdxPlugin(ctx),
     emptyModulesPlugin(ctx, /\.server(\.[jt]sx?)?$/),
-    NodeModulesPolyfillPlugin(),
+    nodeModulesPolyfillPlugin(),
     externalPlugin(/^node:.*/, { sideEffects: false }),
     {
       // TODO: should be removed when error handling for compiler is improved
