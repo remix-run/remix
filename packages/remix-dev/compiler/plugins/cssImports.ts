2c8ca1928661471cf39d267797f949c8e56b6c87
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2023-05-23 14:04:16 +1000

    Add initial POC PostCSS cache

--- packages/remix-dev/compiler/plugins/cssImports.ts
+++ packages/remix-dev/compiler/plugins/cssImports.ts
@@ -2,6 +2,8 @@ import * as path from "path";
 import * as fse from "fs-extra";
 import esbuild from "esbuild";
 import type { Processor } from "postcss";
+import chokidar from "chokidar";
+import picomatch from "picomatch";
 
 import invariant from "../../invariant";
 import type { RemixConfig } from "../../config";
@@ -146,6 +148,184 @@ export function cssFilePlugin({
   };
 }
 
+const _globMatchers = new Map<string, ReturnType<typeof picomatch>>();
+function getGlobMatcher(glob: string) {
+  if (!_globMatchers.has(glob)) {
+    _globMatchers.set(glob, picomatch(glob));
+  }
+  return _globMatchers.get(glob);
+}
+
+function getPostcssCompiler({
+  postcssProcessor,
+  sourcemap,
+}: {
+  postcssProcessor: Processor;
+  sourcemap: boolean;
+}) {
+  let watcher = chokidar.watch([], {
+    ignoreInitial: true,
+  });
+
+  let cachedCssForEntryPoint = new Map<string, Promise<string>>();
+
+  let fileDepsForEntryPoint = new Map<string, Set<string>>();
+  let entryPointsForFileDep = new Map<string, Set<string>>();
+
+  // Glob dependencies are used for Tailwind
+  // e.g. Tailwind directives like `@tailwind utilities` output a bunch of
+  // CSS that changes based on the contents of all TS/JS files in the project
+  // via a glob in the Tailwind config.
+  let globDepsForEntryPoint = new Map<string, Set<string>>();
+  let entryPointsForGlobDep = new Map<string, Set<string>>();
+
+  function invalidateEntryPoint(invalidatedEntryPoint: string) {
+    // If it's not an entry point (or doesn't have a cache entry), bail out
+    if (!cachedCssForEntryPoint.has(invalidatedEntryPoint)) {
+      return;
+    }
+
+    cachedCssForEntryPoint.delete(invalidatedEntryPoint);
+
+    // Reset tracked deps for entry point. Since we're going to recompile,
+    // the entry point will get new deps.
+    let fileDeps = fileDepsForEntryPoint.get(invalidatedEntryPoint);
+    if (fileDeps) {
+      for (let fileDep of fileDeps) {
+        entryPointsForFileDep.get(fileDep)?.delete(invalidatedEntryPoint);
+      }
+      fileDepsForEntryPoint.delete(invalidatedEntryPoint);
+    }
+
+    // Reset tracked glob dependencies for entry point. Since we're going to
+    // recompile, the entry point will get new glob dependencies.
+    let globDeps = globDepsForEntryPoint.get(invalidatedEntryPoint);
+    if (globDeps) {
+      for (let glob of globDeps) {
+        entryPointsForGlobDep.get(glob)?.delete(invalidatedEntryPoint);
+      }
+      globDepsForEntryPoint.delete(invalidatedEntryPoint);
+    }
+  }
+
+  function invalidatePath(invalidatedPath: string) {
+    console.log("invalidate path", { invalidatedPath });
+
+    // This might be an entry point so we invalidate it just in case.
+    invalidateEntryPoint(invalidatedPath);
+
+    // Invalidate all entry points that depend on path.
+    let entryPoints = entryPointsForFileDep.get(invalidatedPath);
+    if (entryPoints) {
+      for (let entryPoint of entryPoints) {
+        console.log("Invalidate entry point for file dep", {
+          invalidatedPath,
+          entryPoint,
+        });
+        invalidateEntryPoint(entryPoint);
+      }
+    }
+
+    // Invalidate all entry points that depend on a glob that matches the path.
+    // Any glob could match the path, so we have to check all globs.
+    for (let [glob, entryPoints] of entryPointsForGlobDep) {
+      let match = getGlobMatcher(glob);
+      if (match && match(invalidatedPath)) {
+        for (let entryPoint of entryPoints) {
+          console.log("Invalidate entry point for glob match", {
+            glob,
+            invalidatedPath,
+            entryPoint,
+          });
+          invalidateEntryPoint(entryPoint);
+        }
+      }
+    }
+  }
+
+  watcher.on("change", (changedPath) => {
+    invalidatePath(changedPath);
+  });
+
+  watcher.on("unlink", (unlinkedPath) => {
+    invalidatePath(unlinkedPath);
+  });
+
+  let compiler = {
+    async processFile(entryPoint: string) {
+      if (cachedCssForEntryPoint.has(entryPoint)) {
+        console.log("Cache hit", { entryPoint });
+        return await cachedCssForEntryPoint.get(entryPoint);
+      }
+
+      console.log("Cache miss", { entryPoint });
+
+      let postCssPromise = fse
+        .readFile(entryPoint, "utf-8")
+        .then(async (contents) => {
+          let result = await postcssProcessor.process(contents, {
+            from: entryPoint,
+            to: entryPoint,
+            map: sourcemap,
+          });
+
+          let newDeps = new Set<string>();
+          let newGlobDeps = new Set<string>();
+
+          for (let msg of result.messages) {
+            if (msg.type === "dependency" && typeof msg.file === "string") {
+              newDeps.add(msg.file);
+              continue;
+            }
+
+            if (
+              msg.type === "dir-dependency" &&
+              typeof msg.dir === "string" &&
+              typeof msg.glob === "string"
+            ) {
+              newGlobDeps.add(path.join(msg.dir, msg.glob));
+              continue;
+            }
+          }
+
+          fileDepsForEntryPoint.set(entryPoint, newDeps);
+          globDepsForEntryPoint.set(entryPoint, newGlobDeps);
+
+          // Track the file dependencies of this entry point
+          for (let newDep of newDeps) {
+            let entryPoints = entryPointsForFileDep.get(newDep);
+            if (!entryPoints) {
+              entryPoints = new Set();
+              entryPointsForFileDep.set(newDep, entryPoints);
+            }
+            entryPoints.add(entryPoint);
+          }
+
+          // Track the glob dependencies of this entry point
+          for (let newGlobDep of newGlobDeps) {
+            let entryPoints = entryPointsForGlobDep.get(newGlobDep);
+            if (!entryPoints) {
+              entryPoints = new Set();
+              entryPointsForGlobDep.set(newGlobDep, entryPoints);
+            }
+            entryPoints.add(entryPoint);
+          }
+
+          watcher.add([entryPoint, ...newDeps, ...newGlobDeps]);
+
+          return result.css;
+        });
+
+      cachedCssForEntryPoint.set(entryPoint, postCssPromise);
+
+      return await postCssPromise;
+    },
+  };
+
+  return compiler;
+}
+let postcssCompiler: ReturnType<typeof getPostcssCompiler>;
+
 function postcssPlugin({
   postcssProcessor,
   options,
@@ -156,16 +336,15 @@ function postcssPlugin({
   return {
     name: "postcss-plugin",
     async setup(build) {
-      build.onLoad({ filter: /\.css$/, namespace: "file" }, async (args) => {
-        let contents = await fse.readFile(args.path, "utf-8");
+      postcssCompiler =
+        postcssCompiler ||
+        getPostcssCompiler({
+          postcssProcessor,
+          sourcemap: options.sourcemap,
+        });
 
-        contents = (
-          await postcssProcessor.process(contents, {
-            from: args.path,
-            to: args.path,
-            map: options.sourcemap,
-          })
-        ).css;
+      build.onLoad({ filter: /\.css$/, namespace: "file" }, async (args) => {
+        let contents = await postcssCompiler.processFile(args.path);
 
         return {
           contents,
