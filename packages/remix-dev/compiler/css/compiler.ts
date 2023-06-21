727aae1247d9f092f28eb8838ca8b60588204ed2
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2023-06-21 11:39:19 +1000

    skip Remix/React packages imported in node_modules

--- packages/remix-dev/compiler/css/compiler.ts
+++ packages/remix-dev/compiler/css/compiler.ts
@@ -80,7 +80,9 @@ const createEsbuildConfig = (ctx: Context): esbuild.BuildOptions => {
       externalPlugin(/^https?:\/\//, { sideEffects: false }),
       mdxPlugin(ctx),
       // Skip compilation of common packages/scopes known not to include CSS imports
-      emptyModulesPlugin(ctx, /^(@remix-run|react|react-dom)(\/.*)?$/),
+      emptyModulesPlugin(ctx, /^(@remix-run|react|react-dom)(\/.*)?$/, {
+        includeNodeModules: true,
+      }),
       emptyModulesPlugin(ctx, /\.server(\.[jt]sx?)?$/),
       externalPlugin(/^node:.*/, { sideEffects: false }),
     ],
