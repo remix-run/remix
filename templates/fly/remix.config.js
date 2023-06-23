c11879f8900330db4edd5e5923c468c8d277b03d
Pedro Cattori <pcattori@gmail.com>
2023-06-23 12:04:34 -0400

    chore(templates/fly): integrate v2_dev

--- templates/fly/remix.config.js
+++ templates/fly/remix.config.js
@@ -5,7 +5,9 @@ module.exports = {
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "build/index.js",
   // publicPath: "/build/",
+  serverModuleFormat: "cjs",
   future: {
+    v2_dev: true,
     v2_errorBoundary: true,
     v2_headers: true,
     v2_meta: true,
