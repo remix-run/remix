3c0842d15c49f0018ca396105bd023cc5df5a1fe
Michaël De Boey <info@michaeldeboey.be>
2023-07-22 21:38:04 +0200

    templates(netlify): show deprecation warning about deprecated Netlify adapter

--- templates/netlify/server.ts
+++ templates/netlify/server.ts
@@ -1,5 +1,5 @@
+import { createRequestHandler } from "@netlify/remix-adapter";
 import * as build from "@remix-run/dev/server-build";
-import { createRequestHandler } from "@remix-run/netlify";
 import { installGlobals } from "@remix-run/node";
 
 installGlobals();
