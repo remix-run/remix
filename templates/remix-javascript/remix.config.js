0622dc8c03dbe511851b6edabdfeaec73ef2ba6e
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2023-07-28 11:31:57 +1000

    refactor: clean flags in remix-javascript template

--- templates/remix-javascript/remix.config.js
+++ templates/remix-javascript/remix.config.js
@@ -7,9 +7,7 @@ module.exports = {
   // publicPath: "/build/",
   serverModuleFormat: "cjs",
   future: {
-    v2_errorBoundary: true,
     v2_meta: true,
-    v2_normalizeFormMethod: true,
     v2_routeConvention: true,
   },
 };
