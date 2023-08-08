ae036556e91ee251be528cee803542f3822149e9
Leo Singer <leo.p.singer@nasa.gov>
2023-08-08 15:42:17 -0400

    Fix typo in manifest regex (#6885)
    Co-authored-by: Pedro Cattori <pcattori@gmail.com>

--- templates/arc/plugin-remix.js
+++ templates/arc/plugin-remix.js
@@ -23,7 +23,7 @@ export default {
           path.resolve(process.cwd(), buildPath),
           "utf8"
         );
-        const manifestMatches = contents.matchAll(/manifest-([A-f0-9]+)\.js/g);
+        const manifestMatches = contents.matchAll(/manifest-([A-Fa-f0-9]+)\.js/g);
         const sent = new Set();
         for (const match of manifestMatches) {
           const buildHash = match[1];
