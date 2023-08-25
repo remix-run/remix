07f66e4502035432ea13e93d3b4851475ca09e88
Matt Brophy <matt@brophy.org>
2023-08-25 11:16:10 -0400

    Actually use MS Edge

--- integration/playwright.config.ts
+++ integration/playwright.config.ts
@@ -27,7 +27,12 @@ const config: PlaywrightTestConfig = {
     },
     {
       name: "msedge",
-      use: devices["Desktop Edge"],
+      use: {
+        ...devices["Desktop Edge"],
+        // Desktop Edge uses chromium by default
+        // https://github.com/microsoft/playwright/blob/main/packages/playwright-core/src/server/deviceDescriptorsSource.json#L1502
+        channel: "msedge",
+      },
     },
     {
       name: "firefox",
