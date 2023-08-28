fd6060cf3e50447fd73cb212ad66b61411eb937b
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2023-08-28 10:53:18 +1000

    refactor to use esbuild OutputFile type directly

--- packages/remix-dev/compiler/js/write.ts
+++ packages/remix-dev/compiler/js/write.ts
@@ -1,13 +1,10 @@
 import * as path from "node:path";
-import type * as esbuild from "esbuild";
+import type { OutputFile } from "esbuild";
 import fse from "fs-extra";
 
 import type { RemixConfig } from "../../config";
 
-export async function write(
-  config: RemixConfig,
-  outputFiles: esbuild.OutputFile[]
-) {
+export async function write(config: RemixConfig, outputFiles: OutputFile[]) {
   await fse.ensureDir(path.dirname(config.assetsBuildDirectory));
 
   for (let file of outputFiles) {
