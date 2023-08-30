55d86c09edddee0de3c8b124340f78ffbfa333a8
Pedro Cattori <pcattori@gmail.com>
2023-08-30 17:12:06 -0400

    use full paths to server build when running remix-serve (#7308)

--- templates/fly/remix.config.js
+++ templates/fly/remix.config.js
@@ -1,7 +1,6 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   ignoredRouteFiles: ["**/.*"],
-  serverModuleFormat: "cjs",
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // publicPath: "/build/",
