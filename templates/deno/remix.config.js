982e72235fa251404163e9c2ad7fb3501725ca2e
Michaël De Boey <info@michaeldeboey.be>
2023-08-30 14:56:30 +0200

    templates: remove `devServerBroadcastDelay` config option

--- templates/deno/remix.config.js
+++ templates/deno/remix.config.js
@@ -1,12 +1,5 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
-  /*
-  If live reload causes page to re-render without changes (live reload is too fast),
-  increase the dev server broadcast delay.
-
-  If live reload seems slow, try to decrease the dev server broadcast delay.
-  */
-  devServerBroadcastDelay: 300,
   ignoredRouteFiles: ["**/.*"],
   server: "./server.ts",
   serverConditions: ["deno", "worker"],
@@ -16,8 +9,8 @@ module.exports = {
   serverPlatform: "neutral",
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
-  // serverBuildPath: "build/index.js",
   // publicPath: "/build/",
+  // serverBuildPath: "build/index.js",
   future: {
     v2_errorBoundary: true,
     v2_headers: true,
