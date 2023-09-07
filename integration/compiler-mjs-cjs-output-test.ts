a5ec0ba6c41cf3957f36ea238068c19582293656
Leo Singer <leo.p.singer@nasa.gov>
2023-09-07 15:26:53 -0400

    Update integration/compiler-mjs-cjs-output-test.ts
    Co-authored-by: Michaël De Boey <info@michaeldeboey.be>

--- integration/compiler-mjs-cjs-output-test.ts
+++ integration/compiler-mjs-cjs-output-test.ts
@@ -19,29 +19,6 @@ test.describe("", () => {
               serverBuildPath: "build/index.${serverModuleExt}",
             };
           `,
-          "package.json": json({
-            name: "remix-template-remix",
-            private: true,
-            sideEffects: false,
-            type: "module",
-            dependencies: {
-              "@remix-run/node": "0.0.0-local-version",
-              "@remix-run/react": "0.0.0-local-version",
-              "@remix-run/serve": "0.0.0-local-version",
-              isbot: "0.0.0-local-version",
-              react: "0.0.0-local-version",
-              "react-dom": "0.0.0-local-version",
-            },
-            devDependencies: {
-              "@remix-run/dev": "0.0.0-local-version",
-              "@types/react": "0.0.0-local-version",
-              "@types/react-dom": "0.0.0-local-version",
-              typescript: "0.0.0-local-version",
-            },
-            engines: {
-              node: ">=18.0.0",
-            },
-          }),
           "app/routes/_index.tsx": js`
             import { json } from "@remix-run/node";
             import { useLoaderData, Link } from "@remix-run/react";
