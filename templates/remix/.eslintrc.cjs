e481e1a3f7f5bc6b6fd3ebdeb2d41dab2adc3dde
Matt Brophy <matt@brophy.org>
2023-10-16 10:28:28 -0400

    Add rules of hooks plugin

--- templates/remix/.eslintrc.cjs
+++ templates/remix/.eslintrc.cjs
@@ -31,6 +31,7 @@ module.exports = {
       extends: [
         "plugin:react/recommended",
         "plugin:react/jsx-runtime",
+        "plugin:react-hooks/recommended",
         "plugin:jsx-a11y/recommended",
         "prettier",
       ],
