36b7a4808d84258cecf74703bd88a3953753db14
Matt Brophy <matt@brophy.org>
2023-10-26 16:02:58 -0400

    Revert "Bump router version"
    This reverts commit ad4618e203d346557b30f7d34e7b20aee469e23b.

--- scripts/bump-router-versions.sh
+++ scripts/bump-router-versions.sh
@@ -23,10 +23,6 @@ fi
 
 set -x
 
-cd packages/remix-dev
-yarn add -E @remix-run/router@${ROUTER_VERSION}
-cd ../..
-
 cd packages/remix-server-runtime
 yarn add -E @remix-run/router@${ROUTER_VERSION}
 cd ../..
