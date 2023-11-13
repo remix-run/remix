0ce66943f1f9a4a72b09b90d57904eb9fbc703e2
alcpereira <48070464+alcpereira@users.noreply.github.com>
2023-11-13 15:30:44 +0000

    docs(remix-react): add JSDoc to LiveReload component (#7977)
    Co-authored-by: Matt Brophy <matt@brophy.org>

--- packages/remix-react/components.tsx
+++ packages/remix-react/components.tsx
@@ -1041,9 +1041,16 @@ export function useFetcher<TData = AppData>(
   return useFetcherRR(opts);
 }
 
-// Dead Code Elimination magic for production builds.
-// This way devs don't have to worry about doing the NODE_ENV check themselves.
+/**
+ * This component connects your app to the Remix asset server and
+ * automatically reloads the page when files change in development.
+ * In production, it renders null, so you can safely render it always in your root route.
+ *
+ * @see https://remix.run/docs/components/live-reload
+ */
 export const LiveReload =
+  // Dead Code Elimination magic for production builds.
+  // This way devs don't have to worry about doing the NODE_ENV check themselves.
   process.env.NODE_ENV !== "development"
     ? () => null
     : function LiveReload({
