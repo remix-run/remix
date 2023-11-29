34027588ceac22cbd9d46cb55a0568904fb422a6
kiliman <kiliman@gmail.com>
2023-11-29 17:05:37 -0500

    fix (templates): Add source map retrieval fix to Express template
    This PR updates the the Express template and fixes the issue where source maps are not getting used. This was traced to how the map file is loaded when HMR reloads the server file with the `?t=timestamp` suffix.
    
    This causes the source map to not be found and therefore not used.
    
    Adding the hook for `retrieveSourceMap`, this strips out the unexpected characters from the filename and properly returns the source map.

--- templates/express/server.js
+++ templates/express/server.js
@@ -9,7 +9,19 @@ import express from "express";
 import morgan from "morgan";
 import sourceMapSupport from "source-map-support";
 
-sourceMapSupport.install();
+sourceMapSupport.install({
+  retrieveSourceMap: function (source) {
+    // get source file without the `file://` prefix or `?t=...` suffix
+    const match = source.match(/^file:\/\/(.*)\?t=[.\d]+$/);
+    if (match) {
+      return {
+        url: source,
+        map: fs.readFileSync(`${match[1]}.map`, "utf8"),
+      };
+    }
+    return null;
+  },
+});
 installGlobals();
 
 /** @typedef {import('@remix-run/node').ServerBuild} ServerBuild */
