081b57884f18a273839c0f9d48f9ff45b4a402b7
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-12-01 08:38:54 +0900

    chore: struggle

--- integration/vite-manifest-invalidation-test.ts
+++ integration/vite-manifest-invalidation-test.ts
@@ -85,7 +85,8 @@ test.describe(async () => {
     await expect
       .poll(async () => {
         console.log("@@@@@@@ trial", i++);
-        await page.goto(`http://localhost:${port}/`);
+        await page.reload();
+        // await page.goto(`http://localhost:${port}/`);
         await page.getByText("Mounted: yes").click();
         console.log(await page.evaluate(() => (window as any).__remixManifest));
         return page.evaluate(
