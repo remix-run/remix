06f6c6523cae839532b2165c28c44546e9a44e5b
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-11-30 16:45:57 +0900

    chore: debug ci

--- integration/vite-dev-manifest-invalidation-test.ts
+++ integration/vite-dev-manifest-invalidation-test.ts
@@ -63,7 +63,9 @@ test.describe(async () => {
 
     // client is not aware of new route addition yet (https://github.com/remix-run/remix/issues/7894)
     // however server should be able to handle new route (thought it needs a few retries to process invalidation)
+    let i = 0;
     await expect(async () => {
+      console.log(":::::::: TRIAL", i++);
       await page.goto(`http://localhost:${port}/other`);
       await page.getByText("new route").click();
     }).toPass();
