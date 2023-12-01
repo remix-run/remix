7d8bf43468cce42cc675e230f5b0470a092595cf
Matt Brophy <matt@brophy.org>
2023-12-01 16:46:44 -0500

    Revert "Remove debugging logs"
    This reverts commit c1dcf71dacf4dc25a82b028f4d3cc42d1f4202b3.

--- packages/remix-dev/devServer_unstable/index.ts
+++ packages/remix-dev/devServer_unstable/index.ts
@@ -241,6 +241,12 @@ export let serve = async (
             let clientLoaderChanges = await state.clientLoaderChanges!;
             if (clientLoaderChanges.ok) {
               newState.prevClientLoaderHashes = clientLoaderChanges.value;
+              console.log("clientLoaderChanges", clientLoaderChanges.value);
+            } else {
+              console.log(
+                "clientLoaderChanges not ok",
+                clientLoaderChanges.value
+              );
             }
             if (
               clientLoaderChanges?.ok &&
