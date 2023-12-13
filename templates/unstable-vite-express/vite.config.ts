e8dd6456e8a7b21ab289a87486f52d017de05bd5
Matt Brophy <matt@brophy.org>
2023-12-13 10:36:15 -0500

    Remove @remix-run/eslint-config dependency (#7597)

--- templates/unstable-vite-express/vite.config.ts
+++ templates/unstable-vite-express/vite.config.ts
@@ -1,5 +1,8 @@
 import { unstable_vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
+// This is only an issue in the Remix monorepo, this should lint properly once
+// you've created an app from this template and this comment can be removed
+// eslint-disable-next-line import/no-unresolved
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
