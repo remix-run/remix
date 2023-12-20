047d0f83f60f0e18380e9ba1983c1126bdde5c73
Matt Brophy <matt@brophy.org>
2023-12-20 15:41:54 -0500

    Add Remix SPA Template

---
+++ templates/spa/app/routes/_index.tsx
@@ -0,0 +1,28 @@
+import type { MetaFunction } from "@remix-run/node";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "New Remix App" },
+    { name: "description", content: "Welcome to Remix (SPA Mode)!" },
+  ];
+};
+
+export default function Index() {
+  return (
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
+      <h1>Welcome to Remix (SPA Mode)</h1>
+      <ul>
+        <li>
+          <a target="_blank" href="https://remix.run/" rel="noreferrer">
+            SPA Mode Guide
+          </a>
+        </li>
+        <li>
+          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
+            Remix Docs
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+}
