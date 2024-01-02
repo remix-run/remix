4500969f50bda1f26faa2db8fe4483c5030c8d12
Matt Brophy <matt@brophy.org>
2024-01-02 16:05:59 -0500

    Revert "chore: use isbot version 4 (#8385)"
    This reverts commit 3fb419501f407e62490ca1e827c51929b39d3b04.

--- templates/cloudflare-pages/app/entry.server.tsx
+++ templates/cloudflare-pages/app/entry.server.tsx
@@ -6,7 +6,7 @@
 
 import type { AppLoadContext, EntryContext } from "@remix-run/cloudflare";
 import { RemixServer } from "@remix-run/react";
-import { isbot } from "isbot";
+import isbot from "isbot";
 import { renderToReadableStream } from "react-dom/server";
 
 export default async function handleRequest(
@@ -31,7 +31,7 @@ export default async function handleRequest(
     }
   );
 
-  if (isbot(request.headers.get("user-agent") || "")) {
+  if (isbot(request.headers.get("user-agent"))) {
     await body.allReady;
   }
 
