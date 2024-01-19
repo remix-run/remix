d71dcf0f019d702417b7e727f02716fb1776c699
Pedro Cattori <pcattori@gmail.com>
2024-01-19 12:35:44 -0500

    add catch-all function for preview/prod

---
+++ templates/unstable-vite-cloudflare/functions/[[path]].ts
@@ -0,0 +1,12 @@
+import { type ServerBuild } from "@remix-run/cloudflare";
+import { createPagesFunctionHandler } from "@remix-run/cloudflare-pages";
+import * as _build from "../build/server";
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const build: ServerBuild = _build as any;
+
+export const onRequest = createPagesFunctionHandler({
+  build,
+  getLoadContext: (context) => ({ env: context.env }),
+  mode: build.mode,
+});
