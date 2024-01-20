130f06020b2de8b3f066634639a70d267862ff53
Pedro Cattori <pcattori@gmail.com>
2024-01-20 12:15:13 -0500

    fix: derive default build mode from `build`

--- packages/remix-server-runtime/server.ts
+++ packages/remix-server-runtime/server.ts
@@ -81,6 +81,7 @@ export const createRequestHandler: CreateRequestHandlerFunction = (
 
   return async function requestHandler(request, loadContext = {}) {
     _build = typeof build === "function" ? await build() : build;
+    mode ??= _build.mode;
     if (typeof build === "function") {
       let derived = derive(_build, mode);
       routes = derived.routes;
