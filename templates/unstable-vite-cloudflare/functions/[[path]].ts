dadd18e15945869b976773e9360c4c5a2f4b1e07
Pedro Cattori <pcattori@gmail.com>
2024-01-20 11:42:37 -0500

    fix lint error

--- templates/unstable-vite-cloudflare/functions/[[path]].ts
+++ templates/unstable-vite-cloudflare/functions/[[path]].ts
@@ -1,5 +1,6 @@
 import { type ServerBuild } from "@remix-run/cloudflare";
 import { createPagesFunctionHandler } from "@remix-run/cloudflare-pages";
+// eslint-disable-next-line import/no-unresolved
 import * as _build from "../build/server";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
