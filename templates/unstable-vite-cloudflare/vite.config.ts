0ee9e54546f35243b805cb8adaa5e352c352d7b6
Pedro Cattori <pcattori@gmail.com>
2024-01-31 15:16:56 -0500

    more renaming

--- templates/unstable-vite-cloudflare/vite.config.ts
+++ templates/unstable-vite-cloudflare/vite.config.ts
@@ -1,6 +1,6 @@
 import {
   unstable_vitePlugin as remix,
-  unstable_vitePluginPresetCloudflare as cloudflare,
+  unstable_cloudflarePreset as cloudflare,
 } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
