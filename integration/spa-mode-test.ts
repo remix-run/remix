4ae73a1faa3b233db34e0458e6fa0d8b4facd198
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2024-02-09 07:19:13 +1100

    Fix SPA mode test

--- integration/spa-mode-test.ts
+++ integration/spa-mode-test.ts
@@ -271,7 +271,7 @@ test.describe("SPA Mode", () => {
         files: {
           "vite.config.ts": js`
             import { defineConfig } from "vite";
-            import { unstable_vitePlugin as remix } from "@remix-run/dev";
+            import { vitePlugin as remix } from "@remix-run/dev";
 
             export default defineConfig({
               plugins: [remix({
