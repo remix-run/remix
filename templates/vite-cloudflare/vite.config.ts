a15d6b6da9d0599738ed983fefe13850d0d84905
Pedro Cattori <pcattori@gmail.com>
2024-02-14 15:33:51 -0500

    template

--- templates/vite-cloudflare/vite.config.ts
+++ templates/vite-cloudflare/vite.config.ts
@@ -1,21 +1,10 @@
 import {
   vitePlugin as remix,
-  cloudflarePreset as cloudflare,
+  cloudflareProxyVitePlugin as remixCloudflareProxy,
 } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { getBindingsProxy } from "wrangler";
 
 export default defineConfig({
-  plugins: [
-    remix({
-      presets: [cloudflare(getBindingsProxy)],
-    }),
-    tsconfigPaths(),
-  ],
-  ssr: {
-    resolve: {
-      externalConditions: ["workerd", "worker"],
-    },
-  },
+  plugins: [remixCloudflareProxy(), remix(), tsconfigPaths()],
 });
