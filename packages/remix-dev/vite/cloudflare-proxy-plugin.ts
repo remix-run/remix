1cfeeab82b46cd7ac1b4e322ff3387dd88d102fe
Izzuddin Natsir <32354148+izznatsir@users.noreply.github.com>
2024-02-21 20:25:27 +0700

    feat: cloudflare websocket proxy

--- packages/remix-dev/vite/cloudflare-proxy-plugin.ts
+++ packages/remix-dev/vite/cloudflare-proxy-plugin.ts
@@ -1,10 +1,13 @@
+import { Server } from "node:http";
+
 import { createRequestHandler } from "@remix-run/server-runtime";
 import {
   type AppLoadContext,
   type ServerBuild,
 } from "@remix-run/server-runtime";
-import { type Plugin } from "vite";
+import { type Connect, type Plugin } from "vite";
 import { type GetPlatformProxyOptions, type PlatformProxy } from "wrangler";
+import { Server as WebSocketServer } from "ws";
 
 import { fromNodeRequest, toNodeRequest } from "./node-adapter";
 
@@ -40,6 +43,13 @@ export const cloudflareDevProxyVitePlugin = <Env, Cf extends CfProperties>({
   return {
     name: NAME,
     config: () => ({
+      server: {
+        hmr: {
+          // HMR must be served on a different port than the dev server
+          // so non-HMR WebSocket upgrade request is handled by the WebSocket proxy
+          port: 4173,
+        },
+      },
       ssr: {
         resolve: {
           externalConditions: ["workerd", "worker"],
@@ -61,6 +71,83 @@ export const cloudflareDevProxyVitePlugin = <Env, Cf extends CfProperties>({
       // Do not include `dispose` in Cloudflare context
       let { dispose, ...cloudflare } = await getPlatformProxy<Env, Cf>(options);
       let context = { cloudflare };
+
+      // Create WebSocket proxy for Durable Object WebSocket
+      if (viteDevServer.httpServer instanceof Server) {
+        let wsServer = new WebSocketServer({
+          server: viteDevServer.httpServer,
+        });
+
+        wsServer.on("connection", async (nodeWs, nodeReq) => {
+          // Patch Node IncomingMessage to be compatible with Connect.IncomingMessage
+          (nodeReq as Connect.IncomingMessage).originalUrl = nodeReq.url;
+
+          let build = (await viteDevServer.ssrLoadModule(
+            serverBuildId
+          )) as ServerBuild;
+
+          let handler = createRequestHandler(build, "development");
+          let req = fromNodeRequest(nodeReq);
+          let loadContext = getLoadContext
+            ? await getLoadContext({ request: req, context })
+            : context;
+
+          let request = fromNodeRequest(nodeReq);
+          let response = (await handler(request, loadContext)) as unknown as {
+            status: number;
+            webSocket?: import("@cloudflare/workers-types").WebSocket;
+          };
+
+          if (response.status !== 101 || response.webSocket === undefined) {
+            nodeWs.close();
+            return;
+          }
+
+          let doWs = response.webSocket;
+          doWs.accept();
+
+          let closedByClient = false;
+          let closedByWorkerd = false;
+
+          nodeWs.on("close", () => {
+            if (closedByWorkerd) {
+              closedByWorkerd = false;
+              viteDevServer.hot.send({
+                type: "full-reload",
+              });
+              return;
+            }
+
+            closedByClient = true;
+            doWs.close();
+          });
+
+          nodeWs.on("message", (data) => {
+            if (Array.isArray(data)) {
+              for (let datum of data) {
+                doWs.send(datum);
+              }
+            } else {
+              doWs.send(data);
+            }
+          });
+
+          doWs.addEventListener("close", () => {
+            if (closedByClient) {
+              closedByClient = false;
+              return;
+            }
+
+            closedByWorkerd = true;
+            nodeWs.close();
+          });
+
+          doWs.addEventListener("message", (e) => {
+            nodeWs.send(e.data);
+          });
+        });
+      }
+
       return () => {
         if (!viteDevServer.config.server.middlewareMode) {
           viteDevServer.middlewares.use(async (nodeReq, nodeRes, next) => {
