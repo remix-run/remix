9b31736fc66fa2b70373252feb30ebbb5ab9173d
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2024-03-07 11:45:57 +1100

    test fixes

--- packages/remix-dev/vite/babel.ts
+++ packages/remix-dev/vite/babel.ts
@@ -1,9 +1,13 @@
+/* eslint-disable @typescript-eslint/consistent-type-imports */
 import type { NodePath } from "@babel/traverse";
 import type { types as BabelTypes } from "@babel/core";
 import { parse } from "@babel/parser";
 import * as t from "@babel/types";
-import traverse from "@babel/traverse";
-import generate from "@babel/generator";
+
+const traverse = require("@babel/traverse")
+  .default as typeof import("@babel/traverse");
+const generate = require("@babel/generator")
+  .default as typeof import("@babel/generator");
 
 export { traverse, generate, parse, t };
 export type { BabelTypes, NodePath };
