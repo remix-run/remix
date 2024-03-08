3eb2b84db56a24e6d12380fdcd96848adbdbeffe
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2024-03-08 13:55:22 +1100

    comment

--- packages/remix-dev/vite/babel.ts
+++ packages/remix-dev/vite/babel.ts
@@ -4,6 +4,8 @@ import type { types as BabelTypes } from "@babel/core";
 import { parse } from "@babel/parser";
 import * as t from "@babel/types";
 
+// These `require`s were needed to support building within vite-ecosystem-ci,
+// otherwise we get errors that `traverse` and `generate` are not functions.
 const traverse = require("@babel/traverse")
   .default as typeof import("@babel/traverse").default;
 const generate = require("@babel/generator")
