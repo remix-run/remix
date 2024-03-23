3a9f5e20d4192626f87430540b036ee596c886a6
AndrewOt <aottavianospam@outlook.com>
2024-03-23 11:57:22 -0500

    Update root.tsx with meta export

--- templates/vite/app/root.tsx
+++ templates/vite/app/root.tsx
@@ -6,12 +6,17 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 
+export const meta: MetaFunction = () => {
+  return [
+    { charSet: "utf-8" },
+    { viewport: "width=device-width,initial-scale=1" },
+  ];
+};
+
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
       </head>
