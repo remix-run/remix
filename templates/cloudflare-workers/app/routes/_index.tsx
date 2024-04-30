b020c01f94db1092fcb6813be239c260cf2b1a5f
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2024-04-30 14:31:14 +1000

    Add Vite-based `cloudflare-workers` template

---
+++ templates/cloudflare-workers/app/routes/_index.tsx
@@ -0,0 +1,35 @@
+import type { MetaFunction } from "@remix-run/cloudflare";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "New Remix App" },
+    {
+      name: "description",
+      content: "Welcome to Remix! Using Vite and Cloudflare Workers!",
+    },
+  ];
+};
+
+export default function Index() {
+  return (
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
+      <h1>Welcome to Remix (with Vite and Cloudflare Workers)</h1>
+      <ul>
+        <li>
+          <a
+            target="_blank"
+            href="https://developers.cloudflare.com/workers/"
+            rel="noreferrer"
+          >
+            Cloudflare Workers Docs
+          </a>
+        </li>
+        <li>
+          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
+            Remix Docs
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+}
