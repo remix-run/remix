bcde53b2e6258756e5053129a84ef3f929e7fb31
wangb <wangbinyq@gmail.com>
2024-05-23 03:16:06 +0800

    update deno to use built in http server (#9210)

--- templates/classic-remix-compiler/deno/server.ts
+++ templates/classic-remix-compiler/deno/server.ts
@@ -1,7 +1,6 @@
 import { createRequestHandlerWithStaticFiles } from "@remix-run/deno";
 // Import path interpreted by the Remix compiler
 import * as build from "@remix-run/dev/server-build";
-import { serve } from "https://deno.land/std@0.128.0/http/server.ts";
 
 const remixHandler = createRequestHandlerWithStaticFiles({
   build,
@@ -9,5 +8,4 @@ const remixHandler = createRequestHandlerWithStaticFiles({
 });
 
 const port = Number(Deno.env.get("PORT")) || 8000;
-console.log(`Listening on http://localhost:${port}`);
-serve(remixHandler, { port });
+Deno.serve({ port }, remixHandler);
