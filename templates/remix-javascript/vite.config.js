71fe552d1b6993ef091ba8a3a1228cf17677bc6c
Brooks Lybrand <brookslybrand@gmail.com>
2024-05-23 11:31:03 -0500

    bump templates to node 20 and remove installGlobals

--- templates/remix-javascript/vite.config.js
+++ templates/remix-javascript/vite.config.js
@@ -1,8 +1,5 @@
 import { vitePlugin as remix } from "@remix-run/dev";
-import { installGlobals } from "@remix-run/node";
 import { defineConfig } from "vite";
-
-installGlobals();
 
 export default defineConfig({
   plugins: [
