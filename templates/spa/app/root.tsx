055fbf4704d04f04f71806121ed8c55bdf1fbf48
Brooks Lybrand <brookslybrand@gmail.com>
2024-06-12 16:32:19 +0200

    Fix tailwind imports

--- templates/spa/app/root.tsx
+++ templates/spa/app/root.tsx
@@ -5,7 +5,7 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
-import "./tailwind.css?url";
+import "./tailwind.css";
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
