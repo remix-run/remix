8aff54cf3d80ab06e28dc5d589118b694ef0b94f
Courey Elliott <coureyelliott@gmail.com>
2024-06-27 09:41:29 -0400

    reverting to previous fix

--- packages/remix-architect/server.ts
+++ packages/remix-architect/server.ts
@@ -88,9 +88,7 @@ export function createRemixHeaders(
   }
 
   if (requestCookies) {
-    for (let cookie of requestCookies) {
-      headers.append("Cookie", cookie);
-    }
+    headers.append("Cookie", requestCookies.join("; "));
   }
 
   return headers;
