64160a03ea28c181245b5198c72bdb38e7d20e7f
Matt Brophy <matt@brophy.org>
2024-07-18 08:53:03 -0400

    Update E2E tests

--- integration/single-fetch-test.ts
+++ integration/single-fetch-test.ts
@@ -1009,6 +1009,7 @@ test.describe("single-fetch", () => {
         status: 302,
         redirect: "/target",
         reload: false,
+        replace: false,
         revalidate: false,
       },
     });
@@ -1148,6 +1149,7 @@ test.describe("single-fetch", () => {
         status: 302,
         redirect: "/target",
         reload: false,
+        replace: false,
         revalidate: false,
       },
     });
@@ -1280,6 +1282,7 @@ test.describe("single-fetch", () => {
         status: 302,
         redirect: "/target",
         reload: false,
+        replace: false,
         revalidate: false,
       },
     });
@@ -1329,6 +1332,62 @@ test.describe("single-fetch", () => {
         status: 302,
         redirect: "/target",
         reload: false,
+        replace: false,
+        revalidate: false,
+      },
+    });
+    expect(status).toBe(202);
+
+    let appFixture = await createAppFixture(fixture);
+    let app = new PlaywrightFixture(appFixture, page);
+    await app.goto("/");
+    await app.clickLink("/data");
+    await page.waitForSelector("#target");
+    expect(await app.getHtml("#target")).toContain("Target");
+  });
+
+  test("processes thrown loader replace redirects via Response", async ({
+    page,
+  }) => {
+    let fixture = await createFixture({
+      config: {
+        future: {
+          unstable_singleFetch: true,
+        },
+      },
+      files: {
+        ...files,
+        "app/routes/data.tsx": js`
+          import { replace } from '@remix-run/node';
+          export function loader() {
+            throw replace('/target');
+          }
+          export default function Component() {
+            return null
+          }
+        `,
+        "app/routes/target.tsx": js`
+          export default function Component() {
+            return <h1 id="target">Target</h1>
+          }
+        `,
+      },
+    });
+
+    console.error = () => {};
+
+    let res = await fixture.requestDocument("/data");
+    expect(res.status).toBe(302);
+    expect(res.headers.get("Location")).toBe("/target");
+    expect(await res.text()).toBe("");
+
+    let { status, data } = await fixture.requestSingleFetchData("/data.data");
+    expect(data).toEqual({
+      [SingleFetchRedirectSymbol]: {
+        status: 302,
+        redirect: "/target",
+        reload: false,
+        replace: true,
         revalidate: false,
       },
     });
@@ -1393,6 +1452,7 @@ test.describe("single-fetch", () => {
       status: 302,
       redirect: "/target",
       reload: false,
+      replace: false,
       revalidate: false,
     });
     expect(status).toBe(202);
@@ -1551,6 +1611,7 @@ test.describe("single-fetch", () => {
       status: 302,
       redirect: "/target",
       reload: false,
+      replace: false,
       revalidate: false,
     });
     expect(status).toBe(202);
@@ -1702,6 +1763,7 @@ test.describe("single-fetch", () => {
       status: 302,
       redirect: "/target",
       reload: false,
+      replace: false,
       revalidate: false,
     });
     expect(status).toBe(202);
@@ -1759,6 +1821,7 @@ test.describe("single-fetch", () => {
       status: 302,
       redirect: "/target",
       reload: false,
+      replace: false,
       revalidate: false,
     });
     expect(status).toBe(202);
@@ -1858,6 +1921,7 @@ test.describe("single-fetch", () => {
         status: 302,
         redirect: "/target",
         reload: false,
+        replace: false,
         revalidate: false,
       },
     });
@@ -1960,6 +2024,7 @@ test.describe("single-fetch", () => {
       status: 302,
       redirect: "/target",
       reload: false,
+      replace: false,
       revalidate: false,
     });
     expect(status).toBe(202);
