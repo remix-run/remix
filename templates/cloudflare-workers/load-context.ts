d2a9bf95224f052b75cbe295da256c2c2f401b30
Andrew Patton <andrew@acusti.ca>
2024-07-18 10:07:20 -0700

    Fix type of AppLoadContext’s cloudflare.caches property

--- templates/cloudflare-workers/load-context.ts
+++ templates/cloudflare-workers/load-context.ts
@@ -1,7 +1,14 @@
 import { type PlatformProxy } from "wrangler";
 
+// NOTE: PlatformProxy’s caches property is incompatible with the caches global
+// https://github.com/cloudflare/workers-sdk/blob/main/packages/wrangler/src/api/integrations/platform/caches.ts
+// TS error: Property 'default' is missing in type 'CacheStorage' but required in type 'CacheStorage_2'.
+type Cloudflare = Omit<PlatformProxy<Env>, "dispose" | "caches"> & {
+  caches: CacheStorage;
+};
+
 declare module "@remix-run/cloudflare" {
   interface AppLoadContext {
-    cloudflare: Omit<PlatformProxy<Env>, "dispose">;
+    cloudflare: Cloudflare;
   }
 }
