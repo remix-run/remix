d446ab6fdba07324790cfc749968568418ff8eac
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2024-07-26 11:41:06 +1000

    Review

--- templates/cloudflare-workers/load-context.ts
+++ templates/cloudflare-workers/load-context.ts
@@ -1,8 +1,7 @@
 import { type PlatformProxy } from "wrangler";
 
-// NOTE: PlatformProxy’s caches property is incompatible with the caches global
+// PlatformProxy’s caches property is incompatible with the caches global
 // https://github.com/cloudflare/workers-sdk/blob/main/packages/wrangler/src/api/integrations/platform/caches.ts
-// TS error: Property 'default' is missing in type 'CacheStorage' but required in type 'CacheStorage_2'.
 type Cloudflare = Omit<PlatformProxy<Env>, "dispose" | "caches"> & {
   caches: CacheStorage;
 };
