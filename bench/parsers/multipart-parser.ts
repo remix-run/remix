365f6c217bfaa8edb574b3050d927bf49fee916d
Michael Jackson <michael@jackson.us>
2024-08-04 14:53:02 -0700

    Parse a stream in the benchmark

--- bench/parsers/multipart-parser.ts
+++ bench/parsers/multipart-parser.ts
@@ -3,11 +3,18 @@ import { MultipartParser } from '@mjackson/multipart-parser';
 import { MultipartMessage } from '../messages.js';
 
 export async function parse(message: MultipartMessage): Promise<number> {
+  let stream = new ReadableStream({
+    start(controller) {
+      for (let chunk of message.generateChunks()) {
+        controller.enqueue(chunk);
+      }
+      controller.close();
+    },
+  });
+
   let start = performance.now();
 
-  let parser = new MultipartParser(message.boundary, { maxFileSize: Infinity });
-
-  await parser.parse(message.generateChunks(), (_) => {
+  await new MultipartParser(message.boundary, { maxFileSize: Infinity }).parse(stream, (_) => {
     // Do nothing
   });
 
