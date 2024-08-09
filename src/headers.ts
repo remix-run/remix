717775b84259071a59cd7cc2040b1a72fd51ac8e
Alex Anderson <hey@ralexanderson.com>
2024-08-09 13:05:02 -0400

    Add Cache Control header implementation.

--- src/headers.ts
+++ src/headers.ts
@@ -2,5 +2,6 @@ export { ContentDispositionInit, ContentDisposition } from './lib/content-dispos
 export { ContentTypeInit, ContentType } from './lib/content-type.js';
 export { CookieInit, Cookie } from './lib/cookie.js';
 export { SetCookieInit, SetCookie } from './lib/set-cookie.js';
+export { CacheControlInit, CacheControl } from './lib/cache-control.js';
 
 export { SuperHeaders as default, SuperHeadersInit, SuperHeaders } from './lib/super-headers.js';
