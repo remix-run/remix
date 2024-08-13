36cd2a63468bbd517a800bdf11bab8667c45dabf
Michael Jackson <michael@jackson.us>
2024-08-12 23:31:39 -0700

    Add version script

---
+++ scripts/version.js
@@ -0,0 +1,30 @@
+import * as cp from 'node:child_process';
+import * as fs from 'node:fs';
+import * as path from 'node:path';
+import * as process from 'node:process';
+
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+const packageVersion = process.env.npm_package_version;
+
+// Update CHANGELOG.md with the current version
+let changelogPath = path.resolve(__dirname, '../CHANGELOG.md');
+let changelog = fs.readFileSync(changelogPath, 'utf-8');
+let match = /^## HEAD\n/m.exec(changelog);
+if (match) {
+  let today = new Date().toLocaleDateString('en-US', {
+    month: 'short',
+    day: 'numeric',
+    year: 'numeric',
+  });
+
+  changelog =
+    changelog.slice(0, match.index) +
+    `## v${packageVersion} (${today})\n` +
+    changelog.slice(match.index + match[0].length);
+
+  fs.writeFileSync(changelogPath, changelog);
+  cp.execSync('git add CHANGELOG.md');
+} else {
+  console.error('Could not find "## HEAD" in CHANGELOG.md');
+  process.exit(1);
+}
