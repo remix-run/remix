9af36182e9ff65e7035cb1bb32f7cdc9a0a45401
Michael Jackson <michael@jackson.us>
2024-08-13 18:55:38 -0700

    Update version script

--- scripts/version.js
+++ scripts/version.js
@@ -6,13 +6,6 @@ import * as process from 'node:process';
 const __dirname = path.dirname(new URL(import.meta.url).pathname);
 const packageVersion = process.env.npm_package_version;
 
-// Update jsr.json with the current version
-let jsrJsonPath = path.resolve(__dirname, '../jsr.json');
-let jsrJson = JSON.parse(fs.readFileSync(jsrJsonPath, 'utf-8'));
-jsrJson.version = packageVersion;
-fs.writeFileSync(jsrJsonPath, JSON.stringify(jsrJson, null, 2) + '\n');
-cp.execSync('git add jsr.json');
-
 // Update CHANGELOG.md with the current version
 let changelogPath = path.resolve(__dirname, '../CHANGELOG.md');
 let changelog = fs.readFileSync(changelogPath, 'utf-8');
@@ -35,3 +28,10 @@ if (match) {
   console.error('Could not find "## HEAD" in CHANGELOG.md');
   process.exit(1);
 }
+
+// Update jsr.json with the current version
+let jsrJsonPath = path.resolve(__dirname, '../jsr.json');
+let jsrJson = JSON.parse(fs.readFileSync(jsrJsonPath, 'utf-8'));
+jsrJson.version = packageVersion;
+fs.writeFileSync(jsrJsonPath, JSON.stringify(jsrJson, null, 2) + '\n');
+cp.execSync('git add jsr.json');
