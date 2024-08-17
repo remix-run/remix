3c5bc5c4c53e07bcce59939d733d9d6377417223
Michael Jackson <michael@jackson.us>
2024-08-17 00:31:45 -0700

    Use low-level API in benchmark

--- bench/parsers/multipart-parser.ts
+++ bench/parsers/multipart-parser.ts
@@ -1,27 +1,13 @@
-import { parseMultipartRequest } from '@mjackson/multipart-parser';
+import { MultipartParser } from '@mjackson/multipart-parser';
 
 import { MultipartMessage } from '../messages.js';
 
 export async function parse(message: MultipartMessage): Promise<number> {
-  let request = {
-    headers: new Headers({
-      'Content-Type': `multipart/form-data; boundary=${message.boundary}`,
-    }),
-    body: new ReadableStream({
-      start(controller) {
-        for (let chunk of message.generateChunks()) {
-          controller.enqueue(chunk);
-        }
-        controller.close();
-      },
-    }),
-  } as unknown as Request;
-
   let start = performance.now();
 
-  for await (let _ of parseMultipartRequest(request)) {
+  await new MultipartParser(message.boundary).parse(message.generateChunks(), (_part) => {
     // Do nothing
-  }
+  });
 
   return performance.now() - start;
 }
