e15394a81f40e7492d957d3fea6bdb7be3d805bf
Michael Jackson <michael@jackson.us>
2024-08-19 14:37:55 -0700

    Add LICENSE

--- src/lib/form-data.spec.ts
+++ src/lib/form-data.spec.ts
@@ -1,10 +1,24 @@
 import * as assert from "node:assert/strict";
-import { describe, it } from "node:test";
+import { describe, it, mock } from "node:test";
 
-import { parseFormData } from "./form-data.js";
+import { type FileUploadHandler, parseFormData } from "./form-data.js";
 
 describe("parseFormData", () => {
-  it("should parse a multipart request", async () => {
+  it("parses a application/x-www-form-urlencoded request", async () => {
+    let request = new Request("http://localhost:8080", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+      },
+      body: "text=Hello%2C%20World!"
+    });
+
+    let formData = await parseFormData(request);
+
+    assert.equal(formData.get("text"), "Hello, World!");
+  });
+
+  it("parses a multipart/form-data request", async () => {
     let request = new Request("http://localhost:8080", {
       method: "POST",
       headers: {
@@ -34,5 +48,34 @@ describe("parseFormData", () => {
     assert.equal(file.name, "example.txt");
     assert.equal(file.type, "text/plain");
     assert.equal(await file.text(), "This is an example file.");
+  });
+
+  it("calls the file upload handler for each file part", async () => {
+    let request = new Request("http://localhost:8080", {
+      method: "POST",
+      headers: {
+        "Content-Type":
+          "multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW"
+      },
+      body: [
+        "------WebKitFormBoundary7MA4YWxkTrZu0gW",
+        'Content-Disposition: form-data; name="file1"; filename="example.txt"',
+        "Content-Type: text/plain",
+        "",
+        "This is an example file.",
+        "------WebKitFormBoundary7MA4YWxkTrZu0gW",
+        'Content-Disposition: form-data; name="file2"; filename="example.txt"',
+        "Content-Type: text/plain",
+        "",
+        "This is another example file.",
+        "------WebKitFormBoundary7MA4YWxkTrZu0gW--"
+      ].join("\r\n")
+    });
+
+    let fileUploadHandler = mock.fn<FileUploadHandler>();
+
+    await parseFormData(request, fileUploadHandler);
+
+    assert.equal(fileUploadHandler.mock.calls.length, 2);
   });
 });
