c8545906d29a6a8496ae7eb372b15cfbdccdfbce
Michael Jackson <michael@jackson.us>
2024-08-21 11:21:51 -0700

    Export everything

--- src/file-storage.ts
+++ src/file-storage.ts
@@ -1,1 +1,5 @@
+export { type ByteRange, getByteLength, getIndexes } from "./lib/byte-range.js";
 export { type FileStorage } from "./lib/file-storage.js";
+export { LazyFile } from "./lib/lazy-file.js";
+export { LocalFileStorage } from "./lib/local-file-storage.js";
+export { MemoryFileStorage } from "./lib/memory-file-storage.js";
