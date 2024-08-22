01d208779d3b9ee64cd863e8ca7e0bd0e88f18b8
Sergio Xalambrí <hello@sergiodxa.com>
2024-08-22 00:51:36 -0500

    Move local and memory to separate exports

--- src/file-storage.ts
+++ src/file-storage.ts
@@ -1,3 +1,1 @@
 export { type FileStorage } from "./lib/file-storage.js";
-export { LocalFileStorage } from "./lib/local-file-storage.js";
-export { MemoryFileStorage } from "./lib/memory-file-storage.js";
