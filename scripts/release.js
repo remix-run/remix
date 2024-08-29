9d781a1bee5a00b5887d8811e7fa9b53dfce4cc6
Michael Jackson <michael@jackson.us>
2024-08-29 08:54:31 -0700

    Add GH releases

--- scripts/release.js
+++ scripts/release.js
@@ -1,9 +1,13 @@
-import { releaseVersion, releaseChangelog } from 'nx/release/index.js';
+import { releaseVersion, releaseChangelog, releasePublish } from 'nx/release/index.js';
 
-const dryRun = true;
+const dryRun = false;
 const verbose = true;
 
-await releaseVersion({ dryRun, verbose });
+let { projectsVersionData } = await releaseVersion({ dryRun, verbose });
+
+await releaseChangelog({ dryRun, verbose, versionData: projectsVersionData });
+
+let statusCode = await releasePublish({ dryRun, verbose });
 
 // Force exit because it's connected to the Nx daemon
 process.exit(0);
