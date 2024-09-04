1ac5d1c26c722252263bdcc865604abc02c0135f
Michael Jackson <michael@jackson.us>
2024-09-04 11:23:04 -0700

    Accept an open file descriptor in writeFile

--- packages/lazy-file/src/fs.ts
+++ packages/lazy-file/src/fs.ts
@@ -23,6 +23,10 @@ export interface GetFileOptions {
  * Returns a `File` from the local filesytem.
  *
  * [MDN `File` Reference](https://developer.mozilla.org/en-US/docs/Web/API/File)
+ *
+ * @param filename The path to the file
+ * @param options Options to override the file's metadata
+ * @returns A `File` object
  */
 export function openFile(filename: string, options?: GetFileOptions): File {
   let stats = fs.statSync(filename);
@@ -64,20 +68,26 @@ function streamFile(filename: string, start = 0, end = Infinity): ReadableStream
 export { openFile as getFile };
 
 /**
- * Writes a `File` to the local filesytem with the given `filename` and resolves when the stream is
- * finished.
+ * Writes a `File` to the local filesytem and resolves when the stream is finished.
  *
  * [MDN `File` Reference](https://developer.mozilla.org/en-US/docs/Web/API/File)
+ *
+ * @param to The path to write the file to, or an open file descriptor
+ * @param file The file to write
+ * @returns A promise that resolves when the file is written
  */
-export function writeFile(filename: string, file: File): Promise<void> {
+export function writeFile(to: string | number | fs.promises.FileHandle, file: File): Promise<void> {
   return new Promise(async (resolve) => {
-    let write = fs.createWriteStream(filename);
+    let writeStream =
+      typeof to === 'string'
+        ? fs.createWriteStream(to)
+        : fs.createWriteStream('ignored', { fd: to });
 
     for await (let chunk of file.stream()) {
-      write.write(chunk);
+      writeStream.write(chunk);
     }
 
-    write.end(() => {
+    writeStream.end(() => {
       resolve();
     });
   });
