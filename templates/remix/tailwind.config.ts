d8031ac4202d62df8f6e4be68b37edf1671c66bb
Brooks Lybrand <brookslybrand@gmail.com>
2024-09-11 16:05:39 -0500

    Inline fonts because I don't want to fight eslint no more

--- templates/remix/tailwind.config.ts
+++ templates/remix/tailwind.config.ts
@@ -1,12 +1,20 @@
 import type { Config } from "tailwindcss";
-import defaultTheme from "tailwindcss/defaultTheme";
 
 export default {
   content: ["./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       fontFamily: {
-        sans: ['"Inter"', ...defaultTheme.fontFamily.sans],
+        sans: [
+          '"Inter"',
+          "ui-sans-serif",
+          "system-ui",
+          "sans-serif",
+          '"Apple Color Emoji"',
+          '"Segoe UI Emoji"',
+          '"Segoe UI Symbol"',
+          '"Noto Color Emoji"',
+        ],
       },
     },
   },
