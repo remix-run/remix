67c3de5573d14a23286511636893a99f48854838
Zach Wily <zach@zwily.com>
2024-10-04 10:24:03 -0600

    put LiveReload back

--- templates/classic-remix-compiler/cloudflare-workers/app/root.tsx
+++ templates/classic-remix-compiler/cloudflare-workers/app/root.tsx
@@ -1,5 +1,6 @@
 import {
   Links,
+  LiveReload,
   Meta,
   Outlet,
   Scripts,
@@ -36,6 +37,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         {children}
         <ScrollRestoration />
         <Scripts />
+        <LiveReload />
       </body>
     </html>
   );
