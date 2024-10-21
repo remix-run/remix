09c5dcb7a777a8792f74ecb2cc5008d3961aac14
Jake Ginnivan <jake@ginnivan.net>
2024-10-21 14:15:00 +0800

    Update single fetch deprecation notice to also include info about null
    Resource routes can no longer just return null, call this out in the deprecation notice.

--- packages/remix-server-runtime/deprecations.ts
+++ packages/remix-server-runtime/deprecations.ts
@@ -4,7 +4,7 @@ export function resourceRouteJsonWarning(
 ) {
   return (
     "⚠️ REMIX FUTURE CHANGE: Resource routes will no longer be able to " +
-    "return raw JavaScript objects in v3 when Single Fetch becomes the default. " +
+    "return null or raw JavaScript objects in v3 when Single Fetch becomes the default. " +
     "You can prepare for this change at your convenience by wrapping the data " +
     `returned from your \`${type}\` function in the \`${routeId}\` route with ` +
     "`json()`.  For instructions on making this change see " +
