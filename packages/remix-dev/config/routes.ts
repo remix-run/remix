2dcd85feb8dfc70dcb6ff37d2fb06bec35075fad
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2024-10-29 22:27:49 +1100

    Add `routes.ts` support behind future flag (#10107)

--- packages/remix-dev/config/routes.ts
+++ packages/remix-dev/config/routes.ts
@@ -1,10 +1,24 @@
 import * as path from "node:path";
+import * as v from "valibot";
+
+import invariant from "../invariant";
+
+let routeConfigAppDirectory: string;
+
+export function setRouteConfigAppDirectory(directory: string) {
+  routeConfigAppDirectory = directory;
+}
 
 /**
- * A route that was created using `defineRoutes` or created conventionally from
- * looking at the files on the filesystem.
+ * Provides the absolute path to the app directory, for use within `routes.ts`.
+ * This is designed to support resolving file system routes.
  */
-export interface ConfigRoute {
+export function getRouteConfigAppDirectory() {
+  invariant(routeConfigAppDirectory);
+  return routeConfigAppDirectory;
+}
+
+export interface RouteManifestEntry {
   /**
    * The path this route uses to match on the URL pathname.
    */
@@ -40,7 +54,197 @@ export interface ConfigRoute {
 }
 
 export interface RouteManifest {
-  [routeId: string]: ConfigRoute;
+  [routeId: string]: RouteManifestEntry;
+}
+
+export function routeManifestToRouteConfig(
+  routeManifest: RouteManifest,
+  rootId = "root"
+): RouteConfigEntry[] {
+  let routeConfigById: {
+    [id: string]: Omit<RouteConfigEntry, "id"> &
+      Required<Pick<RouteConfigEntry, "id">>;
+  } = {};
+
+  for (let id in routeManifest) {
+    let route = routeManifest[id];
+    routeConfigById[id] = {
+      id: route.id,
+      file: route.file,
+      path: route.path,
+      index: route.index,
+      caseSensitive: route.caseSensitive,
+    };
+  }
+
+  let routeConfig: RouteConfigEntry[] = [];
+
+  for (let id in routeConfigById) {
+    let route = routeConfigById[id];
+    let parentId = routeManifest[route.id].parentId;
+    if (parentId === rootId) {
+      routeConfig.push(route);
+    } else {
+      let parentRoute = parentId && routeConfigById[parentId];
+      if (parentRoute) {
+        parentRoute.children = parentRoute.children || [];
+        parentRoute.children.push(route);
+      }
+    }
+  }
+
+  return routeConfig;
+}
+
+/**
+ * Configuration for an individual route, for use within `routes.ts`. As a
+ * convenience, route config entries can be created with the {@link route},
+ * {@link index} and {@link layout} helper functions.
+ */
+export interface RouteConfigEntry {
+  /**
+   * The unique id for this route.
+   */
+  id?: string;
+
+  /**
+   * The path this route uses to match on the URL pathname.
+   */
+  path?: string;
+
+  /**
+   * Should be `true` if it is an index route. This disallows child routes.
+   */
+  index?: boolean;
+
+  /**
+   * Should be `true` if the `path` is case-sensitive. Defaults to `false`.
+   */
+  caseSensitive?: boolean;
+
+  /**
+   * The path to the entry point for this route, relative to
+   * `config.appDirectory`.
+   */
+  file: string;
+
+  /**
+   * The child routes.
+   */
+  children?: RouteConfigEntry[];
+}
+
+export const routeConfigEntrySchema: v.BaseSchema<
+  RouteConfigEntry,
+  any,
+  v.BaseIssue<unknown>
+> = v.pipe(
+  v.custom<RouteConfigEntry>((value) => {
+    return !(
+      typeof value === "object" &&
+      value !== null &&
+      "then" in value &&
+      "catch" in value
+    );
+  }, "Invalid type: Expected object but received a promise. Did you forget to await?"),
+  v.object({
+    id: v.optional(v.string()),
+    path: v.optional(v.string()),
+    index: v.optional(v.boolean()),
+    caseSensitive: v.optional(v.boolean()),
+    file: v.string(),
+    children: v.optional(v.array(v.lazy(() => routeConfigEntrySchema))),
+  })
+);
+
+export const resolvedRouteConfigSchema = v.array(routeConfigEntrySchema);
+type ResolvedRouteConfig = v.InferInput<typeof resolvedRouteConfigSchema>;
+
+/**
+ * Route config to be exported via the `routes` export within `routes.ts`.
+ */
+export type RouteConfig = ResolvedRouteConfig | Promise<ResolvedRouteConfig>;
+
+export function validateRouteConfig({
+  routeConfigFile,
+  routeConfig,
+}: {
+  routeConfigFile: string;
+  routeConfig: unknown;
+}): { valid: false; message: string } | { valid: true } {
+  if (!routeConfig) {
+    return {
+      valid: false,
+      message: `No "routes" export defined in "${routeConfigFile}.`,
+    };
+  }
+
+  if (!Array.isArray(routeConfig)) {
+    return {
+      valid: false,
+      message: `Route config in "${routeConfigFile}" must be an array.`,
+    };
+  }
+
+  let { issues } = v.safeParse(resolvedRouteConfigSchema, routeConfig);
+
+  if (issues?.length) {
+    let { root, nested } = v.flatten(issues);
+    return {
+      valid: false,
+      message: [
+        `Route config in "${routeConfigFile}" is invalid.`,
+        root ? `${root}` : [],
+        nested
+          ? Object.entries(nested).map(
+              ([path, message]) => `Path: routes.${path}\n${message}`
+            )
+          : [],
+      ]
+        .flat()
+        .join("\n\n"),
+    };
+  }
+
+  return { valid: true };
+}
+
+export function configRoutesToRouteManifest(
+  routes: RouteConfigEntry[],
+  rootId = "root"
+): RouteManifest {
+  let routeManifest: RouteManifest = {};
+
+  function walk(route: RouteConfigEntry, parentId: string) {
+    let id = route.id || createRouteId(route.file);
+    let manifestItem: RouteManifestEntry = {
+      id,
+      parentId,
+      file: route.file,
+      path: route.path,
+      index: route.index,
+      caseSensitive: route.caseSensitive,
+    };
+
+    if (routeManifest.hasOwnProperty(id)) {
+      throw new Error(
+        `Unable to define routes with duplicate route id: "${id}"`
+      );
+    }
+    routeManifest[id] = manifestItem;
+
+    if (route.children) {
+      for (let child of route.children) {
+        walk(child, id);
+      }
+    }
+  }
+
+  for (let route of routes) {
+    walk(route, rootId);
+  }
+
+  return routeManifest;
 }
 
 export interface DefineRouteOptions {
@@ -115,7 +319,7 @@ export function defineRoutes(
   callback: (defineRoute: DefineRouteFunction) => void
 ): RouteManifest {
   let routes: RouteManifest = Object.create(null);
-  let parentRoutes: ConfigRoute[] = [];
+  let parentRoutes: RouteManifestEntry[] = [];
   let alreadyReturned = false;
 
   let defineRoute: DefineRouteFunction = (
@@ -143,7 +347,7 @@ export function defineRoutes(
       options = optionsOrChildren || {};
     }
 
-    let route: ConfigRoute = {
+    let route: RouteManifestEntry = {
       path: path ? path : undefined,
       index: options.index ? true : undefined,
       caseSensitive: options.caseSensitive ? true : undefined,
--- packages/remix-dev/index.ts
+++ packages/remix-dev/index.ts
@@ -5,6 +5,19 @@ export type { AppConfig, RemixConfig as ResolvedRemixConfig } from "./config";
 export * as cli from "./cli/index";
 
 export type { Manifest as AssetsManifest } from "./manifest";
+export type {
+  DefineRoutesFunction as UNSAFE_DefineRoutesFunction,
+  RouteManifest as UNSAFE_RouteManifest,
+  RouteManifestEntry as UNSAFE_RouteManifestEntry,
+  RouteConfig as UNSAFE_RouteConfig,
+  RouteConfigEntry as UNSAFE_RouteConfigEntry,
+} from "./config/routes";
+export {
+  defineRoutes as UNSAFE_defineRoutes,
+  routeManifestToRouteConfig as UNSAFE_routeManifestToRouteConfig,
+  getRouteConfigAppDirectory as UNSAFE_getRouteConfigAppDirectory,
+} from "./config/routes";
+export { flatRoutes as UNSAFE_flatRoutes } from "./config/flat-routes";
 export { getDependenciesToBundle } from "./dependencies";
 export type {
   BuildManifest,
