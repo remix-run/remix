f17baccc93d600d820e3ed9b9b13113129c2ddcc
Michael Jackson <michael@jackson.us>
2024-11-14 13:53:11 -0800

    Update release script

--- scripts/release.ts
+++ scripts/release.ts
@@ -7,6 +7,8 @@ import * as semver from 'semver';
 
 const __dirname = path.dirname(new URL(import.meta.url).pathname);
 
+// This script should be run as part of an npm script ("scripts" key in package.json)
+// so that it can access the package name and version from environment variables.
 const packageName = process.env.npm_package_name;
 const packageVersion = process.env.npm_package_version;
 const releaseType = process.argv[2];
@@ -20,7 +22,7 @@ let newVersion = semver.inc(packageVersion, releaseType as semver.ReleaseType);
 let [_packageScope, packageNameWithoutScope] = packageName.split('/');
 let packageDir = path.resolve(__dirname, '..', 'packages', packageNameWithoutScope);
 
-// Ensure git staging area is clean
+// 1) Ensure git staging area is clean
 let status = cp.execSync('git status --porcelain').toString();
 if (status !== '') {
   console.error('Git staging area is not clean');
@@ -30,19 +32,19 @@ if (status !== '') {
 console.log(`Releasing ${packageNameWithoutScope}@${newVersion} ...`);
 console.log();
 
-function execWithLog(command: string) {
+function logAndExec(command: string) {
   console.log(`$ ${command}`);
   cp.execSync(command, { stdio: 'inherit' });
 }
 
-// Update package.json
+// 2) Update package.json with the new release version
 let packageJsonPath = path.relative(process.cwd(), path.join(packageDir, 'package.json'));
 let packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf-8'));
 packageJson.version = newVersion;
 fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2) + '\n');
-execWithLog(`git add ${packageJsonPath}`);
+logAndExec(`git add ${packageJsonPath}`);
 
-// Update CHANGELOG.md
+// 3) Swap out "## HEAD" in CHANGELOG.md with the new release version + date
 let changelogPath = path.relative(process.cwd(), path.join(packageDir, 'CHANGELOG.md'));
 let changelog = fs.readFileSync(changelogPath, 'utf-8');
 let match = /^## HEAD\n/m.exec(changelog);
@@ -55,21 +57,21 @@ if (match) {
     changelog.slice(match.index + match[0].length);
 
   fs.writeFileSync(changelogPath, changelog);
-  execWithLog(`git add ${changelogPath}`);
+  logAndExec(`git add ${changelogPath}`);
 }
 
-// Update jsr.json
+// 4) Update jsr.json (if applicable) with the new release version
 let jsrJsonPath = path.relative(process.cwd(), path.join(packageDir, 'jsr.json'));
 if (fs.existsSync(jsrJsonPath)) {
   let jsrJson = JSON.parse(fs.readFileSync(jsrJsonPath, 'utf-8'));
   jsrJson.version = newVersion;
   fs.writeFileSync(jsrJsonPath, JSON.stringify(jsrJson, null, 2) + '\n');
-  execWithLog(`git add ${jsrJsonPath}`);
+  logAndExec(`git add ${jsrJsonPath}`);
 }
 
-// Commit and tag
+// 5) Commit and tag
 let tag = `${packageNameWithoutScope}@${newVersion}`;
-execWithLog(`git commit -m "Release ${tag}"`);
-execWithLog(`git tag ${tag}`);
+logAndExec(`git commit -m "Release ${tag}"`);
+logAndExec(`git tag ${tag}`);
 
 console.log();
