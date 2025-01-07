180420363afa51bf8308d96db1c692e1c2f4d197
Ryan Florence <rpflorence@gmail.com>
2025-01-07 15:45:08 -0700

    failing test

--- packages/file-storage/src/lib/local-file-storage.test.ts
+++ packages/file-storage/src/lib/local-file-storage.test.ts
@@ -43,4 +43,31 @@ describe('LocalFileStorage', () => {
     assert.ok(!(await storage.has('hello')));
     assert.equal(await storage.get('hello'), null);
   });
+
+  it('handles race conditions', async () => {
+    let storage = new LocalFileStorage(directory);
+    let lastModified = Date.now();
+
+    let file1 = new File(['Hello, world!'], 'hello1.txt', {
+      type: 'text/plain',
+      lastModified,
+    });
+
+    let file2 = new File(['Hello, universe!'], 'hello2.txt', {
+      type: 'text/plain',
+      lastModified,
+    });
+
+    let setPromise = storage.set('one', file1);
+    await storage.set('two', file2);
+
+    let retrieved1 = await storage.get('one');
+    assert.ok(retrieved1);
+    assert.equal(await retrieved1.text(), 'Hello, world!');
+
+    await setPromise;
+    let retrieved2 = await storage.get('two');
+    assert.ok(retrieved2);
+    assert.equal(await retrieved2.text(), 'Hello, universe!');
+  });
 });
