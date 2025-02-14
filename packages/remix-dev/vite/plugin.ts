026c244826a3d3ccc8ea35aa3a7f4468be760cc8
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2025-02-14 14:44:56 +1100

    Add Vite v6 support (#10351)

--- packages/remix-dev/vite/plugin.ts
+++ packages/remix-dev/vite/plugin.ts
@@ -39,7 +39,8 @@ import * as VirtualModule from "./vmod";
 import { resolveFileUrl } from "./resolve-file-url";
 import { combineURLs } from "./combine-urls";
 import { removeExports } from "./remove-exports";
-import { importViteEsmSync, preloadViteEsm } from "./import-vite-esm-sync";
+import { isInRemixMonorepo } from "./is-in-remix-monorepo";
+import { getVite, preloadVite } from "./vite";
 import * as ViteNode from "./vite-node";
 
 export async function resolveViteConfig({
@@ -51,7 +52,8 @@ export async function resolveViteConfig({
   mode?: string;
   root: string;
 }) {
-  let vite = await import("vite");
+  await preloadVite();
+  let vite = getVite();
 
   let viteConfig = await vite.resolveConfig(
     { mode, configFile, root },
@@ -300,7 +302,7 @@ const resolveRelativeRouteFilePath = (
   route: RouteManifestEntry,
   remixConfig: ResolvedVitePluginConfig
 ) => {
-  let vite = importViteEsmSync();
+  let vite = getVite();
   let file = route.file;
   let fullPath = path.resolve(remixConfig.appDirectory, file);
 
@@ -330,7 +332,7 @@ const resolveChunk = (
   viteManifest: Vite.Manifest,
   absoluteFilePath: string
 ) => {
-  let vite = importViteEsmSync();
+  let vite = getVite();
   let rootRelativeFilePath = vite.normalizePath(
     path.relative(ctx.rootDirectory, absoluteFilePath)
   );
@@ -708,7 +710,7 @@ export const remixVitePlugin: RemixVitePlugin = (remixUserConfig = {}) => {
       {
         rootDirectory,
         isSpaMode,
-        vite: importViteEsmSync(),
+        vite: getVite(),
         routeConfigChanged,
         viteUserConfig,
         routesViteNodeContext,
@@ -1037,10 +1039,10 @@ export const remixVitePlugin: RemixVitePlugin = (remixUserConfig = {}) => {
       name: "remix",
       config: async (_viteUserConfig, _viteConfigEnv) => {
         // Preload Vite's ESM build up-front as soon as we're in an async context
-        await preloadViteEsm();
+        await preloadVite();
 
         // Ensure sync import of Vite works after async preload
-        let vite = importViteEsmSync();
+        let vite = getVite();
 
         viteUserConfig = _viteUserConfig;
         viteConfigEnv = _viteConfigEnv;
@@ -1234,7 +1236,7 @@ export const remixVitePlugin: RemixVitePlugin = (remixUserConfig = {}) => {
           );
         }
 
-        let vite = importViteEsmSync();
+        let vite = getVite();
 
         let childCompilerConfigFile = await vite.loadConfigFromFile(
           {
@@ -1373,7 +1375,7 @@ export const remixVitePlugin: RemixVitePlugin = (remixUserConfig = {}) => {
 
         // Invalidate virtual modules and update cached plugin config via file watcher
         viteDevServer.watcher.on("all", async (eventName, rawFilepath) => {
-          let { normalizePath } = importViteEsmSync();
+          let { normalizePath } = getVite();
           let filepath = normalizePath(rawFilepath);
 
           let appFileAddedOrRemoved =
@@ -1577,7 +1579,7 @@ export const remixVitePlugin: RemixVitePlugin = (remixUserConfig = {}) => {
           return;
         }
 
-        let vite = importViteEsmSync();
+        let vite = getVite();
         let importerShort = vite.normalizePath(
           path.relative(ctx.rootDirectory, importer)
         );
@@ -1816,13 +1818,64 @@ export const remixVitePlugin: RemixVitePlugin = (remixUserConfig = {}) => {
         return modules;
       },
     },
+    {
+      name: "remix-server-change-trigger-client-hmr",
+      // This hook is only available in Vite v6+ so this is a no-op in v5.
+      // Previously the server and client modules were shared in a single module
+      // graph. This meant that changes to server code automatically resulted in
+      // client HMR updates. In Vite v6+ these module graphs are separate from
+      // each other so we need to manually trigger client HMR updates if server
+      // code has changed.
+      hotUpdate(this, { server, modules }) {
+        if (this.environment.name !== "ssr" && modules.length <= 0) {
+          return;
+        }
+
+        let clientModules = uniqueNodes(
+          modules.flatMap((mod) =>
+            getParentClientNodes(server.environments.client.moduleGraph, mod)
+          )
+        );
+
+        for (let clientModule of clientModules) {
+          server.environments.client.reloadModule(clientModule);
+        }
+      },
+    },
   ];
 };
 
-function isInRemixMonorepo() {
-  let devPath = path.dirname(require.resolve("@remix-run/dev/package.json"));
-  let devParentDir = path.basename(path.resolve(devPath, ".."));
-  return devParentDir === "packages";
+function getParentClientNodes(
+  clientModuleGraph: Vite.EnvironmentModuleGraph,
+  module: Vite.EnvironmentModuleNode
+): Vite.EnvironmentModuleNode[] {
+  if (!module.id) {
+    return [];
+  }
+
+  let clientModule = clientModuleGraph.getModuleById(module.id);
+  if (clientModule) {
+    return [clientModule];
+  }
+
+  return [...module.importers].flatMap((importer) =>
+    getParentClientNodes(clientModuleGraph, importer)
+  );
+}
+
+function uniqueNodes(
+  nodes: Vite.EnvironmentModuleNode[]
+): Vite.EnvironmentModuleNode[] {
+  let nodeUrls = new Set<string>();
+  let unique: Vite.EnvironmentModuleNode[] = [];
+  for (let node of nodes) {
+    if (nodeUrls.has(node.url)) {
+      continue;
+    }
+    nodeUrls.add(node.url);
+    unique.push(node);
+  }
+  return unique;
 }
 
 function isEqualJson(v1: unknown, v2: unknown) {
@@ -1895,7 +1948,7 @@ function getRoute(
   pluginConfig: ResolvedVitePluginConfig,
   file: string
 ): RouteManifestEntry | undefined {
-  let vite = importViteEsmSync();
+  let vite = getVite();
   let routePath = vite.normalizePath(
     path.relative(pluginConfig.appDirectory, file)
   );
