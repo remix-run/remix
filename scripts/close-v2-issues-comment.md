1eaf8d3268228060578aeb8ec4369b9175dc3c4e
Matt Brophy <matt@brophy.org>
2025-05-19 14:22:37 -0400

    Github action to help close stale v2 issues (#10626)

---
+++ scripts/close-v2-issues-comment.md
@@ -0,0 +1,17 @@
+Thank you for opening this issue, and our apologies we haven't gotten around to it yet!
+
+With the release of [React Router v7](https://remix.run/blog/react-router-v7) we are sun-setting continued development/maintenance on Remix v2. If you have not already [upgraded to React Router v7](https://reactrouter.com/upgrading/remix), we recommend you do so. We've tried to make the upgrade process as smooth as possible with our [Future Flags](https://remix.run/docs/en/main/start/future-flags). We are now in the process of cleaning up outdated issues and pull requests to improve the overall hygiene of our repositories.
+
+We plan to continue to address 2 types of issues in Remix v2:
+
+- Bugs that pose security concerns
+- Bugs that prevent upgrading to React Router v7
+
+If you believe this issue meets one of those criteria, please respond or [create a new issue](https://github.com/remix-run/remix/issues/new?template=bug_report.yml).
+
+For all other issues, ongoing maintenance will be happening in React Router v7, so:
+
+- If this is a bug, please reopen this issue [in that repo](https://github.com/remix-run/react-router/issues/new?template=bug_report.yml) with a new [minimal reproduction](https://reactrouter.com/new) against v7
+- If this is a feature request, please open a new [Proposal Discussion](https://github.com/remix-run/react-router/discussions/new?category=proposals) in React Router, and if it gets enough community support it can be considered for implementation
+
+If you have any questions you can always reach out on [Discord](https://rmx.as/discord). Thanks again for providing feedback and helping us make our framework even better!
--- scripts/close-v2-issues.mjs
+++ scripts/close-v2-issues.mjs
@@ -1,8 +1,79 @@
 import { execSync } from "node:child_process";
+import { parseArgs } from "node:util";
 
-let getIssuesCmd = (q) => `gh issue list --search "${q}" --json number`;
-let issueCmd = getIssuesCmd("is:issue state:open updated:<2024-06-30");
-console.log(`Executing command: ${issueCmd}`);
-let result = execSync(issueCmd).toString();
+const CI = process.env.CI === "true";
 
-console.log(`Result: ${result}`);
+let oneYearAgo = new Date(Date.now() - 365 * 24 * 60 * 60 * 1000);
+
+const { values: args } = parseArgs({
+  options: {
+    date: {
+      type: "string",
+      short: "d",
+      default: new Date(oneYearAgo).toISOString().substring(0, 10),
+    },
+  },
+  strict: true,
+});
+
+if (!args.date || !/^\d{4}-\d{2}-\d{2}$/.test(args.date)) {
+  console.error("Missing or invalid date - expected YYYY-MM-DD.");
+  process.exit(1);
+} else {
+  run();
+}
+
+async function run() {
+  /** @type {(ms: number) => Promise<void>} */
+  let sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+  /** @type {(q: string) => string} */
+  let getIssuesCmd = (q) => `gh issue list --search "${q}" --json number`;
+
+  /** @type {(n: number) => string} */
+  let getCommentCmd = (n) =>
+    `gh issue comment ${n} -F ./scripts/close-v2-issues-comment.md`;
+
+  /** @type {(n: number) => string} */
+  let getCloseCmd = (n) => `gh issue close ${n} -r "not planned"`;
+
+  let issueCmd = getIssuesCmd(`is:issue state:open updated:<${args.date}`);
+  console.log(`Executing command: ${issueCmd}`);
+  let result = execSync(issueCmd).toString();
+  console.log(`Result: ${result}`);
+
+  let issues = JSON.parse(result).map((i) => i.number);
+  if (issues.length > 50) {
+    console.log("❌ Refusing to close more than 50 issues at once, exiting.");
+    process.exit(1);
+  }
+
+  console.log(`Parsed ${issues.length} issues`);
+
+  for (let issue of issues) {
+    console.log(`--- Processing issue #${issue} ---`);
+    let commentResult = runCmdIfTokenExists(getCommentCmd(issue));
+    console.log(`Commented on issue #${issue}: ${commentResult}`);
+    await sleep(250);
+
+    runCmdIfTokenExists(getCloseCmd(issue));
+    // No log here since the GH CLI already logs for issue close
+    await sleep(250);
+  }
+
+  console.log("Done!");
+}
+
+/**
+ * @param {string} cmd
+ * @return {string}
+ */
+function runCmdIfTokenExists(cmd) {
+  if (CI) {
+    console.log();
+    return execSync(cmd).toString();
+  } else {
+    console.log(`⚠️ Local run, skipping command: ${cmd}`);
+    return "<skipped>";
+  }
+}
