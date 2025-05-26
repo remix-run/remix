2f1e040eb80cc9b096ab44478f9752b190ddca9c
Matt Brophy <matt@brophy.org>
2025-05-26 10:47:40 -0400

    Update script to be able to close v2 prs as well

--- scripts/close-v2-issues.mjs
+++ scripts/close-v2-issues.mjs
@@ -12,11 +12,21 @@ const { values: args } = parseArgs({
       short: "d",
       default: new Date(oneYearAgo).toISOString().substring(0, 10),
     },
+    type: {
+      type: "string",
+      short: "t",
+      default: "issues", // issues | prs
+    },
   },
   strict: true,
 });
 
-if (!args.date || !/^\d{4}-\d{2}-\d{2}$/.test(args.date)) {
+console.log(args);
+
+if (!args.type || !["issues", "prs"].includes(args.type)) {
+  console.error("Missing or invalid date - expected YYYY-MM-DD.");
+  process.exit(1);
+} else if (!args.date || !/^\d{4}-\d{2}-\d{2}$/.test(args.date)) {
   console.error("Missing or invalid date - expected YYYY-MM-DD.");
   process.exit(1);
 } else {
@@ -27,33 +37,51 @@ async function run() {
   /** @type {(ms: number) => Promise<void>} */
   let sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
+  let isPullRequests = args.type === "prs";
+
   /** @type {(q: string) => string} */
-  let getIssuesCmd = (q) => `gh issue list --search "${q}" --json number`;
+  let getQueryCmd = (q) =>
+    isPullRequests
+      ? `gh pr list --search "${q}" --json number`
+      : `gh issue list --search "${q}" --json number`;
 
   /** @type {(n: number) => string} */
   let getCommentCmd = (n) =>
-    `gh issue comment ${n} -F ./scripts/close-v2-issues-comment.md`;
+    isPullRequests
+      ? `gh pr comment ${n} -F ./scripts/close-v2-prs-comment.md`
+      : `gh issue comment ${n} -F ./scripts/close-v2-issues-comment.md`;
 
   /** @type {(n: number) => string} */
-  let getCloseCmd = (n) => `gh issue close ${n} -r "not planned"`;
+  let getCloseCmd = (n) =>
+    isPullRequests
+      ? `gh pr close ${n}`
+      : `gh issue close ${n} -r "not planned"`;
 
-  let issueCmd = getIssuesCmd(`is:issue state:open updated:<${args.date}`);
+  let query = isPullRequests
+    ? `is:pr is:open updated:<${args.date}`
+    : `is:issue state:open updated:<${args.date}`;
+  let issueCmd = getQueryCmd(query);
   console.log(`Executing command: ${issueCmd}`);
+
   let result = execSync(issueCmd).toString();
   console.log(`Result: ${result}`);
 
   let issues = JSON.parse(result).map((i) => i.number);
   if (issues.length > 50) {
-    console.log("❌ Refusing to close more than 50 issues at once, exiting.");
+    console.log(
+      "❌ Refusing to close more than 50 issues/PRs at once, exiting."
+    );
     process.exit(1);
   }
 
-  console.log(`Parsed ${issues.length} issues`);
+  let label = isPullRequests ? "PR" : "issue";
+
+  console.log(`Parsed ${issues.length} ${label}s`);
 
   for (let issue of issues) {
-    console.log(`--- Processing issue #${issue} ---`);
+    console.log(`--- Processing ${label} #${issue} ---`);
     let commentResult = runCmdIfTokenExists(getCommentCmd(issue));
-    console.log(`Commented on issue #${issue}: ${commentResult}`);
+    console.log(`Commented on ${label} #${issue}: ${commentResult}`);
     await sleep(250);
 
     runCmdIfTokenExists(getCloseCmd(issue));
---
+++ scripts/close-v2-prs-comment.md
@@ -0,0 +1,17 @@
+Thank you for opening this pull request, and our apologies we haven't gotten around to it yet!
+
+With the release of [React Router v7](https://remix.run/blog/react-router-v7) we are sun-setting continued development/maintenance on Remix v2. If you have not already [upgraded to React Router v7](https://reactrouter.com/upgrading/remix), we recommend you do so. We've tried to make the upgrade process as smooth as possible with our [Future Flags](https://remix.run/docs/en/main/start/future-flags). We are now in the process of cleaning up outdated issues and pull requests to improve the overall hygiene of our repositories.
+
+We plan to continue to address 2 types of issues in Remix v2:
+
+- Bugs that pose security concerns
+- Bugs that prevent upgrading to React Router v7
+
+If you believe this pull request meets one of those criteria, please respond or create a new pull request so it pops up on our radar (since github notifications may get lost in the noise 😕).
+
+For all other issues/changes, ongoing maintenance will be happening in React Router v7, so:
+
+- If this is a bug fix, please reopen against [React Router v7](https://github.com/remix-run/react-router)
+- If this is a new feature, please open a new [Proposal Discussion](https://github.com/remix-run/react-router/discussions/new?category=proposals) in React Router, and if it gets enough community support it can be considered for implementation
+
+If you have any questions you can always reach out on [Discord](https://rmx.as/discord). Thanks again for providing feedback and helping us make our framework even better!
