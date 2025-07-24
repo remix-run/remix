51fcb653252b48d47c32b0959d245a0400b818ac
Michael Jackson <mjijackson@gmail.com>
2025-07-24 14:59:55 -0700

    Update view-changes script

--- scripts/utils/packages.js
+++ scripts/utils/packages.js
@@ -1,3 +1,4 @@
+import * as fs from 'node:fs';
 import * as path from 'node:path';
 
 const __dirname = path.dirname(new URL(import.meta.url).pathname);
@@ -7,9 +8,22 @@ export const packagesDir = path.relative(
   path.resolve(__dirname, '..', '..', 'packages'),
 );
 
+/** @type () => string[] */
+export function getAllPackageNames() {
+  return fs.readdirSync(packagesDir).filter((name) => {
+    let dir = getPackageDir(name);
+    return fs.existsSync(dir) && fs.statSync(dir).isDirectory();
+  });
+}
+
 /** @type (packageName: string) => string */
 export function getPackageDir(packageName) {
   return path.resolve(packagesDir, packageName);
+}
+
+/** @type (packageName: string) => boolean */
+export function packageExists(packageName) {
+  return fs.existsSync(getPackageDir(packageName));
 }
 
 /** @type (packageName: string, filename: string) => string */
