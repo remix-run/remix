9eef82dd5ad20e8a43a6a24b780d70a9968d26c2
Mehdi Achour <machour@gmail.com>
2025-09-01 18:08:46 +0100

    Add some (failing?) edge cases

--- packages/route-pattern/src/lib/route-pattern.test.ts
+++ packages/route-pattern/src/lib/route-pattern.test.ts
@@ -420,6 +420,27 @@ describe('RoutePattern', () => {
         })
       })
 
+      it('handles arabic in URLs', () => {
+        let pattern = new RoutePattern('users/:username')
+        assert.deepEqual(pattern.match('https://example.com/users/مهدي'), {
+          params: { username: 'مهدي' },
+        })
+      })
+
+      it('handles accents in URLs', () => {
+        let pattern = new RoutePattern('users/:username')
+        assert.deepEqual(pattern.match('https://example.com/users/sélim'), {
+          params: { username: 'sélim' },
+        })
+      })
+
+      it('handles emojis in URLs', () => {
+        let pattern = new RoutePattern('frameworks/:framework')
+        assert.deepEqual(pattern.match('https://example.com/frameworks/💿'), {
+          params: { framework: '💿' },
+        })
+      })
+
       it('handles wildcards that look like paths', () => {
         let pattern = new RoutePattern('proxy/*url')
         assert.deepEqual(pattern.match('https://example.com/proxy/https://other.com/api')?.params, {
