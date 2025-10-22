fa14bc02b475895b76dbb1b3afe2fd18efd3aef4
Matt Brophy <matt@brophy.org>
2025-10-22 15:07:49 -0400

    Rename @remix-run/cookies -> @remix-run/cookie

---
+++ packages/cookie/CHANGELOG.md
@@ -0,0 +1,5 @@
+# `cookie` CHANGELOG
+
+This is the changelog for [`@remix-run/cookie`](https://github.com/remix-run/remix/tree/main/packages/cookie). It follows [semantic versioning](https://semver.org/).
+
+## HEAD
--- packages/cookie/src/lib/cookie.test.ts
+++ packages/cookie/src/lib/cookie.test.ts
@@ -1,7 +1,7 @@
 import * as assert from 'node:assert/strict'
 import { describe, it } from 'node:test'
 
-import { createCookie, isCookie } from './cookies.ts'
+import { createCookie, isCookie } from './cookie.ts'
 
 function getCookieFromSetCookie(setCookie: string): string {
   return setCookie.split(/;\s*/)[0]
