b85c017f13919872e83f0b1357abc88fe7ceb269
Pedro Cattori <pcattori@gmail.com>
2025-10-25 13:54:02 -0400

    adapt implementations from `events.ts` for `events2.ts` (type stubs)

--- packages/events/src/lib/events2.ts
+++ packages/events/src/lib/events2.ts
@@ -1,17 +1,3 @@
-export declare function events<target extends EventTarget>(
-  target: target,
-  signal?: AbortSignal,
-): EventContainer<target>
-
-// prettier-ignore
-type EventContainer<target extends EventTarget> = {
-  on: <arg, type extends GetEventType<target> | [InteractionFn<Event>, string]>(arg:
-    arg & { type: type } extends EventDescriptor<EventTarget, any> ? EventDescriptor<target, type> :
-    arg extends Array<EventDescriptor<target, any>> ? arg :
-    EventDescriptor<target, type>
-  ) => void
-}
-
 type EventListenerWithSignal<event extends Event> = (
   event: event,
   signal: AbortSignal,
@@ -28,28 +14,178 @@ type EventDescriptor<
     type extends InteractionFn<infer event> ? event :
     never
   >
-  options?: EventListenerOptions
+  options: AddEventListenerOptions
 }
-
-// prettier-ignore
-export declare function bind<target extends EventTarget, type extends GetEventType<target>>(
-  type: Autocomplete<type>,
-  listener: EventListenerWithSignal<GetEvent<target, type>>,
-  options?: AddEventListenerOptions,
-): EventDescriptor<target, type>
-
-// prettier-ignore
-export declare function bind<target extends EventTarget, event extends Event, type extends event['type']>(
-  type: [InteractionFn<event>, type],
-  listener: EventListenerWithSignal<EventWithTarget<event, target>>,
-  options?: AddEventListenerOptions,
-): EventDescriptor<target, [InteractionFn<event>, type]>
 
 type Autocomplete<T> = T | (string & {})
 
 // prettier-ignore
 export type EventWithTarget<event extends Event, target extends EventTarget> =
   Omit<event, 'currentTarget'> & { currentTarget: target }
+
+// events ------------------------------------------------------------------------------------------
+
+export function events<target extends EventTarget>(
+  target: target,
+  signal?: AbortSignal,
+): EventContainer<target> {
+  let controller = new AbortController()
+  if (signal) {
+    signal.addEventListener(
+      'abort',
+      () => controller.abort(new DOMException('1', 'EventContainer')),
+      { once: true },
+    )
+  }
+
+  let bindings: EventDescriptor<target, any>[] = []
+
+  return {
+    on: (nextDescriptors) => {
+      // prettier-ignore
+      let descriptors: Array<EventDescriptor<target, any>> =
+        Array.isArray(nextDescriptors) ? nextDescriptors :
+        nextDescriptors === undefined ? [] :
+        [nextDescriptors]
+
+      if (bindingsChanged(bindings, descriptors)) {
+        controller.abort(new DOMException('bindings changed', 'EventContainer'))
+        controller = new AbortController()
+        bindings = addEventListeners(target, descriptors, controller.signal)
+      } else {
+        bindings = updateBindingsInPlace(bindings, descriptors, controller.signal)
+      }
+    },
+    dispose: () => controller.abort(),
+  }
+}
+
+function updateBindingsInPlace(
+  descriptors: EventDescriptor<any, any>[],
+  nextDescriptors: EventDescriptor<any, any>[],
+  containerSignal: AbortSignal,
+) {
+  let bindings = []
+  for (let i = 0; i < descriptors.length; i++) {
+    bindings.push({
+      type: descriptors[i].type,
+      options: descriptors[i].options,
+      listener: withSignal(nextDescriptors[i].listener, containerSignal),
+    })
+  }
+  return bindings
+}
+
+function bindingsChanged(
+  bindings: EventDescriptor<any, any>[],
+  nextDescriptors: EventDescriptor<any, any>[],
+): boolean {
+  if (bindings.length !== nextDescriptors.length) return true
+  for (let i = 0; i < bindings.length; i++) {
+    if (bindings[i].type !== nextDescriptors[i].type) return true
+    if (optionsChanged(bindings[i].options, nextDescriptors[i].options)) return true
+  }
+  return false
+}
+
+function optionsChanged(a: AddEventListenerOptions, b: AddEventListenerOptions): boolean {
+  return (
+    a.capture !== b.capture || a.once !== b.once || a.passive !== b.passive || a.signal !== b.signal
+  )
+}
+
+let attachedInteractions = new WeakMap<EventTarget, Interaction<any>>()
+
+function addEventListeners<target extends EventTarget>(
+  target: target,
+  descriptors: EventDescriptor<target, any>[],
+  containerSignal: AbortSignal,
+  bindings: EventDescriptor<target, any>[] = [],
+) {
+  for (let descriptor of descriptors) {
+    if (Array.isArray(descriptor.type)) {
+      let [interactionType, eventType] = descriptor.type
+
+      if (!attachedInteractions.has(target)) {
+        let handle: Interaction<any> = {
+          target,
+          signal: containerSignal,
+          dispatchEvent: (event) => {
+            target.dispatchEvent(event)
+          },
+        }
+
+        let childDescriptors = interactionType.call(handle)
+        addEventListeners(target, childDescriptors, containerSignal, bindings)
+        attachedInteractions.set(target, handle)
+      }
+
+      let interactionDescriptor = { ...descriptor, type: eventType }
+      addEventListeners(target, [interactionDescriptor], containerSignal, bindings)
+      continue
+    }
+
+    let listenerSignal = descriptor.options.signal ?? containerSignal
+    let binding = {
+      type: descriptor.type,
+      options: { ...descriptor.options, signal: listenerSignal },
+      listener: withSignal(descriptor.listener, listenerSignal),
+    }
+    target.addEventListener(binding.type, binding.listener, binding.options)
+    bindings.push(binding)
+  }
+
+  return bindings
+}
+
+function withSignal(
+  listener: EventListenerWithSignal<any>,
+  containerSignal: AbortSignal,
+): EventListener {
+  let controller = new AbortController()
+
+  containerSignal.addEventListener('abort', () => controller.abort(), { once: true })
+
+  return (event) => {
+    controller.abort(new DOMException('', 'EventReentry'))
+    controller = new AbortController()
+    listener(event, controller.signal)
+  }
+}
+
+// prettier-ignore
+type EventContainer<target extends EventTarget> = {
+  on: <arg, type extends GetEventType<target> | [InteractionFn<Event>, string]>(arg:
+    arg & { type: type } extends EventDescriptor<EventTarget, any> ? EventDescriptor<target, type> :
+    arg extends Array<EventDescriptor<target, any>> ? arg :
+    EventDescriptor<target, type>
+  ) => void
+  dispose: () => void
+}
+
+// bind --------------------------------------------------------------------------------------------
+
+// prettier-ignore
+export function bind<target extends EventTarget, type extends GetEventType<target>>(
+  type: Autocomplete<type>,
+  listener: EventListenerWithSignal<GetEvent<target, type>>,
+  options?: AddEventListenerOptions,
+): EventDescriptor<target, type>
+
+// prettier-ignore
+export function bind<target extends EventTarget, event extends Event, type extends event['type']>(
+  type: [InteractionFn<event>, type],
+  listener: EventListenerWithSignal<EventWithTarget<event, target>>,
+  options?: AddEventListenerOptions,
+): EventDescriptor<target, [InteractionFn<event>, type]>
+
+export function bind(
+  type: string | [InteractionFn<any>, string],
+  listener: EventListenerWithSignal<any>,
+  options: AddEventListenerOptions = {},
+): EventDescriptor<any, any> {
+  return { type, listener, options }
+}
 
 // EventMaps ---------------------------------------------------------------------------------------
 
