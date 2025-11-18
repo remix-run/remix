e0794999b5313a8dec16829094b9bc0575810f87
Ryan Florence <rpflorence@gmail.com>
2025-11-18 13:30:24 -0700

    component: add public exports

---
+++ packages/component/src/index.ts
@@ -0,0 +1,11 @@
+import './lib/remix-types.ts'
+
+export { createRoot } from './lib/vdom.ts'
+
+export { createElement } from './lib/create-element.ts'
+
+export {
+  Fragment,
+  // Catch,
+  // Frame,
+} from './lib/component.ts'
