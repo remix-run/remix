6786163effe3c86c7f0a80639e0242da53c64d84
Matt Brophy <matt@brophy.org>
2025-12-11 17:03:42 -0500

    Add docs scripts

--- packages/fetch-router/src/lib/router.test.ts
+++ packages/fetch-router/src/lib/router.test.ts
@@ -915,6 +915,7 @@ describe('custom matcher', () => {
     }
 
     let customMatcher = new CustomMatcher()
+    // @ts-expect-error
     let router = createRouter({ matcher: customMatcher })
     router.get('/', () => new Response('Home'))
 
@@ -935,6 +936,7 @@ describe('custom matcher', () => {
     }
 
     let customMatcher = new CustomMatcher()
+    // @ts-expect-error
     let router = createRouter({ matcher: customMatcher })
     router.get('/home', () => new Response('Home'))
     router.get('/about', () => new Response('About'))
