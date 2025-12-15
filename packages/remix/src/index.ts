8e8f5a0962e10bdaa83c5025667b674f4d584554
Matt Brophy <matt@brophy.org>
2025-12-15 09:50:29 -0500

    Convert unpkg demo to use remix package

--- packages/remix/src/index.ts
+++ packages/remix/src/index.ts
@@ -1,33 +1,46 @@
 export { asyncContext, getContext } from './lib/async-context-middleware'
 export { compression } from './lib/compression-middleware'
 export { Cookie, createCookie } from './lib/cookie'
-export { createFsFileStorage } from './lib/file-storage/fs'
-export type { FileUpload } from './lib/form-data-middleware'
-export { formData } from './lib/form-data-middleware'
 export {
   createRouter,
-  type Controller,
-  type BuildAction,
-  type Middleware,
-  type Route,
   createStorageKey,
   del,
+  form,
   get,
   post,
   put,
-  route,
-  form,
   resources,
+  route,
+  type BuildAction,
+  type Controller,
+  type Middleware,
+  type Route,
 } from './lib/fetch-router'
-export { SetCookie, Cookie as CookieHeader } from './lib/headers'
+export { createFsFileStorage } from './lib/file-storage/fs'
+export { formData } from './lib/form-data-middleware'
+export type { FileUpload } from './lib/form-data-middleware'
+export { Cookie as CookieHeader, SetCookie } from './lib/headers'
+export { html, isSafeHtml, type SafeHtml } from './lib/html-template'
 export { on } from './lib/interaction.ts'
 export { logger } from './lib/logger-middleware'
 export { methodOverride } from './lib/method-override-middleware'
+export { detectMimeType, isCompressibleMimeType } from './lib/mime'
 export { createRequestListener } from './lib/node-fetch-server'
-export { createRedirectResponse } from './lib/response/redirect'
-export { createHtmlResponse } from './lib/response/html'
 export { createFileResponse } from './lib/response/file'
+export { createHtmlResponse } from './lib/response/html'
+export { createRedirectResponse } from './lib/response/redirect'
 export { createSession, Session } from './lib/session'
 export { session } from './lib/session-middleware'
 export { createFsSessionStorage } from './lib/session/fs-storage'
 export { staticFiles } from './lib/static-middleware'
+export {
+  parseTar,
+  parseTarHeader,
+  TarEntry,
+  TarParseError,
+  TarParser,
+  type ParseTarHeaderOptions,
+  type ParseTarOptions,
+  type TarHeader,
+  type TarParserOptions,
+} from './lib/tar-parser'
