779f29df72a02e661c19d0a98475d6fd9665ad8d
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2025-12-19 10:37:55 +1100

    Use `stringifyRawHeaders` in multipart-parser test

--- packages/multipart-parser/test/utils.ts
+++ packages/multipart-parser/test/utils.ts
@@ -1,4 +1,4 @@
-import { ContentDisposition, ContentType } from '@remix-run/headers'
+import { ContentDisposition, ContentType, stringifyRawHeaders } from '@remix-run/headers'
 
 export type PartValue =
   | string
@@ -27,38 +27,30 @@ export function createMultipartMessage(
     for (let [name, value] of Object.entries(parts)) {
       pushLine(`--${boundary}`)
 
+      let headers = new Headers({
+        'Content-Disposition': new ContentDisposition({
+          type: 'form-data',
+          name,
+          filename: typeof value === 'object' ? value.filename : undefined,
+          filenameSplat: typeof value === 'object' ? value.filenameSplat : undefined,
+        }).toString(),
+      })
+
+      if (typeof value === 'object' && value.mediaType) {
+        let contentType = new ContentType({ mediaType: value.mediaType })
+        headers.set('Content-Type', contentType.toString())
+      }
+
+      pushLine(stringifyRawHeaders(headers))
+      pushLine()
+
       if (typeof value === 'string') {
-        let contentDisposition = new ContentDisposition({
-          type: 'form-data',
-          name,
-        })
-
-        pushLine(`Content-Disposition: ${contentDisposition}`)
-        pushLine()
         pushLine(value)
+      } else if (typeof value.content === 'string') {
+        pushLine(value.content)
       } else {
-        let contentDisposition = new ContentDisposition({
-          type: 'form-data',
-          name,
-          filename: value.filename,
-          filenameSplat: value.filenameSplat,
-        })
-
-        let headerLines = [`Content-Disposition: ${contentDisposition}`]
-
-        if (value.mediaType) {
-          let contentType = new ContentType({ mediaType: value.mediaType })
-          headerLines.push(`Content-Type: ${contentType}`)
-        }
-
-        pushLine(headerLines.join('\r\n'))
+        chunks.push(value.content as Uint8Array<ArrayBuffer>)
         pushLine()
-        if (typeof value.content === 'string') {
-          pushLine(value.content)
-        } else {
-          chunks.push(value.content as Uint8Array<ArrayBuffer>)
-          pushLine()
-        }
       }
     }
   }
