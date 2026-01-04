8fa7520a9b70d9d5fedf70214b7458f41713bfdb
Pedro Cattori <pcattori@gmail.com>
2026-01-04 17:39:57 -0500

    experimental: part pattern parsing

---
+++ packages/route-pattern/src/experimental/part-pattern.ts
@@ -0,0 +1,202 @@
+import { ParseError } from './errors.ts'
+import type { Span } from './span.ts'
+
+type AST = {
+  tokens: Array<Token>
+  paramNames: Array<string>
+  optionals: Map<number, number>
+}
+
+type Token =
+  | { type: 'text'; text: string }
+  | { type: '(' | ')' }
+  | { type: ':' | '*'; nameIndex: number }
+
+type Variant = {
+  key: string
+  paramIndices: Array<number>
+}
+
+const IDENTIFIER_RE = /^[a-zA-Z_$][a-zA-Z_$0-9]*/
+
+export class PartPattern {
+  readonly tokens: AST['tokens']
+  readonly paramNames: AST['paramNames']
+  readonly optionals: AST['optionals']
+
+  constructor(ast: AST) {
+    this.tokens = ast.tokens
+    this.paramNames = ast.paramNames
+    this.optionals = ast.optionals
+  }
+
+  static parse(source: string, span?: Span): PartPattern {
+    span ??= [0, source.length]
+
+    let ast: AST = {
+      tokens: [],
+      paramNames: [],
+      optionals: new Map(),
+    }
+
+    let appendText = (text: string) => {
+      let currentToken = ast.tokens.at(-1)
+      if (currentToken?.type === 'text') {
+        currentToken.text += text
+      } else {
+        ast.tokens.push({ type: 'text', text })
+      }
+    }
+
+    let i = span[0]
+    let optionalStack: Array<number> = []
+    while (i < span[1]) {
+      let char = source[i]
+
+      // optional begin
+      if (char === '(') {
+        optionalStack.push(ast.tokens.length)
+        ast.tokens.push({ type: char })
+        i += 1
+        continue
+      }
+
+      // optional end
+      if (char === ')') {
+        let begin = optionalStack.pop()
+        if (begin === undefined) {
+          throw new ParseError('unmatched )', source, i)
+        }
+        ast.optionals.set(begin, ast.tokens.length)
+        ast.tokens.push({ type: char })
+        i += 1
+        continue
+      }
+
+      // variable
+      if (char === ':') {
+        i += 1
+        let name = IDENTIFIER_RE.exec(source.slice(i, span[1]))?.[0]
+        if (!name) {
+          throw new ParseError('missing variable name', source, i - 1)
+        }
+        ast.tokens.push({ type: ':', nameIndex: ast.paramNames.length })
+        ast.paramNames.push(name)
+        i += name.length
+        continue
+      }
+
+      // wildcard
+      if (char === '*') {
+        i += 1
+        let name = IDENTIFIER_RE.exec(source.slice(i, span[1]))?.[0]
+        ast.tokens.push({ type: '*', nameIndex: ast.paramNames.length })
+        ast.paramNames.push(name ?? '*')
+        i += name?.length ?? 0
+        continue
+      }
+
+      // escaped char
+      if (char === '\\') {
+        if (i + 1 === span[1]) {
+          throw new ParseError('dangling escape', source, i)
+        }
+        let text = source.slice(i, i + 2)
+        appendText(text)
+        i += text.length
+        continue
+      }
+
+      // text
+      appendText(char)
+      i += 1
+    }
+    if (optionalStack.length > 0) {
+      throw new ParseError('unmatched (', source, optionalStack.at(-1)!)
+    }
+
+    return new PartPattern(ast)
+  }
+
+  variants(): Array<Variant> {
+    let result: Array<Variant> = []
+
+    let stack: Array<{ index: number; variant: Variant }> = [
+      { index: 0, variant: { key: '', paramIndices: [] } },
+    ]
+    while (stack.length > 0) {
+      let { index, variant } = stack.pop()!
+
+      if (index === this.tokens.length) {
+        result.push(variant)
+        continue
+      }
+
+      let token = this.tokens[index]
+      if (token.type === '(') {
+        stack.push(
+          { index: index + 1, variant }, // include optional
+          { index: this.optionals.get(index)! + 1, variant: structuredClone(variant) }, // exclude optional
+        )
+        continue
+      }
+      if (token.type === ')') {
+        stack.push({ index: index + 1, variant })
+        continue
+      }
+
+      if (token.type === ':') {
+        variant.key += '{:}'
+        variant.paramIndices.push(token.nameIndex)
+        stack.push({ index: index + 1, variant })
+        continue
+      }
+
+      if (token.type === '*') {
+        variant.key += '{*}'
+        variant.paramIndices.push(token.nameIndex)
+        stack.push({ index: index + 1, variant })
+        continue
+      }
+
+      if (token.type === 'text') {
+        variant.key += token.text
+        stack.push({ index: index + 1, variant })
+        continue
+      }
+
+      throw unrecognized(token.type)
+    }
+
+    return result
+  }
+
+  toString(): string {
+    let result = ''
+    for (let token of this.tokens) {
+      if (token.type === '(' || token.type === ')') {
+        result += token.type
+        continue
+      }
+
+      if (token.type === ':' || token.type === '*') {
+        let name = this.paramNames[token.nameIndex]
+        if (name === '*') name = ''
+        result += token.type + name
+        continue
+      }
+
+      if (token.type === 'text') {
+        result += token.text
+        continue
+      }
+
+      throw unrecognized(token.type)
+    }
+    return result
+  }
+}
+
+function unrecognized(tokenType: never) {
+  return new Error(`Unrecognized token type '${tokenType}'`)
+}
