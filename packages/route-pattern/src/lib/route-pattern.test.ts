6abd39c429215150ec39bf17b26ab96800c35fa6
Matt Brophy <matt@brophy.org>
2026-01-07 09:55:15 -0500

    Revert "Add temporary failing test"
    This reverts commit dfeba6851a47e84cc91de9beeacc1a48333eb040.

--- packages/route-pattern/src/lib/route-pattern.test.ts
+++ packages/route-pattern/src/lib/route-pattern.test.ts
@@ -5,10 +5,6 @@ import { RoutePattern } from './route-pattern.ts'
 
 describe('RoutePattern', () => {
   describe('constructor', () => {
-    it('TEMPORARY FAILING TEST', () => {
-      assert.equal(true, false)
-    })
-
     it('stores the input pattern', () => {
       let pattern = new RoutePattern('users/:id')
       assert.equal(pattern.source, 'users/:id')
