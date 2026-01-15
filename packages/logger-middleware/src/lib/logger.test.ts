2715ada2e30dc66330e441985243acba181e64cd
Divine <pauldivineagionoja@gmail.com>
2026-01-15 21:01:55 +0100

    add some integration test for the colorizer class

--- packages/logger-middleware/src/lib/logger.test.ts
+++ packages/logger-middleware/src/lib/logger.test.ts
@@ -33,6 +33,50 @@ describe('logger', () => {
 
     assert.equal(messages.length, 1)
     let message = messages[0]
-    assert.match(message, /\[\d{2}\/\w{3}\/\d{4}:\d{2}:\d{2}:\d{2} [+-]\d{4}\] GET \/ \d+ \d+/)
+    assert.match(message, /\[\d{2}\/\w{3}\/\d{4}:\d{2}:\d{2}:\d{2} [+-]\d{4}\] GET \/ 200 4/)
+  })
+
+  it('logs with pretty formatters', async () => {
+    let messages: string[] = []
+    let router = createRouter({
+      middleware: [
+        logger({
+          log: (message) => messages.push(message),
+          format: '%durationPretty %contentLengthPretty',
+        }),
+      ],
+    })
+
+    router.map('/', () => new Response('Home', { headers: { 'Content-Length': '1234' } }))
+
+    await router.fetch('https://remix.run')
+
+    assert.equal(messages.length, 1)
+    // Assert that the output matches the new format, e.g., "12ms 1.2 kB"
+    assert.match(messages[0], /\d+ms 1.2 kB/)
+  })
+
+  it('logs with colors when enabled', async () => {
+    let messages: string[] = []
+    let router = createRouter({
+      middleware: [
+        logger({
+          log: (message) => messages.push(message),
+          colors: true,
+          format: '%status',
+        }),
+      ],
+    })
+
+    router.map('/', () => new Response('OK'))
+
+    await router.fetch('https://remix.run')
+
+    assert.equal(messages.length, 1)
+
+    // This checks if the output for a 200 status is either "200" (if no TTY)
+    // or the green-colored version of "200".
+    let green200 = '\x1b[32m200\x1b[0m'
+    assert.ok(messages[0] === '200' || messages[0] === green200)
   })
 })
