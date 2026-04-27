b5134ada283e93e65279f1964a67ab7d4a8b696c
Michael Jackson <mjijackson@gmail.com>
2026-04-27 11:41:40 -0700

    Re-order imports

--- packages/terminal/src/index.ts
+++ packages/terminal/src/index.ts
@@ -5,9 +5,13 @@ export type {
   TerminalModifierName,
   TerminalStyleName,
 } from './lib/ansi.ts'
+
 export { shouldUseColors } from './lib/color-support.ts'
 export type { TerminalEnvironment, UseColorOptions } from './lib/color-support.ts'
+
 export { createStyles } from './lib/styles.ts'
+export type { TerminalStyle, TerminalStyles } from './lib/styles.ts'
+
 export { createTerminal } from './lib/terminal.ts'
 export type {
   Terminal,
@@ -15,4 +19,3 @@ export type {
   TerminalOptions,
   TerminalOutputStream,
 } from './lib/terminal.ts'
-export type { TerminalStyle, TerminalStyles } from './lib/styles.ts'
