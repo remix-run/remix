80bb93ca6fd4cf50fd9544d8bb1cb036e62e7f14
Michael Jackson <mjijackson@gmail.com>
2026-04-27 12:30:48 -0700

    Use node test runner for foundational package tests

--- packages/terminal/src/lib/ansi.test.ts
+++ packages/terminal/src/lib/ansi.test.ts
@@ -1,5 +1,5 @@
-import * as assert from '@remix-run/assert'
-import { describe, it } from '@remix-run/test'
+import * as assert from 'node:assert/strict'
+import { describe, it } from 'node:test'
 
 import { ansi, ansiResetCode, ansiStyleCodes, stripAnsi } from './ansi.ts'
 
