87b43472558f2d85747479c4f6eedb565efff134
Michael Jackson <mjijackson@gmail.com>
2026-04-29 22:33:17 -0700

    Use node-serve in bootstrap app

--- packages/cli/bootstrap/server.ts
+++ packages/cli/bootstrap/server.ts
@@ -1,24 +1,25 @@
-import * as http from 'node:http'
-import { createRequestListener } from 'remix/node-fetch-server'
+import { serve } from 'remix/node-serve'
 
 import { router } from './app/router.ts'
 
-const server = http.createServer(
-  createRequestListener(async (request) => {
+const port = process.env.PORT ? Number.parseInt(process.env.PORT, 10) : 44100
+
+const server = serve(
+  async (request) => {
     try {
       return await router.fetch(request)
     } catch (error) {
       console.error(error)
       return new Response('Internal Server Error', { status: 500 })
     }
-  }),
+  },
+  {
+    port,
+  },
 )
 
-const port = process.env.PORT ? Number.parseInt(process.env.PORT, 10) : 44100
-
-server.listen(port, () => {
-  console.log(`Server listening on http://localhost:${port}`)
-})
+await server.ready
+console.log(`Server listening on http://localhost:${server.port}`)
 
 let shuttingDown = false
 
@@ -28,9 +29,8 @@ function shutdown() {
   }
 
   shuttingDown = true
-  server.close(() => {
-    process.exit(0)
-  })
+  server.close()
+  process.exit(0)
 }
 
 process.on('SIGINT', shutdown)
