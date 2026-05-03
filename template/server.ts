64cd77477ebbb47496fdf5d4d8cbf2d56ef7ae8e
Michael Jackson <mjijackson@gmail.com>
2026-05-03 01:09:40 -0700

    Use node-fetch-server in the default template (#11341)

--- template/server.ts
+++ template/server.ts
@@ -1,25 +1,24 @@
-import { serve } from 'remix/node-serve'
+import * as http from 'node:http'
+import { createRequestListener } from 'remix/node-fetch-server'
 
 import { router } from './app/router.ts'
 
 const port = process.env.PORT ? Number.parseInt(process.env.PORT, 10) : 44100
 
-const server = serve(
-  async (request) => {
+const server = http.createServer(
+  createRequestListener(async (request) => {
     try {
       return await router.fetch(request)
     } catch (error) {
       console.error(error)
       return new Response('Internal Server Error', { status: 500 })
     }
-  },
-  {
-    port,
-  },
+  }),
 )
 
-await server.ready
-console.log(`Server listening on http://localhost:${server.port}`)
+server.listen(port, () => {
+  console.log(`Server listening on http://localhost:${port}`)
+})
 
 let shuttingDown = false
 
@@ -29,8 +28,8 @@ function shutdown() {
   }
 
   shuttingDown = true
-  server.close()
-  process.exit(0)
+  server.close(() => process.exit(0))
+  server.closeAllConnections()
 }
 
 process.on('SIGINT', shutdown)
