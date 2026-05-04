141268943e9a2d0fc8e4722fbb6604fb99031622
Brooks Lybrand <brookslybrand@gmail.com>
2026-05-04 11:50:29 -0500

    Add favicon to app template

--- template/app/router.ts
+++ template/app/router.ts
@@ -1,8 +1,11 @@
 import { createRouter } from 'remix/fetch-router'
+import { staticFiles } from 'remix/static-middleware'
 
 import controller from './actions/controller.tsx'
 import { routes } from './routes.ts'
 
-export const router = createRouter()
+export const router = createRouter({
+  middleware: [staticFiles('./public', { index: false })],
+})
 
 router.map(routes, controller)
