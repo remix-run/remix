b3ff9cda6f2ba8548fa0ca4f02a6ee80159e5524
Michael Jackson <mjijackson@gmail.com>
2026-06-01 14:15:54 -0700

    Improve assert Node compatibility (#11489)

--- packages/assert/src/index.ts
+++ packages/assert/src/index.ts
@@ -1,5 +1,59 @@
-import * as assertWrapper from './lib/assert.ts'
+import {
+  AssertionError,
+  assert as assertFn,
+  deepEqual,
+  doesNotMatch,
+  doesNotReject,
+  doesNotThrow,
+  equal,
+  fail,
+  match,
+  notDeepEqual,
+  notEqual,
+  ok,
+  partialDeepEqual,
+  rejects,
+  throws,
+} from './lib/assert.ts'
 
 export * from './lib/assert.ts'
 export { expect, type Expect, type Expectation } from './lib/expect.ts'
-export default assertWrapper
+
+interface Assert {
+  (value: unknown, message?: string | Error): asserts value
+  AssertionError: typeof AssertionError
+  assert: typeof assertFn
+  ok: typeof ok
+  equal: typeof equal
+  notEqual: typeof notEqual
+  deepEqual: typeof deepEqual
+  partialDeepEqual: typeof partialDeepEqual
+  notDeepEqual: typeof notDeepEqual
+  fail: typeof fail
+  match: typeof match
+  doesNotMatch: typeof doesNotMatch
+  throws: typeof throws
+  doesNotThrow: typeof doesNotThrow
+  rejects: typeof rejects
+  doesNotReject: typeof doesNotReject
+}
+
+const assert: Assert = Object.assign(assertFn, {
+  AssertionError,
+  assert: assertFn,
+  ok,
+  equal,
+  notEqual,
+  deepEqual,
+  partialDeepEqual,
+  notDeepEqual,
+  fail,
+  match,
+  doesNotMatch,
+  throws,
+  doesNotThrow,
+  rejects,
+  doesNotReject,
+})
+
+export default assert
