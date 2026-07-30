273fbe8056413c645a87c2597a18666050e62a3f
Brooks Lybrand <brookslybrand@gmail.com>
2026-07-30 12:29:35 -0500

    Fix guides navigation highlights (#11663)

--- guides/app/actions/docs/layout.tsx
+++ guides/app/actions/docs/layout.tsx
@@ -73,7 +73,7 @@ export function DocsChapter(handle: Handle<DocsChapterProps>) {
         chapters={handle.props.chapters}
         currentChapterSlug={handle.props.slug}
       >
-        <div class="docs-layout">
+        <div class="docs-layout" data-key={`docs-chapter-${handle.props.slug}`}>
           <article class="docs-article">
             <nav class="docs-breadcrumb" aria-label="Breadcrumb" data-pagefind-ignore>
               <a href={routes.docs.index.href()}>Docs</a>
