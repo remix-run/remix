a5f1abb163e0e70b6e9c612952615a5dc16bf195
Matt Brophy <matt@brophy.org>
2026-08-27 16:20:14 -0400

    Revert "Add mixin input type reproduction"
    This reverts commit efa4869dfc62df5d93e8f4dfddf64a6f5d5408e4.

--- packages/mixin-repro/src/index.ts
+++
@@ -1,5 +0,0 @@
-import { mixinRepro } from '@remix-run/ui'
-
-export function reproduce() {
-  return mixinRepro
-}
--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -36,7 +36,6 @@ export type {
   FrameProps,
 } from './runtime/component.ts'
 export type { LoadModule, ResolveFrame, ResolveFrameOptions } from './runtime/frame.ts'
-export { mixinRepro } from './runtime/mixin-repro.ts'
 
 // -- Elements/JSX/Props --
 export { createElement } from './runtime/create-element.ts'
